f18444c918b7ded166a1d72f01c56af617fc62b8
Robert Davey <robert.davey@tgac.ac.uk>
2014-04-10 14:53:21 +0100

    MISO-66: Fix container platform DB patch

--- sqlstore/src/main/resources/schemas/patches/patch_20140408_container-platform.sql
+++ sqlstore/src/main/resources/schemas/patches/patch_20140408_container-platform.sql
@@ -1,6 +1,6 @@
 USE lims;
 
-ALTER TABLE lims.SequencerPartitionContainer CHANGE platformType platform BIGINT(20);
+UPDATE SequencerPartitionContainer s SET s.platformType = 0;
 
 UPDATE SequencerPartitionContainer s,
 (SELECT spc.containerId AS cId, p.platformId AS pId, p.name, p.instrumentModel
@@ -9,6 +9,7 @@ INNER JOIN SequencerReference sr ON sr.platformId = p.platformId
 INNER JOIN Run ra ON sr.referenceId = ra.sequencerReference_sequencerReferenceId
 INNER JOIN Run_SequencerPartitionContainer rf ON rf.Run_runId = ra.runId
 LEFT JOIN SequencerPartitionContainer spc ON spc.containerId = rf.containers_containerId) AS src
-SET s.platform = src.pId
+SET s.platformType = src.pId
 WHERE s.containerId=src.cId;
 
+ALTER TABLE lims.SequencerPartitionContainer CHANGE platformType platform BIGINT(20);
