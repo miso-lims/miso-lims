fc8b48aee58ddc28a6847cf980e56f998125f62c
Xingdong Bian <xingdong.bian@tgac.ac.uk>
2015-01-20 11:24:10 +0000

    Add workflow classes

---
+++ core/src/main/java/uk/ac/bbsrc/tgac/miso/core/workflow/WorkflowDefinition.java
@@ -0,0 +1,17 @@
+package uk.ac.bbsrc.tgac.miso.core.workflow;
+
+import java.util.List;
+
+/**
+ * uk.ac.bbsrc.tgac.miso.core.workflow
+ * <p/>
+ * Info
+ *
+ * @author Rob Davey
+ * @date 15/03/13
+ * @since 0.2.0
+ */
+public interface WorkflowDefinition {
+  public String getName();
+  public List<WorkflowProcess> getWorkflowProcesses();
+}
