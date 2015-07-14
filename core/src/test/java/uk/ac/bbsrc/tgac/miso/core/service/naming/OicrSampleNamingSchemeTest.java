d3875d1f6ec83614acf92c53a82432f3d4b0ad61
Tony DeBat <tony.debat@oicr.on.ca>
2015-07-14 17:29:08 -0400

    GLT-163 Added an OICR sample naming class along with tests. Updated spring configuration to load OICR sample naming class instead of default.

---
+++ core/src/test/java/uk/ac/bbsrc/tgac/miso/core/service/naming/OicrSampleNamingSchemeTest.java
@@ -0,0 +1,36 @@
+package uk.ac.bbsrc.tgac.miso.core.service.naming;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import uk.ac.bbsrc.tgac.miso.core.data.Sample;
+
+public class OicrSampleNamingSchemeTest {
+	
+	private MisoNamingScheme<Sample> sut;
+
+	@Before
+	public void setUp() throws Exception {
+		sut = new OicrSampleNamingScheme();
+	}
+	
+	@Test
+	public void test_alias() throws Exception {
+		assertThat(sut.validateField("alias", "BART_1273_Br_P_nn_1-1_D"), is(true));
+	}
+	
+	@Test
+	public void test_alias_with_aliquot() throws Exception {
+		assertThat(sut.validateField("alias", "BART_1273_Br_P_nn_1-1_D_4"), is(true));
+	}
+	
+	@Test
+	public void test_alias_with_stock_aliquot() throws Exception {
+		assertThat(sut.validateField("alias", "BART_1273_Br_P_nn_1-1_D_S3"), is(true));
+	}
+
+
+}
