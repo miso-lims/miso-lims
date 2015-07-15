b4bbb46ea811bfdaf7568c97bd5a4ae1775eeea7
Walid Shaheer <wshaheer@outlook.com>
2015-07-15 11:59:14 -0400

    added checks for java version, maven version and mysql running instance

--- build/script.sh
+++ build/script.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 set -e
 source script.properties
+# Check java installation and version
+JAVA_VERSION=`java -version 2>&1 | grep "java version" | awk '{print $3}' | tr -d \" | awk '{split($0, array, ".")} END{print array[2]}'`
+MAVEN_VERSION=`java -version 2>&1 | grep "java version" | awk '{print $3}' | tr -d \" | awk '{split($0, array, ".")} END{print array[2]}'`
+JAVA_INSTALL=$1
+MAVEN_INSTALL=$1
+MYSQL_SERVICE=$1
+if [ "$JAVA_VERSION" -gt 7 ]; then
+	echo "Java install OK"
+	JAVA_INSTALL=$0
+else
+	echo "JDK version 7 or greater is requried"
+	exit 1
+fi
+if [ "$MAVEN_VERSION" -gt 7 ]; then
+	echo "Maven install OK"
+	MAVEN_INSTALL=$0
+else
+	echo "Maven version 2 or greater is requried"
+	exit 1
+fi
+if pgrep mysql >/dev/null 2>&1; then
+	echo "MySQL running"
+	MYSQL_SERVICE=$0
+else
+	echo "MySQL not running"
+	exit 1
+fi
+echo "If program exits, should not reach here"
 if [ -d "$CONTEXT_PATH" ]; then
 	echo "$CONTEXT_PATH directory already exits"
 	# Application Context (edit and add to META-INF)
