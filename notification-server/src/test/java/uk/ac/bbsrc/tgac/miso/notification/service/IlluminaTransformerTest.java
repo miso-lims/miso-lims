65ce46a6923e61342577e7d32e1de7f01d56515f
Dillan Cooke <dillan.cooke@oicr.on.ca>
2015-08-14 16:58:02 -0400

    unit tests, fixed bugs found while testing
    Conflicts:
    	notification-server/src/test/java/uk/ac/bbsrc/tgac/miso/notification/service/IlluminaTransformerTest.java

--- notification-server/src/test/java/uk/ac/bbsrc/tgac/miso/notification/service/IlluminaTransformerTest.java
+++ notification-server/src/test/java/uk/ac/bbsrc/tgac/miso/notification/service/IlluminaTransformerTest.java
@@ -14,9 +14,24 @@ import org.junit.Test;
 
 public class IlluminaTransformerTest {
   
-  private static final String h1080_84_raw = "/runs/111110_h1080_0084_AC08UPACXX_raw";
-  private static final String h1080_84_gzip = "/runs/111110_h1080_0084_AC08UPACXX_raw";
-  private static final String m753_25_running = "/runs/150812_M00753_0025_000000000-AHEN9_running";
+  private static final String h1080_84_raw = "/runs/raw/Completed/111110_h1080_0084_AC08UPACXX";
+  private static final String h1080_84_gzip = "/runs/gzipped/Completed/111110_h1080_0084_AC08UPACXX";
+  private static final String h1080_84_nostat = "/runs/raw/Completed/noStatus/111110_h1080_0084_AC08UPACXX";
+  private static final String h1080_84_nostat_noparam = "/runs/raw/Completed/noStatus_noParams/111110_h1080_0084_AC08UPACXX";
+  private static final String h1080_84_noinfo = "/runs/raw/Completed/noRunInfo/111110_h1080_0084_AC08UPACXX";
+  private static final String h1080_84_runcomplete = "/runs/raw/Completed/run.complete/111110_h1080_0084_AC08UPACXX";
+  private static final String m753_25_running = "/runs/raw/Running/150812_M00753_0025_000000000-AHEN9";
+  private static final String h1179_162_gzip = "/runs/raw/Completed/140108_SN7001179_0162_AC3E41ACXX";
+  private static final String h1080_84_lastCycle_viaStatus = "/runs/raw/Completed/lastCycle/viaStatus/111110_h1080_0084_AC08UPACXX";
+  private static final String h1179_162_gzip_lastCycle_viaCycleTimes = "/runs/raw/Completed/lastCycle/viaCycleTimes/140108_SN7001179_0162_AC3E41ACXX";
+  private static final String h1179_162_gzip_lastCycle_fail = "/runs/raw/Completed/lastCycle/fail/140108_SN7001179_0162_AC3E41ACXX";
+  private static final String h1080_84_date_viaCycleTimes = "/runs/raw/Completed/dateCompleted/viaCycleTimes/111110_h1080_0084_AC08UPACXX";
+  private static final String h1179_162_date_viaRtaComplete = "/runs/raw/Completed/dateCompleted/viaRTAComplete/140108_SN7001179_0162_AC3E41ACXX";
+  private static final String h1080_84_date_viaRtaLog = "/runs/raw/Completed/dateCompleted/viaRTALog/111110_h1080_0084_AC08UPACXX";
+  private static final String h1179_162_date_fail = "/runs/raw/Completed/dateCompleted/fail/140108_SN7001179_0162_AC3E41ACXX";
+  private static final String h1179_162_minimal_1 = "/runs/raw/Completed/minimal/01/140108_SN7001179_0162_AC3E41ACXX";
+  private static final String h1179_162_minimal_2 = "/runs/raw/Completed/minimal/02/140108_SN7001179_0162_AC3E41ACXX";
+  
   
   private String getResourcePath(String path) {
     return this.getClass().getResource(path).getPath();
@@ -94,6 +109,18 @@ public class IlluminaTransformerTest {
   }
   
   @Test
+  public void testTransform2Gzipped() throws JSONException {
+    Map<String, String> map = transform(h1179_162_gzip);
+    
+    JSONArray completed = new JSONArray(map.get(IlluminaTransformer.STATUS_COMPLETE));
+    assertTrue(completed.length() == 1);
+    
+    JSONObject run = (JSONObject) completed.get(0);
+    assertComplete(run, false);
+    assertTrue("140108_SN7001179_0162_AC3E41ACXX".equals(run.getString(IlluminaTransformer.JSON_RUN_NAME)));
+  }
+  
+  @Test
   public void testRawVsGzipped() throws JSONException {
     Map<String, String> map = transform(h1080_84_raw);
     JSONObject raw = (JSONObject) new JSONArray(map.get(IlluminaTransformer.STATUS_COMPLETE)).get(0);
@@ -109,6 +136,148 @@ public class IlluminaTransformerTest {
     Map<String, String> map = transform(m753_25_running);
     JSONArray running = new JSONArray(map.get(IlluminaTransformer.STATUS_RUNNING));
     assertTrue(running.length() == 1);
+  }
+  
+  @Test
+  public void testStatusMissing() throws JSONException {
+    Map<String, String> map = transform(h1080_84_raw);
+    JSONObject raw = (JSONObject) new JSONArray(map.get(IlluminaTransformer.STATUS_COMPLETE)).get(0);
+    
+    // Should be able to get all the same data, except the status field
+    map = transform(h1080_84_nostat);
+    JSONObject nostat = (JSONObject) new JSONArray(map.get(IlluminaTransformer.STATUS_COMPLETE)).get(0);
+    
+    assertEqualFields(raw, nostat, IlluminaTransformer.JSON_RUN_NAME);
+    assertEqualFields(raw, nostat, IlluminaTransformer.JSON_RUN_INFO);
+    assertEqualFields(raw, nostat, IlluminaTransformer.JSON_RUN_PARAMS);
+    assertEqualFields(raw, nostat, IlluminaTransformer.JSON_SEQUENCER_NAME);
+    assertEqualFields(raw, nostat, IlluminaTransformer.JSON_CONTAINER_ID);
+    assertEqualFields(raw, nostat, IlluminaTransformer.JSON_LANE_COUNT);
+    assertEqualFields(raw, nostat, IlluminaTransformer.JSON_NUM_CYCLES);
+    assertEqualFields(raw, nostat, IlluminaTransformer.JSON_START_DATE);
+    assertEqualFields(raw, nostat, IlluminaTransformer.JSON_COMPLETE_DATE);
+  }
+  
+  @Test
+  public void testStatusAndParamsMissing() throws JSONException {
+    Map<String, String> map = transform(h1080_84_raw);
+    JSONObject raw = (JSONObject) new JSONArray(map.get(IlluminaTransformer.STATUS_COMPLETE)).get(0);
+    
+    // Should be able to get all the same data, except the status and runParams fields
+    map = transform(h1080_84_nostat_noparam);
+    JSONObject nostat_noparam = (JSONObject) new JSONArray(map.get(IlluminaTransformer.STATUS_COMPLETE)).get(0);
+    
+    assertEqualFields(raw, nostat_noparam, IlluminaTransformer.JSON_RUN_NAME);
+    assertEqualFields(raw, nostat_noparam, IlluminaTransformer.JSON_RUN_INFO);
+    assertEqualFields(raw, nostat_noparam, IlluminaTransformer.JSON_SEQUENCER_NAME);
+    assertEqualFields(raw, nostat_noparam, IlluminaTransformer.JSON_CONTAINER_ID);
+    assertEqualFields(raw, nostat_noparam, IlluminaTransformer.JSON_LANE_COUNT);
+    assertEqualFields(raw, nostat_noparam, IlluminaTransformer.JSON_NUM_CYCLES);
+    assertEqualFields(raw, nostat_noparam, IlluminaTransformer.JSON_START_DATE);
+    assertEqualFields(raw, nostat_noparam, IlluminaTransformer.JSON_COMPLETE_DATE);
+  }
+  
+  @Test
+  public void testRunInfoMissing() throws JSONException {
+    Map<String, String> map = transform(h1080_84_raw);
+    JSONObject raw = (JSONObject) new JSONArray(map.get(IlluminaTransformer.STATUS_COMPLETE)).get(0);
+    
+    // Should be able to get all the same data, except the runInfo and laneCount fields
+    map = transform(h1080_84_noinfo);
+    JSONObject nostat = (JSONObject) new JSONArray(map.get(IlluminaTransformer.STATUS_COMPLETE)).get(0);
+    
+    assertEqualFields(raw, nostat, IlluminaTransformer.JSON_RUN_NAME);
+    assertEqualFields(raw, nostat, IlluminaTransformer.JSON_STATUS);
+    assertEqualFields(raw, nostat, IlluminaTransformer.JSON_RUN_PARAMS);
+    assertEqualFields(raw, nostat, IlluminaTransformer.JSON_SEQUENCER_NAME);
+    assertEqualFields(raw, nostat, IlluminaTransformer.JSON_CONTAINER_ID);
+    assertEqualFields(raw, nostat, IlluminaTransformer.JSON_NUM_CYCLES);
+    assertEqualFields(raw, nostat, IlluminaTransformer.JSON_START_DATE);
+    assertEqualFields(raw, nostat, IlluminaTransformer.JSON_COMPLETE_DATE);
+  }
+  
+  @Test
+  public void testRunCompleteFile() throws JSONException {
+    Map<String, String> map = transform(h1080_84_runcomplete);
+    assertTrue(new JSONArray(map.get(IlluminaTransformer.STATUS_COMPLETE)).length() == 1);
+  }
+  
+  @Test
+  public void testLastCycleViaStatus() throws JSONException {
+    Map<String, String> map = transform(h1080_84_lastCycle_viaStatus);
+    assertTrue(new JSONArray(map.get(IlluminaTransformer.STATUS_COMPLETE)).length() == 1);
+  }
+  
+  @Test
+  public void testLastCycleViaCycleTimes() throws JSONException {
+    Map<String, String> map = transform(h1179_162_gzip_lastCycle_viaCycleTimes);
+    assertTrue(new JSONArray(map.get(IlluminaTransformer.STATUS_COMPLETE)).length() == 1);
+  }
+  
+  @Test
+  public void testLastCycleNotFound() throws JSONException {
+    Map<String, String> map = transform(h1179_162_gzip_lastCycle_fail);
+    assertTrue(new JSONArray(map.get(IlluminaTransformer.STATUS_COMPLETE)).length() == 0);
+  }
+  
+  @Test
+  public void testDateCompletedViaCycleTimes() throws JSONException {
+    Map<String, String> map = transform(h1080_84_date_viaCycleTimes);
+    JSONObject run = (JSONObject) new JSONArray(map.get(IlluminaTransformer.STATUS_COMPLETE)).get(0);
+    
+    assertTrue(run.has(IlluminaTransformer.JSON_COMPLETE_DATE));
+    String date = run.getString(IlluminaTransformer.JSON_COMPLETE_DATE);
+    assertTrue(date != null && !date.equals(""));
+    assertTrue(!date.equals("null"));
+  }
+  
+  @Test
+  public void testDateCompletedViaRtaComplete() throws JSONException {
+    Map<String, String> map = transform(h1179_162_date_viaRtaComplete);
+    JSONObject run = (JSONObject) new JSONArray(map.get(IlluminaTransformer.STATUS_COMPLETE)).get(0);
+    
+    assertTrue(run.has(IlluminaTransformer.JSON_COMPLETE_DATE));
+    String date = run.getString(IlluminaTransformer.JSON_COMPLETE_DATE);
+    assertTrue(date != null && !date.equals(""));
+    assertTrue(!date.equals("null"));
+  }
+  
+  @Test
+  public void testDateCompletedViaRtaLog() throws JSONException {
+    Map<String, String> map = transform(h1080_84_date_viaRtaLog);
+    JSONObject run = (JSONObject) new JSONArray(map.get(IlluminaTransformer.STATUS_COMPLETE)).get(0);
+    
+    assertTrue(run.has(IlluminaTransformer.JSON_COMPLETE_DATE));
+    String date = run.getString(IlluminaTransformer.JSON_COMPLETE_DATE);
+    assertTrue(date != null && !date.equals(""));
+    assertTrue(!date.equals("null"));
+  }
+  
+  @Test
+  public void testDateCompletedFail() throws JSONException {
+    Map<String, String> map = transform(h1179_162_date_fail);
+    JSONObject run = (JSONObject) new JSONArray(map.get(IlluminaTransformer.STATUS_COMPLETE)).get(0);
+    
+    assertTrue(run.has(IlluminaTransformer.JSON_COMPLETE_DATE));
+    String date = run.getString(IlluminaTransformer.JSON_COMPLETE_DATE);
+    assertTrue(date != null && !date.equals(""));
+    assertTrue(date.equals("null"));
+  }
+  
+  @Test
+  public void testMinimalFileSet1() throws JSONException {
+    Map<String, String> map = transform(h1179_162_minimal_1);
+    JSONObject run = (JSONObject) new JSONArray(map.get(IlluminaTransformer.STATUS_COMPLETE)).get(0);
+    
+    assertComplete(run, false);
+  }
+  
+  @Test
+  public void testMinimalFileSet2() throws JSONException {
+    Map<String, String> map = transform(h1179_162_minimal_2);
+    JSONObject run = (JSONObject) new JSONArray(map.get(IlluminaTransformer.STATUS_COMPLETE)).get(0);
+    
+    assertComplete(run, false);
   }
   
 }
