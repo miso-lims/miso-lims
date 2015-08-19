c59c1aabfec1a33206f040b3b394168fbb760544
Walid Shaheer <wshaheer@outlook.com>
2015-08-19 14:39:27 -0400

    [Fixed] jenkins database setup script to be aware of own location

--- build/jenkins_setupDatabase.sh
+++ build/jenkins_setupDatabase.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Get script directory
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 set -e
 Q1="DROP DATABSE IF EXISTS $DB_NAME"
 Q2="CREATE DATABASE IF NOT EXISTS $DB_NAME;"
@@ -9,5 +11,5 @@ SQL="${Q1}${Q2}${Q3}${Q4}"
 $( mysql --host=${DB_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASS} -e "$SQL" )
 echo "User $MYSQL_USER added to $DB_NAME database"
 # Add schemas to lims db
-$( mysql --host=${DB_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASS} -D $DB_NAME < /home/tdebat/jenkins/lims-schema-20150617.sql )
-$( mysql --host=${DB_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASS} -D $DB_NAME < /home/tdebat/jenkins/miso_type_data_20120921.sql )
+$( mysql --host=${DB_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASS} -D $DB_NAME < "${DIR}/lims-schema-20150617.sql" )
+$( mysql --host=${DB_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASS} -D $DB_NAME < "${DIR}/miso_type_data_20120921.sql" )
