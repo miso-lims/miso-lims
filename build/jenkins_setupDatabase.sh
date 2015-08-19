2af300ee50a196edb07e244433459334a8d34f97
Walid Shaheer <wshaheer@outlook.com>
2015-08-19 15:00:46 -0400

    [Fixed] database setup script to correct syntax and formatting

--- build/jenkins_setupDatabase.sh
+++ build/jenkins_setupDatabase.sh
@@ -8,8 +8,8 @@ Q2="CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 Q3="GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO ${MYSQL_USER}@${DB_HOST};"
 Q4="FLUSH PRIVILEGES;"
 SQL="${Q1}${Q2}${Q3}${Q4}"
-$( mysql --host=${DB_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASS} -e "$SQL" )
+$( mysql --host="${DB_HOST}" --user="${MYSQL_USER}" --password="${MYSQL_PASS}" -e "$SQL" )
 echo "User $MYSQL_USER added to $DB_NAME database"
 # Add schemas to lims db
-$( mysql --host=${DB_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASS} -D $DB_NAME < "${DIR}/lims-schema-20150617.sql" )
-$( mysql --host=${DB_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASS} -D $DB_NAME < "${DIR}/miso_type_data_20120921.sql" )
+$( mysql --host="${DB_HOST}" --user="${MYSQL_USER}" --password="${MYSQL_PASS}" -D "$DB_NAME" < "${DIR}/lims-schema-20150617.sql" )
+$( mysql --host="${DB_HOST}" --user="${MYSQL_USER}" --password="${MYSQL_PASS}" -D "$DB_NAME" < "${DIR}/miso_type_data_20120921.sql" )
