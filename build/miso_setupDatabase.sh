f100fdd905568fcee859221c812715835f298fcd
Walid Shaheer <wshaheer@outlook.com>
2015-08-20 15:42:30 -0400

    [Updated] properties file to remove values specific to local machine, local deployment and database setup scripts to remove unrequired steps and evaluation

--- build/miso_setupDatabase.sh
+++ build/miso_setupDatabase.sh
@@ -4,24 +4,16 @@ set -e
 # Get script directory
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # Set Environment variables
-source "$DIR/context.properties"
-DB_ARGS=($DB_HOST $DB_NAME $DB_USER $DB_PASS)
+source "${DIR}/context.properties"
 # Create/Update MySQL db with credentials set in properties file
-MYSQL=`which mysql`
-Q1="CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-Q2="GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'$DB_HOST' IDENTIFIED BY '$DB_PASS';"
-Q3="FLUSH PRIVILEGES;"
-SQL="${Q1}${Q2}${Q3}"
-# Check for missing variables required to create/update db
-for (( i=0; i<4; i++));
-do
-	if [ -z "${DB_ARGS[i]}" ]; then
-		echo "Missing arguements"
-		exit 65
-	fi
-done
-$MYSQL -u root -e "$SQL"
+MYSQL=$( which mysql )
+Q1="DROP DATABASE IF EXISTS $DB_NAME;"
+Q2="CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+Q3="GRANT ALL ON $DB_NAME.* TO $MYSQL_USER@$DB_HOST;"
+Q4="FLUSH PRIVILEGES;"
+SQL="${Q1}${Q2}${Q3}${Q4}"
+$MYSQL --login-path=client -e "$SQL"
 echo "Database $DB_NAME and user $DB_USER created"
 # Add schemas to lims db
-$MYSQL -D $DB_NAME < "$DIR/../sqlstore/src/main/resources/schemas/lims-schema-20150617.sql"
-$MYSQL -D $DB_NAME < "$DIR/../sqlstore/src/main/resources/schemas/miso_type_data_20120921.sql"
+$MYSQL --login-path=miso -D $DB_NAME < "$DIR/../sqlstore/src/main/resources/schemas/lims-schema-20150617.sql"
+$MYSQL --login-path=miso -D $DB_NAME < "$DIR/../sqlstore/src/main/resources/schemas/miso_type_data_20120921.sql"
