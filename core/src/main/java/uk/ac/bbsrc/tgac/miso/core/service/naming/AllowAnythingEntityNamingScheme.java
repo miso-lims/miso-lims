d4598328a0e9e3f95d2a2a69819081a68fee29ea
Andre Masella <andre.masella@oicr.on.ca>
2015-11-17 14:15:08 -0500

    Make allow anything naming scheme allow anything

--- core/src/main/java/uk/ac/bbsrc/tgac/miso/core/service/naming/AllowAnythingEntityNamingScheme.java
+++ core/src/main/java/uk/ac/bbsrc/tgac/miso/core/service/naming/AllowAnythingEntityNamingScheme.java
@@ -2,18 +2,11 @@ package uk.ac.bbsrc.tgac.miso.core.service.naming;
 
 import static uk.ac.bbsrc.tgac.miso.core.util.LimsUtils.isStringEmptyOrNull;
 
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Pattern;
-
+import net.sourceforge.fluxion.spi.ServiceProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import net.sourceforge.fluxion.spi.ServiceProvider;
 import uk.ac.bbsrc.tgac.miso.core.data.Nameable;
 import uk.ac.bbsrc.tgac.miso.core.exception.MisoNamingException;
-import uk.ac.bbsrc.tgac.miso.core.util.LimsUtils;
 
 /**
  * uk.ac.bbsrc.tgac.miso.core.service.naming
@@ -29,13 +22,10 @@ public class AllowAnythingEntityNamingScheme<T extends Nameable> implements Miso
   protected static final Logger log = LoggerFactory.getLogger(AllowAnythingEntityNamingScheme.class);
 
   private Class<T> type;
-  private Map<String, Pattern> validationMap = new HashMap<String, Pattern>();
-  private Map<String, NameGenerator<T>> customNameGeneratorMap = new HashMap<String, NameGenerator<T>>();
 
   public AllowAnythingEntityNamingScheme() {
     try {
       type = (Class<T>) Class.forName("uk.ac.bbsrc.tgac.miso.core.data.Nameable");
-      validationMap.put("name", Pattern.compile("(.*)"));
     } catch (ClassNotFoundException e) {
       log.error("constructor", e);
     }
@@ -43,21 +33,10 @@ public class AllowAnythingEntityNamingScheme<T extends Nameable> implements Miso
 
   public AllowAnythingEntityNamingScheme(Class<T> type) {
     this.type = type;
-    validationMap.put("name", Pattern.compile("(.*)"));
   }
 
   @Override
   public void setValidationRegex(String fieldName, String regex) throws MisoNamingException {
-    if (fieldCheck(fieldName)) {
-      if (validationMap.get(fieldName) != null) {
-        log.warn("Setting validation regex from '" + validationMap.get(fieldName).pattern() + "' to '" + regex
-            + "'. This usually doesn't happen at " + "runtime unless a custom regex is specified at MISO startup!");
-        validationMap.put(fieldName, Pattern.compile(regex));
-      }
-    } else {
-      throw new MisoNamingException("Cannot set validation regex for a field (via 'get" + LimsUtils.capitalise(fieldName)
-          + "') that doesn't exist in " + namingSchemeFor().getCanonicalName());
-    }
   }
 
   @Override
@@ -76,29 +55,12 @@ public class AllowAnythingEntityNamingScheme<T extends Nameable> implements Miso
 
   @Override
   public String generateNameFor(String fieldName, T o) throws MisoNamingException {
-    if (customNameGeneratorMap.get(fieldName) != null) {
-      NameGenerator<T> lng = customNameGeneratorMap.get(fieldName);
-      String customName = lng.generateName(o);
-      if (validateField(fieldName, customName)) {
-        return customName;
-      } else {
-        throw new MisoNamingException("Custom naming generator '" + lng.getGeneratorName() + "' supplied for entity field '" + fieldName
-            + "' generated an invalid name according to the validation scheme '" + validationMap.get(fieldName) + "'");
-      }
-    } else {
-      log.info("Generating name for " + o.getClass().getSimpleName() + " :: " + o.getId());
-      return "" + o.getId();
-    }
+    return o.getClass().getSimpleName() + o.getId();
   }
 
   @Override
   public String getValidationRegex(String fieldName) throws MisoNamingException {
-    Pattern p = validationMap.get(fieldName);
-    if (p != null) {
-      return validationMap.get(fieldName).pattern();
-    } else {
-      throw new MisoNamingException("No such field registered for validation");
-    }
+    return ".*";
   }
 
   @Override
@@ -108,34 +70,22 @@ public class AllowAnythingEntityNamingScheme<T extends Nameable> implements Miso
 
   @Override
   public void registerCustomNameGenerator(String fieldName, NameGenerator<T> nameGenerator) {
-    this.customNameGeneratorMap.put(fieldName, nameGenerator);
   }
 
   @Override
   public void unregisterCustomNameGenerator(String fieldName) {
-    this.customNameGeneratorMap.remove(fieldName);
   }
 
   @Override
   public boolean allowDuplicateEntityNameFor(String fieldName) {
-    // if the field exists, then duplicates are allowed
-    return fieldCheck(fieldName);
+    return true;
   }
 
   @Override
   public void setAllowDuplicateEntityName(String fieldName, boolean allow) {
-    log.error("All duplicate names are allowed for all fields in this scheme.");
   }
 
   private boolean fieldCheck(String fieldName) {
-    try {
-      Method m = namingSchemeFor().getMethod("get" + LimsUtils.capitalise(fieldName));
-      if (m != null) {
         return true;
-      }
-    } catch (NoSuchMethodException e) {
-      log.error("No such field '" + fieldName + "' on class " + namingSchemeFor().getCanonicalName(), e);
-    }
-    return false;
   }
 }
