cdb2bb5a141fd06f5c98bab3187043ec7592ef68
Tony DeBat <tony.debat@oicr.on.ca>
2015-11-27 14:52:35 -0500

    GLT-234 Update hibernate annotations to handle change logs in Sample.

--- core/src/main/java/uk/ac/bbsrc/tgac/miso/core/data/AbstractSample.java
+++ core/src/main/java/uk/ac/bbsrc/tgac/miso/core/data/AbstractSample.java
@@ -51,12 +51,14 @@ import com.eaglegenomics.simlims.core.Note;
 import com.eaglegenomics.simlims.core.SecurityProfile;
 import com.eaglegenomics.simlims.core.User;
 
+import uk.ac.bbsrc.tgac.miso.core.data.impl.IdentityImpl;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.ProjectImpl;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.SampleAdditionalInfoImpl;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.SampleAnalyteImpl;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.SampleAnalyteNode;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.SampleIdentityNode;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.SampleImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.UserImpl;
 import uk.ac.bbsrc.tgac.miso.core.exception.MalformedLibraryException;
 import uk.ac.bbsrc.tgac.miso.core.exception.MalformedSampleException;
 import uk.ac.bbsrc.tgac.miso.core.exception.MalformedSampleQcException;
@@ -94,6 +96,7 @@ public abstract class AbstractSample extends AbstractBoxable implements Sample {
   @Transient
   private Collection<Note> notes = new HashSet<Note>();
 
+  @Transient
   private final Collection<ChangeLog> changeLog = new ArrayList<>();
 
   @Transient
@@ -116,7 +119,12 @@ public abstract class AbstractSample extends AbstractBoxable implements Sample {
   private String identificationBarcode;
   private String locationBarcode;
   private String alias;
+
+  @Transient
   private Date lastUpdated;
+
+  @OneToOne(targetEntity = UserImpl.class)
+  @JoinColumn(name = "lastModifier", nullable = false)
   private User lastModifier;
   private boolean empty;
 
@@ -124,7 +132,7 @@ public abstract class AbstractSample extends AbstractBoxable implements Sample {
   @JoinColumn(name = "sampleAnalyteId")
   private SampleAnalyte sampleAnalyte;
 
-  @OneToOne(targetEntity = Identity.class)
+  @OneToOne(targetEntity = IdentityImpl.class)
   @JoinColumn(name = "identityId")
   private Identity identity;
 
