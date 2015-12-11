2869415829a5baea654f6b044db69842901122d6
Andre Masella <andre.masella@oicr.on.ca>
2015-12-11 10:02:09 -0500

    Annotation for sample hierarchy

--- core/src/main/java/uk/ac/bbsrc/tgac/miso/core/data/impl/SampleAnalyteNode.java
+++ core/src/main/java/uk/ac/bbsrc/tgac/miso/core/data/impl/SampleAnalyteNode.java
@@ -1,5 +1,8 @@
 package uk.ac.bbsrc.tgac.miso.core.data.impl;
 
+import uk.ac.bbsrc.tgac.miso.core.data.SampleCategory;
+
+@SampleCategory(alias = "Analyte")
 public class SampleAnalyteNode extends SampleImpl {
 
   private static final long serialVersionUID = 1L;
--- core/src/main/java/uk/ac/bbsrc/tgac/miso/core/data/impl/SampleIdentityNode.java
+++ core/src/main/java/uk/ac/bbsrc/tgac/miso/core/data/impl/SampleIdentityNode.java
@@ -1,5 +1,8 @@
 package uk.ac.bbsrc.tgac.miso.core.data.impl;
 
+import uk.ac.bbsrc.tgac.miso.core.data.SampleCategory;
+
+@SampleCategory(alias = "Identity")
 public class SampleIdentityNode extends SampleImpl {
 
   private static final long serialVersionUID = 1L;
