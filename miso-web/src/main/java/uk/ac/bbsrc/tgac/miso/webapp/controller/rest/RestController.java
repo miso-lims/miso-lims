e45b06dcdb4c16687fb3b913c159f3b6e162e187
Dillan Cooke <dillan.cooke@oicr.on.ca>
2015-12-18 11:16:28 -0500

    made RestController abstract, added Javadoc

--- miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/controller/rest/RestController.java
+++ miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/controller/rest/RestController.java
@@ -8,10 +8,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import uk.ac.bbsrc.tgac.miso.webapp.controller.rest.RestExceptionHandler.RestError;
 
-public class RestController {
+/**
+ * Abstract class meant to be extended by all REST controllers. Adds exception handling for all exceptions
+ */
+public abstract class RestController {
   
+  /**
+   * converts an Exception into a RestError model to be returned in response to a REST request. Ensures that the correct response 
+   * status is set.
+   * 
+   * @param request the request being handled when the exception occurred
+   * @param response the response to be sent
+   * @param exception the exception to be handled
+   * @return the RestError containing exception and HTTP response details
+   */
   @ExceptionHandler(Exception.class)
-  protected @ResponseBody RestError handleError(HttpServletRequest request, HttpServletResponse response, Exception exception) {
+  private @ResponseBody RestError handleError(HttpServletRequest request, HttpServletResponse response, Exception exception) {
     return RestExceptionHandler.handleException(request, response, exception);
   }
   
