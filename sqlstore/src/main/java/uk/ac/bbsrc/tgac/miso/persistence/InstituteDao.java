bd36db50353b4242f46089e5f9a5acb94f8b30c3
Dillan Cooke <dillan.cooke@oicr.on.ca>
2016-01-28 13:00:59 -0500

    javadocs

--- sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/InstituteDao.java
+++ sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/InstituteDao.java
@@ -6,14 +6,39 @@ import uk.ac.bbsrc.tgac.miso.core.data.Institute;
 
 public interface InstituteDao {
   
+  /**
+   * @return a list of all Institutes
+   */
   List<Institute> getInstitute();
   
+  /**
+   * Retrieve a single Institute by ID
+   * 
+   * @param id ID of the Institute to retrieve
+   * @return
+   */
   Institute getInstitute(Long id);
   
+  /**
+   * Save a new Institute
+   * 
+   * @param institute the institute to save
+   * @return the ID of the newly-saved Institute
+   */
   Long addInstitute(Institute institute);
   
+  /**
+   * Delete an Institute
+   * 
+   * @param institute the Institute to delete
+   */
   void deleteInstitute(Institute institute);
   
+  /**
+   * Save a modified Institute
+   * 
+   * @param institute the Institute to save
+   */
   void update(Institute institute);
   
 }
