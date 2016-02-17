814230d3930ffed9a6e675f1b55214a1f085b590
Andre Masella <andre.masella@oicr.on.ca>
2016-02-17 10:35:54 -0500

    Access all of the Group IDs associated with a Project or Subproject

--- miso-service/src/main/java/uk/ac/bbsrc/tgac/miso/service/SampleGroupService.java
+++ miso-service/src/main/java/uk/ac/bbsrc/tgac/miso/service/SampleGroupService.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Set;
 
 import uk.ac.bbsrc.tgac.miso.core.data.SampleGroupId;
+import uk.ac.bbsrc.tgac.miso.service.security.AuthorizationException;
 
 public interface SampleGroupService {
 
@@ -14,6 +15,10 @@ public interface SampleGroupService {
   void update(SampleGroupId sampleGroup) throws IOException;
 
   Set<SampleGroupId> getAll() throws IOException;
+
+  Set<SampleGroupId> getAllForProject(Long projectId) throws AuthorizationException, IOException;
+
+  Set<SampleGroupId> getAllForSubproject(Long subprojectId) throws AuthorizationException, IOException;
 
   void delete(Long sampleGroupId) throws IOException;
 
