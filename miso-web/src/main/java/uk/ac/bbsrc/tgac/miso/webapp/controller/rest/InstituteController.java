6432daacb936f78ed62a2db3007e67c98c9ab05e
Chris Salt <christopher.salt@tgac.ac.uk>
2016-02-24 12:49:31 +0000

    Fix missing import

--- miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/controller/rest/InstituteController.java
+++ miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/controller/rest/InstituteController.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.Response.Status;
 
+import org.hibernate.exception.ConstraintViolationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
