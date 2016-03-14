3baaf34bcc946cd50e47393d2b47c9b60512a206
Przemek Kowalczyk <przemek.kowalczyk@oicr.on.ca>
2016-03-14 13:52:01 -0400

    add reference genomes into table

---
+++ sqlstore/src/main/resources/db/migration/v8002_reference_genomes.sql
@@ -0,0 +1,17 @@
+INSERT INTO `ReferenceGenome` (`alias`) VALUES
+('Human hg19 random'),
+('Human hg19'),
+('Human hg18 random'),
+('Human hg18'),
+('Mouse mm9'),
+('Rat rn4'),
+('E. coli DH10B'),
+('phiX174'),
+('C. elegans ce6'),
+('C. pygerythrus'),
+('Vaccinia JX-594 (OICR)'),
+('P. falciparum'),
+('R. sphaeroides'),
+('S. aureus'),
+('de novo assemby'),
+('See comments');
