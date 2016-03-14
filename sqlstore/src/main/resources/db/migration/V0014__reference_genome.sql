8742d1fac2ca4b11f3086f84e56301428348251d
Przemek Kowalczyk <przemek.kowalczyk@oicr.on.ca>
2016-03-14 15:01:18 -0400

    move oicr specific reference genomes to micr branch, add default unknown value.

--- sqlstore/src/main/resources/db/migration/V0014__reference_genome.sql
+++ sqlstore/src/main/resources/db/migration/V0014__reference_genome.sql
@@ -3,26 +3,11 @@ CREATE TABLE `ReferenceGenome` (
   `alias` VARCHAR(255) NOT NULL UNIQUE,
   PRIMARY KEY (`referenceGenomeId`));
 
-INSERT INTO `ReferenceGenome` (`alias`) VALUES ( 'Human hg19 random');
-INSERT INTO `ReferenceGenome` (`alias`) VALUES ( 'Human hg19');
-INSERT INTO `ReferenceGenome` (`alias`) VALUES ( 'Human hg18 random');
-INSERT INTO `ReferenceGenome` (`alias`) VALUES ( 'Human hg18');
-INSERT INTO `ReferenceGenome` (`alias`) VALUES ( 'Mouse mm9');
-INSERT INTO `ReferenceGenome` (`alias`) VALUES ( 'Rat rn4');
-INSERT INTO `ReferenceGenome` (`alias`) VALUES ( 'E. coli DH10B');
-INSERT INTO `ReferenceGenome` (`alias`) VALUES ( 'phiX174');
-INSERT INTO `ReferenceGenome` (`alias`) VALUES ( 'C. elegans ce6');
-INSERT INTO `ReferenceGenome` (`alias`) VALUES ( 'C. pygerythrus');
-INSERT INTO `ReferenceGenome` (`alias`) VALUES ( 'Vaccinia JX-594 (OICR)');
-INSERT INTO `ReferenceGenome` (`alias`) VALUES ( 'P. falciparum');
-INSERT INTO `ReferenceGenome` (`alias`) VALUES ( 'R. sphaeroides');
-INSERT INTO `ReferenceGenome` (`alias`) VALUES ( 'S. aureus');
-INSERT INTO `ReferenceGenome` (`alias`) VALUES ( 'de novo assemby');
-INSERT INTO `ReferenceGenome` (`alias`) VALUES ( 'See comments');
+INSERT INTO `ReferenceGenome` ( `referenceGenomeId`, `alias`) VALUES (0,'Unknown');
 
-ALTER TABLE Project ADD COLUMN `referenceGenomeId` BIGINT (20) NOT NULL after alias;
+ALTER TABLE Project ADD COLUMN `referenceGenomeId` BIGINT (20) NOT NULL DEFAULT 0 after alias;
 
-ALTER TABLE Subproject ADD COLUMN `referenceGenomeId` BIGINT (20) NOT NULL after alias;
+ALTER TABLE Subproject ADD COLUMN `referenceGenomeId` BIGINT (20) NOT NULL DEFAULT 0 after alias;
 
 ALTER TABLE Project ADD FOREIGN KEY (referenceGenomeId) REFERENCES ReferenceGenome (referenceGenomeId);
 
