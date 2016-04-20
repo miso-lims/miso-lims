d2c748ff08156e762cb72e22374bdcd8f45c99f4
Andre Masella <andre.masella@oicr.on.ca>
2016-04-20 15:54:18 -0400

    Avoid all HTML special characters in field names

--- miso-web/src/main/webapp/scripts/lims.js
+++ miso-web/src/main/webapp/scripts/lims.js
@@ -232,7 +232,7 @@ Utils.fileUpload = {
 Utils.validation = {
   dateRegex: '^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[012])\/(19|20)[0-9]{2}$',
   dateTimeRegex: '^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[012])\/(19|20)[0-9]{2} ([01][0-9]|2[0-3]):[0-5][0-9]$',
-  sanitizeRegex: '[^<>\'\/]+',
+  sanitizeRegex: '[^<>\'\"\/&]+',
   unicodeWordRegex: '^[\\p{L}0-9_\\^\\-\\.\\s]+$',
   _unicodeWord: XRegExp('^[\\p{L}0-9_\\^\\-\\.\\s]+$'),
 
