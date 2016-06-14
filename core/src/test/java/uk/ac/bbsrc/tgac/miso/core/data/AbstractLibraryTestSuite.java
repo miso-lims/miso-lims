34deb84ae31ae71783b09d981d0203a7a3fa1add
Heather Armstrong <heather.armstrong@oicr.on.ca>
2016-06-14 16:00:16 -0400

    GLT-702 fix recursion: Lib<->LibAddInfo & TagBC<->TagBCFam

--- core/src/test/java/uk/ac/bbsrc/tgac/miso/core/data/AbstractLibraryTestSuite.java
+++ core/src/test/java/uk/ac/bbsrc/tgac/miso/core/data/AbstractLibraryTestSuite.java
@@ -1,9 +1,14 @@
 package uk.ac.bbsrc.tgac.miso.core.data;
 
+import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
 
+import org.codehaus.jackson.map.ObjectMapper;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -11,16 +16,24 @@ import org.mockito.Mockito;
 import com.eaglegenomics.simlims.core.SecurityProfile;
 import com.eaglegenomics.simlims.core.User;
 
+import uk.ac.bbsrc.tgac.miso.core.data.impl.LibraryAdditionalInfoImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.LibraryImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.SampleImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.TissueOriginImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.TissueTypeImpl;
 import uk.ac.bbsrc.tgac.miso.core.security.SecurableByProfile;
 
 public class AbstractLibraryTestSuite {
 
   AbstractLibrary al;
 
+  protected ObjectMapper mapper;
+
   @Before
   public void setUp() throws Exception {
     al = new AbstractLibrary() {
     };
+    mapper = new ObjectMapper();
   }
 
   @Test
@@ -44,9 +57,18 @@ public class AbstractLibraryTestSuite {
     al.inheritPermissions(parent);
   }
 
-  // TODO - cs. This exception is never thrown! Remove it or add exception.
-  // @Test(expected = MalformedLibraryException.class)
-  // public final void testaddQc() {
-  //
-  // }
+  @Test
+  public final void testLibrarySerialization()  throws Exception {
+    Sample sample = new SampleImpl();
+    Library library = new LibraryImpl();
+    LibraryAdditionalInfo lai = new LibraryAdditionalInfoImpl();
+    library.setSample(sample);
+    lai.setTissueOrigin(new TissueOriginImpl());
+    lai.setTissueType(new TissueTypeImpl());
+    lai.setLibrary(library);
+    library.setLibraryAdditionalInfo(lai);
+    library.setAlias("TestLib");
+    String mappedLib = mapper.writer().writeValueAsString(library);
+    assertThat(mappedLib, containsString("TestLib"));
+  }
 }
