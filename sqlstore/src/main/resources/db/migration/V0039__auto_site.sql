1f87fc02f09ab7d2c9f82f52c43d93eca49eee6b
Tony DeBat <tony.debat@oicr.on.ca>
2016-08-16 15:21:43 -0400

    Cut new release

--- sqlstore/src/main/resources/db/migration/V0039__auto_site.sql
+++ sqlstore/src/main/resources/db/migration/V0039__auto_site.sql
@@ -1,3 +1,5 @@
+-- EPIC-TCR_barcodes
+--StartNoTest
 INSERT INTO TagBarcodeFamily (name, platformType) VALUES ('EPIC-TCR Dual Index', 'ILLUMINA');
 
 INSERT INTO TagBarcodes (name, sequence, position, tagFamilyId)
@@ -9,3 +11,17 @@ INSERT INTO TagBarcodes (name, sequence, position, tagFamilyId)
 SELECT tb.name, tb.sequence, 2, (SELECT tagFamilyId FROM TagBarcodeFamily WHERE name = 'EPIC-TCR Dual Index')
 FROM TagBarcodes tb JOIN TagBarcodeFamily fam ON fam.tagFamilyId = tb.tagFamilyId
 WHERE fam.name = 'TCRindex2';
+--EndNoTest
+
+-- add_kits
+--StartNoTest
+INSERT INTO KitDescriptor(name, version, manufacturer, partNumber, stockLevel, kitType, platformType, description, lastModifier)
+SELECT 'KAPA OnBead', 0, 'KAPA', 1, 0, 'Library', 'Illumina', 'n/a', 1
+FROM DUAL
+WHERE NOT EXISTS (SELECT 1 FROM KitDescriptor WHERE name = 'KAPA OnBead');
+
+INSERT INTO KitDescriptor(name, version, manufacturer, partNumber, stockLevel, kitType, platformType, description, lastModifier)
+VALUES ('CRISPR Seq Prep', 0, 'CRISPR', 1, 0, 'Library', 'Illumina', 'n/a', 1),
+('Nextera XT', 0, 'Nextera', 1, 0, 'Library', 'Illumina', 'n/a', 1);
+--EndNoTest
+
--- sqlstore/src/main/resources/db/migration/V8015__add_kits.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO KitDescriptor(name, version, manufacturer, partNumber, stockLevel, kitType, platformType, description, lastModifier)
-SELECT 'KAPA OnBead', 0, 'KAPA', 1, 0, 'Library', 'Illumina', 'n/a', 1
-FROM DUAL
-WHERE NOT EXISTS (SELECT 1 FROM KitDescriptor WHERE name = 'KAPA OnBead');
-
-INSERT INTO KitDescriptor(name, version, manufacturer, partNumber, stockLevel, kitType, platformType, description, lastModifier)
-VALUES ('CRISPR Seq Prep', 0, 'CRISPR', 1, 0, 'Library', 'Illumina', 'n/a', 1),
-('Nextera XT', 0, 'Nextera', 1, 0, 'Library', 'Illumina', 'n/a', 1);
