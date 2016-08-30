0956098a02aeb1e601a029b72f438fbc1e6dc0b7
Heather Armstrong <heather.armstrong@oicr.on.ca>
2016-08-30 14:46:41 -0400

    Cut new release

--- sqlstore/src/main/resources/db/migration/V8019__nonstandard_alias.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `SampleAdditionalInfo` ADD COLUMN nonStandardAlias BOOLEAN NOT NULL DEFAULT false;
-ALTER TABLE `LibraryAdditionalInfo` ADD COLUMN nonStandardAlias BOOLEAN NOT NULL DEFAULT false;
