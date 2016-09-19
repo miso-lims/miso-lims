a645fe1566ff8258abad1e634fc95fcbc15e6531
Andre Masella <andre@masella.name>
2016-09-19 10:11:01 -0400

    Allow updating sample identification barcode from HoT (GLT-1122) (#430)

--- miso-service/src/main/java/uk/ac/bbsrc/tgac/miso/service/impl/DefaultSampleService.java
+++ miso-service/src/main/java/uk/ac/bbsrc/tgac/miso/service/impl/DefaultSampleService.java
@@ -509,6 +509,7 @@ public class DefaultSampleService implements SampleService {
     target.setDescription(source.getDescription());
     target.setEmpty(source.isEmpty());
     target.setVolume(source.getVolume());
+    target.setIdentificationBarcode(source.getIdentificationBarcode());
     if (isDetailedSample(target)) {
       sampleAdditionalInfoService.applyChanges((SampleAdditionalInfo) target, (SampleAdditionalInfo) source);
       if (isIdentitySample(target)) {
