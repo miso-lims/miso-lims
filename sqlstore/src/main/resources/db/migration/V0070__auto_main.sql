a11ff24a8cd10d30456e28ca7757123e4a45ca43
Andre Masella <andre.masella@oicr.on.ca>
2016-09-20 11:07:20 -0400

    Cut new release

--- sqlstore/src/main/resources/db/migration/V0070__auto_main.sql
+++ sqlstore/src/main/resources/db/migration/V0070__auto_main.sql
@@ -1,3 +1,12 @@
+-- box_position_unique
+
+ALTER TABLE Library ADD CONSTRAINT library_boxPositionId_unique UNIQUE (boxPositionId);
+ALTER TABLE Pool ADD CONSTRAINT pool_boxPositionId_unique UNIQUE (boxPositionId);
+ALTER TABLE Sample ADD CONSTRAINT sample_boxPositionId_unique UNIQUE (boxPositionId);
+
+
+-- small_changes
+
 -- GLT-1133 Add PacBio RS II
 INSERT INTO Platform (name, instrumentModel, description, numContainers) VALUES ('PacBio', 'PacBio RS II', '', 1), ('PacBio', 'Sequel', '', 1);
 
@@ -5,3 +14,5 @@ INSERT INTO Platform (name, instrumentModel, description, numContainers) VALUES 
 UPDATE Indices SET position = 2 WHERE name LIKE 'S5%';
 UPDATE IndexFamily SET name = 'Nextera XT Dual Index' WHERE name = 'Nextera';
 UPDATE IndexFamily SET name = 'Nextera DNA Dual Index' WHERE name = 'Nextera Dual Index';
+
+
--- sqlstore/src/main/resources/db/migration/V8001__box_position_unique.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE Library ADD CONSTRAINT library_boxPositionId_unique UNIQUE (boxPositionId);
-ALTER TABLE Pool ADD CONSTRAINT pool_boxPositionId_unique UNIQUE (boxPositionId);
-ALTER TABLE Sample ADD CONSTRAINT sample_boxPositionId_unique UNIQUE (boxPositionId);
