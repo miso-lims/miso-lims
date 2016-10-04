c93c34eb2624d71d5a173190b4081397c375afa7
Heather Armstrong <heather.armstrong@oicr.on.ca>
2016-10-04 14:51:48 -0400

    Cut new release

--- sqlstore/src/main/resources/db/migration/V0090__auto_main.sql
+++ sqlstore/src/main/resources/db/migration/V0090__auto_main.sql
@@ -1,3 +1,5 @@
+-- missing_nextflex_12bp
+
 INSERT INTO Indices(indexFamilyId, position, name, sequence) VALUES
   ((SELECT indexFamilyId FROM IndexFamily WHERE name = 'NEXTflex 12bp'), 1, 'Index 193', 'CTGATATGCTGG'),
   ((SELECT indexFamilyId FROM IndexFamily WHERE name = 'NEXTflex 12bp'), 1, 'Index 194', 'TCCTCTCAGCAA'),
@@ -191,3 +193,61 @@ INSERT INTO Indices(indexFamilyId, position, name, sequence) VALUES
   ((SELECT indexFamilyId FROM IndexFamily WHERE name = 'NEXTflex 12bp'), 1, 'Index 382', 'ACTTTGGTTGCC'),
   ((SELECT indexFamilyId FROM IndexFamily WHERE name = 'NEXTflex 12bp'), 1, 'Index 383', 'AACTGCATGCGG'),
   ((SELECT indexFamilyId FROM IndexFamily WHERE name = 'NEXTflex 12bp'), 1, 'Index 384', 'GGGAAGGCCTAA');
+
+
+-- pre-migration_ids
+
+ALTER TABLE DetailedSample ADD COLUMN preMigrationId bigint(20);
+ALTER TABLE DetailedSample ADD CONSTRAINT sample_preMigrationId UNIQUE (premigrationId);
+ALTER TABLE LibraryAdditionalInfo ADD COLUMN preMigrationId bigint(20);
+ALTER TABLE LibraryAdditionalInfo ADD CONSTRAINT library_preMigrationId UNIQUE (premigrationId);
+ALTER TABLE LibraryDilution ADD COLUMN preMigrationId bigint(20);
+ALTER TABLE LibraryDilution ADD CONSTRAINT dilution_preMigrationId UNIQUE (premigrationId);
+
+
+-- qcPassedDetails_to_detailedQcStatus
+
+-- add basic defaults
+
+INSERT INTO QcPassedDetail (status, description, noteRequired, createdBy, creationDate, updatedBy, lastUpdated) 
+  SELECT TRUE, 'Ready', FALSE, (SELECT userId FROM User WHERE loginName = 'admin'), NOW(), (SELECT userId FROM User WHERE loginName = 'admin'), NOW() 
+  FROM DUAL
+  WHERE NOT EXISTS (SELECT description FROM QcPassedDetail WHERE description = 'Ready' LIMIT 1);
+INSERT INTO QcPassedDetail (status, description, noteRequired, createdBy, creationDate, updatedBy, lastUpdated) 
+  SELECT FALSE, 'Failed: QC', FALSE, (SELECT userId FROM User WHERE loginName = 'admin'), NOW(), (SELECT userId FROM User WHERE loginName = 'admin'), NOW() 
+  FROM DUAL
+  WHERE NOT EXISTS (SELECT description FROM QcPassedDetail WHERE description = 'Failed: QC' LIMIT 1);
+
+-- remove unnecessary 'Not Ready' option, as it will be represented by NULL.
+ALTER TABLE DetailedSample DROP FOREIGN KEY `FKa2t38wms0eer896xo4fw76tw0`;
+UPDATE DetailedSample SET qcPassedDetailId = NULL WHERE qcPassedDetailId = (
+  SELECT qcPassedDetailId FROM QcPassedDetail WHERE description = 'Not Ready'
+);
+DELETE FROM QcPassedDetail WHERE description = 'Not Ready';
+UPDATE QcPassedDetail SET noteRequired = false WHERE description IN ('Waiting: Path Report', 'Failed: QC'); 
+
+-- add a detailedQcStatusId value for all samples since this is now required for detailed samples
+UPDATE DetailedSample ds
+  SET ds.qcPassedDetailId = (SELECT qcPassedDetailId FROM QcPassedDetail WHERE description = 'Ready')
+  WHERE ds.sampleId IN (
+    SELECT s.sampleId FROM Sample s
+    WHERE s.qcPassed = true);
+UPDATE DetailedSample ds
+  SET ds.qcPassedDetailId = NULL
+  WHERE ds.sampleId IN (
+    SELECT s.sampleId FROM Sample s
+    WHERE s.qcPassed IS NULL);
+UPDATE DetailedSample ds
+  SET ds.qcPassedDetailId = (SELECT qcPassedDetailId FROM QcPassedDetail WHERE description = 'Failed: QC')
+  WHERE ds.sampleId IN (
+    SELECT s.sampleId FROM Sample s
+    WHERE s.qcPassed = false);
+
+ALTER TABLE DetailedSample ADD COLUMN detailedQcStatusNote VARCHAR(500) DEFAULT NULL;
+
+ALTER TABLE QcPassedDetail RENAME TO DetailedQcStatus;
+ALTER TABLE DetailedQcStatus CHANGE COLUMN qcPassedDetailId detailedQcStatusId BIGINT(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE DetailedSample CHANGE COLUMN qcPassedDetailId detailedQcStatusId BIGINT(20) DEFAULT NULL;
+ALTER TABLE DetailedSample ADD CONSTRAINT `FK_detailedQcStatus` FOREIGN KEY (detailedQcStatusId) REFERENCES DetailedQcStatus (detailedQcStatusId);
+
+
--- sqlstore/src/main/resources/db/migration/V8001__pre-migration_ids.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE DetailedSample ADD COLUMN preMigrationId bigint(20);
-ALTER TABLE DetailedSample ADD CONSTRAINT sample_preMigrationId UNIQUE (premigrationId);
-ALTER TABLE LibraryAdditionalInfo ADD COLUMN preMigrationId bigint(20);
-ALTER TABLE LibraryAdditionalInfo ADD CONSTRAINT library_preMigrationId UNIQUE (premigrationId);
-ALTER TABLE LibraryDilution ADD COLUMN preMigrationId bigint(20);
-ALTER TABLE LibraryDilution ADD CONSTRAINT dilution_preMigrationId UNIQUE (premigrationId);
