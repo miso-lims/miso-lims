7b34f0785fc7fd5d7ae79656e30d3ba43f59c4c2
Dillan Cooke <dillan.cooke@oicr.on.ca>
2016-10-05 17:11:25 -0400

    add/revise unique constraints for value types

---
+++ sqlstore/src/main/resources/db/migration/V8500__value_type_unique_constraints.sql
@@ -0,0 +1,13 @@
+ALTER TABLE Indices ADD CONSTRAINT uk_index UNIQUE (indexFamilyId, position, sequence);
+ALTER TABLE KitDescriptor ADD CONSTRAINT uk_kitName UNIQUE (name);
+ALTER TABLE LibrarySelectionType ADD CONSTRAINT uk_librarySelectionType_name UNIQUE (name);
+ALTER TABLE LibraryStrategyType ADD CONSTRAINT uk_libraryStrategyType_name UNIQUE (name);
+ALTER TABLE LibraryType ADD CONSTRAINT uk_libraryType_byPlatform UNIQUE (platformType, description);
+ALTER TABLE LibraryDesign MODIFY name varchar(255) NOT NULL;
+ALTER TABLE LibraryDesign ADD CONSTRAINT uk_libraryDesign_name UNIQUE (name);
+ALTER TABLE QCType ADD CONSTRAINT uk_qcType_byTarget UNIQUE (qcTarget, name);
+ALTER TABLE SequencerReference ADD CONSTRAINT uk_sequencer_name UNIQUE (name);
+DROP INDEX UKd5487vldy3xo0x7iw6vmspvpf ON DetailedQcStatus;
+ALTER TABLE DetailedQcStatus ADD CONSTRAINT uk_detailedQcStatus_description UNIQUE (description);
+DROP INDEX UKdewdpl9hfwp6plc9gln8rtcx5 ON SampleClass;
+ALTER TABLE SampleClass ADD CONSTRAINT uk_sampleClass_alias UNIQUE (alias);
