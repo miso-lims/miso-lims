a9129e170d5109f0dde885061b2295214507e81d
Tony DeBat <tony.debat@oicr.on.ca>
2016-10-11 15:39:02 -0400

    Cut new release

--- sqlstore/src/main/resources/db/migration/V8001__trash_to_discard.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE Pool CHANGE COLUMN emptied discarded tinyint(1) NOT NULL DEFAULT '0';
-ALTER TABLE Library CHANGE COLUMN emptied discarded tinyint(1) NOT NULL DEFAULT '0';
-ALTER TABLE Sample CHANGE COLUMN emptied discarded tinyint(1) NOT NULL DEFAULT '0';
