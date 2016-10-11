cd2aca3c0e00e57f4d95aeae78f80d8b72f8f6da
djcooke <dillan.cooke@oicr.on.ca>
2016-10-11 15:32:05 -0400

    undo kitDescriptor unique constraint (#485)

--- sqlstore/src/main/resources/db/migration/V8500__value_type_unique_constraints.sql
+++ sqlstore/src/main/resources/db/migration/V8500__value_type_unique_constraints.sql
@@ -1,5 +1,4 @@
 ALTER TABLE Indices ADD CONSTRAINT uk_index UNIQUE (indexFamilyId, position, sequence);
-ALTER TABLE KitDescriptor ADD CONSTRAINT uk_kitName UNIQUE (name);
 ALTER TABLE LibrarySelectionType ADD CONSTRAINT uk_librarySelectionType_name UNIQUE (name);
 ALTER TABLE LibraryStrategyType ADD CONSTRAINT uk_libraryStrategyType_name UNIQUE (name);
 ALTER TABLE LibraryType ADD CONSTRAINT uk_libraryType_byPlatform UNIQUE (platformType, description);
@@ -7,7 +6,9 @@ ALTER TABLE LibraryDesign MODIFY name varchar(255) NOT NULL;
 ALTER TABLE LibraryDesign ADD CONSTRAINT uk_libraryDesign_name UNIQUE (name);
 ALTER TABLE QCType ADD CONSTRAINT uk_qcType_byTarget UNIQUE (qcTarget, name);
 ALTER TABLE SequencerReference ADD CONSTRAINT uk_sequencer_name UNIQUE (name);
+--StartNoTest
 DROP INDEX UKd5487vldy3xo0x7iw6vmspvpf ON DetailedQcStatus;
-ALTER TABLE DetailedQcStatus ADD CONSTRAINT uk_detailedQcStatus_description UNIQUE (description);
 DROP INDEX UKdewdpl9hfwp6plc9gln8rtcx5 ON SampleClass;
+--EndNoTest
+ALTER TABLE DetailedQcStatus ADD CONSTRAINT uk_detailedQcStatus_description UNIQUE (description);
 ALTER TABLE SampleClass ADD CONSTRAINT uk_sampleClass_alias UNIQUE (alias);
