e704f77127713c4e15d11b5be9d0fd9beb0286e7
Andre Masella <andre@masella.name>
2016-10-18 13:34:05 -0400

    Add NEO kit (#103)
    * Add NEO kit
    
    * Fixup

---
+++ sqlstore/src/main/resources/db/migration/V8800__neo_mr_kit.sql
@@ -0,0 +1,3 @@
+INSERT INTO KitDescriptor(name, version, manufacturer, partNumber, stockLevel, kitType, platformType, description, lastModifier)
+VALUES ('Neo MR', 0, 'Illumina', 1, 0, 'Library', 'Illumina', 'NeoPrep mRNA library preparation', 1);
+
