a594fd98ec507e9f0261780a431191e2a47480ae
Andre Masella <andre.masella@oicr.on.ca>
2016-10-24 17:02:19 -0400

    Add GA_TruSeq_Library kit

---
+++ sqlstore/src/main/resources/db/migration/V8800__ga_truseq_lib_kit.sql
@@ -0,0 +1,2 @@
+INSERT INTO KitDescriptor(name, version, manufacturer, partNumber, stockLevel, kitType, platformType, description, lastModifier)
+ VALUES ('GA_TruSeq_Library', 0, 'Illumina', 1, 0, 'Library', 'Illumina', 'TruSeq Genomic Library prep kit', 1);
