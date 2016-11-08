572d0fca2fc3abf6ae7300e8a7febf3207b74129
Tony DeBat <tony.debat@oicr.on.ca>
2016-11-08 15:56:57 -0500

    Merge TGAC to OICR.

--- sqlstore/src/main/resources/db/migration/V0131__auto_site.sql
+++ sqlstore/src/main/resources/db/migration/V0131__auto_site.sql
@@ -1,3 +1,21 @@
+-- lockdown_aml
+--StartNoTest
+--StartNoTest
+SET @time = NOW();
+SELECT userId INTO @user FROM User WHERE loginName = 'admin';
+
+INSERT INTO KitDescriptor (name, version, manufacturer, partNumber, stockLevel, kitType, platformType, description, lastModifier) VALUES
+('KAPA Hyper Prep & SureSelect XT2 v5', 5, 'KAPA', 1, 0, 'Library', 'Illumina', NULL, @user);
+
+SELECT kitDescriptorId INTO @kapa FROM KitDescriptor WHERE name = "KAPA Hyper Prep & SureSelect XT2 v5";
+
+INSERT INTO TargetedResequencing (alias, description, createdBy, creationDate, updatedBy, lastUpdated, kitDescriptorId) VALUES
+('xGen Lockdown AML', 'xGen Lockdown AML', @user, @time, @user, @time, @kapa);
+--EndNoTest
+--EndNoTest
+
+-- add_more_libraryDesigns
+--StartNoTest
 --StartNoTest
 INSERT INTO LibraryDesignCode (code, description) VALUES ("TR", "TR");
 
@@ -13,3 +31,38 @@ INSERT INTO LibraryDesign (name, librarySelectionType, libraryStrategyType, libr
   ("WT", (SELECT librarySelectionTypeId FROM LibrarySelectionType WHERE name = "cDNA"), (SELECT libraryStrategyTypeId FROM LibraryStrategyType WHERE name = "RNA-Seq"), (SELECT libraryDesignCodeId FROM LibraryDesignCode WHERE code = "WT"), (SELECT sampleClassId FROM SampleClass WHERE alias = "rRNA_depleted")),
   ("TR", (SELECT librarySelectionTypeId FROM LibrarySelectionType WHERE name = "cDNA"), (SELECT libraryStrategyTypeId FROM LibraryStrategyType WHERE name = "RNA-Seq"), (SELECT libraryDesignCodeId FROM LibraryDesignCode WHERE code = "TR"), (SELECT sampleClassId FROM SampleClass WHERE alias = "whole RNA (aliquot)"));
 --EndNoTest
+--EndNoTest
+
+-- misc_values
+--StartNoTest
+--StartNoTest
+SET @time = NOW();
+SELECT userId INTO @user FROM User WHERE loginName = 'admin';
+
+INSERT INTO SamplePurpose (alias, createdBy, creationDate, updatedBy, lastUpdated)
+VALUES ('Ion Torrent', @user, @time, @user, @time);
+
+UPDATE Lab SET instituteId = (SELECT instituteId FROM Institute WHERE alias = 'Ottawa Hospital Research Institute')
+WHERE alias = 'John Bell';
+
+INSERT INTO Institute (alias, createdBy, creationDate, updatedBy, lastUpdated)
+VALUES ('Sick Kids', @user, @time, @user, @time);
+SET @institute = LAST_INSERT_ID();
+
+INSERT INTO Lab (instituteId, alias, createdBy, creationDate, updatedBy, lastUpdated)
+VALUES (@institute, 'Not Specified', @user, @time, @user, @time);
+
+INSERT INTO Indices (name, sequence, position, indexFamilyId)
+VALUES ('S517', 'GCGTAAGA', 2, (SELECT indexFamilyId FROM IndexFamily WHERE name = 'Nextera XT Dual Index'));
+--EndNoTest
+--EndNoTest
+
+-- london_hs
+--StartNoTest
+--StartNoTest
+SELECT userId INTO @admin FROM User WHERE loginName = 'admin';
+INSERT INTO Institute(alias, createdBy, creationDate, updatedBy, lastUpdated) VALUES ('London Health Sciences Center', @admin, CURRENT_TIMESTAMP, @admin, CURRENT_TIMESTAMP);
+INSERT INTO Lab(instituteId, alias, createdBy, creationDate, updatedBy, lastUpdated) VALUES ((SELECT instituteId FROM Institute WHERE alias = 'London Health Sciences Center'), 'Not Specified', @admin, CURRENT_TIMESTAMP, @admin, CURRENT_TIMESTAMP);
+--EndNoTest
+--EndNoTest
+
