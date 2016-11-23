9ab2c4a7014cdc73a52f79c9385f633dc41cb51d
Dillan Cooke <dillan.cooke@oicr.on.ca>
2016-11-23 17:26:18 -0500

    Cut new release

--- sqlstore/src/main/resources/db/migration/V0142__auto_site.sql
+++ sqlstore/src/main/resources/db/migration/V0142__auto_site.sql
@@ -1,3 +1,5 @@
+-- libraryDesignCode_descriptions
+-- StartNoTest
 --StartNoTest
 UPDATE LibraryDesignCode SET description = 'ATAC-Seq' WHERE code = 'AS';
 UPDATE LibraryDesignCode SET description = 'ChIP-Seq' WHERE code = 'CH';
@@ -9,3 +11,11 @@ UPDATE LibraryDesignCode SET description = 'Whole Genome' WHERE code = 'WG';
 UPDATE LibraryDesignCode SET description = 'Whole Transcriptome' WHERE code = 'WT';
 UPDATE LibraryDesignCode SET description = 'Total RNA' WHERE code = 'TR';
 --EndNoTest
+-- EndNoTest
+
+-- stein_lab
+-- StartNoTest
+SELECT userId INTO @admin FROM User WHERE loginName = 'admin';
+INSERT INTO Lab(instituteId, alias, createdBy, creationDate, updatedBy, lastUpdated) VALUES ((SELECT instituteId FROM Institute WHERE alias = 'Ontario Institute for Cancer Research'), 'Lincoln Stein', @admin, CURRENT_TIMESTAMP, @admin, CURRENT_TIMESTAMP);
+-- EndNoTest
+
