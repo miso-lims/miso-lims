a068b248b692d83bf97d31783f453c29296fddfb
Heather Armstrong <heather.armstrong@oicr.on.ca>
2016-12-01 16:11:01 -0500

    boilerplate

--- sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateAlertDao.java
+++ sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateAlertDao.java
@@ -1,3 +1,26 @@
+/*
+ * Copyright (c) 2012. The Genome Analysis Centre, Norwich, UK
+ * MISO project contacts: Robert Davey, Mario Caccamo @ TGAC
+ * *********************************************************************
+ *
+ * This file is part of MISO.
+ *
+ * MISO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MISO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MISO. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * *********************************************************************
+ */
+
 package uk.ac.bbsrc.tgac.miso.persistence.impl;
 
 import java.io.IOException;
@@ -24,6 +47,13 @@ import uk.ac.bbsrc.tgac.miso.core.event.impl.DefaultAlert;
 import uk.ac.bbsrc.tgac.miso.core.event.type.AlertLevel;
 import uk.ac.bbsrc.tgac.miso.core.store.AlertStore;
 import uk.ac.bbsrc.tgac.miso.core.util.CoverageIgnore;
+
+/**
+ * uk.ac.bbsrc.tgac.miso.hibernate.persistence.impl
+ *
+ * @author Heather Armstrong
+ * @since 0.2.43
+ */
 
 @Transactional(rollbackFor = Exception.class)
 public class HibernateAlertDao implements AlertStore {
