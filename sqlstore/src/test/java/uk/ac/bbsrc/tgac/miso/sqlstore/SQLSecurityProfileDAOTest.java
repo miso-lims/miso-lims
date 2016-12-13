a18d99a8ab087f05e6ab5bbc69e0960bab7be2a4
Andre Masella <andre.masella@oicr.on.ca>
2016-12-13 15:42:49 -0500

    Convert SecurityProfile DAO to Hibernate
    JIRA: GLT-1291

--- sqlstore/src/test/java/uk/ac/bbsrc/tgac/miso/sqlstore/SQLSecurityProfileDAOTest.java
+++ sqlstore/src/test/java/uk/ac/bbsrc/tgac/miso/sqlstore/SQLSecurityProfileDAOTest.java
@@ -3,11 +3,9 @@ package uk.ac.bbsrc.tgac.miso.sqlstore;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
 
+import org.hibernate.SessionFactory;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -21,10 +19,10 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import com.eaglegenomics.simlims.core.Group;
 import com.eaglegenomics.simlims.core.SecurityProfile;
 import com.eaglegenomics.simlims.core.User;
-import com.eaglegenomics.simlims.core.manager.SecurityManager;
 
 import uk.ac.bbsrc.tgac.miso.AbstractDAOTest;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.UserImpl;
+import uk.ac.bbsrc.tgac.miso.persistence.impl.HibernateSecurityProfileDao;
 
 public class SQLSecurityProfileDAOTest extends AbstractDAOTest {
 
@@ -43,105 +41,16 @@ public class SQLSecurityProfileDAOTest extends AbstractDAOTest {
   @Spy
   private JdbcTemplate jdbcTemplate;
 
-  private final SecurityManager securityManager = new SecurityManager() {
-
-    @Override
-    public long saveUser(User arg0) throws IOException {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public long saveGroup(Group arg0) throws IOException {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public Collection<User> listUsersByIds(Collection<Long> ids) throws IOException {
-      List<User> users = new ArrayList<>();
-      for (long id : ids) {
-        users.add(getUserById(id));
-      }
-
-      return users;
-    }
-
-    @Override
-    public Collection<User> listUsersByGroupName(String arg0) throws IOException {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public Collection<Group> listGroupsByIds(Collection<Long> ids) throws IOException {
-      List<Group> groups = new ArrayList<>();
-      for (long id : ids) {
-        if (id == 2) {
-          groups.add(GROUP);
-        } else {
-          throw new UnsupportedOperationException();
-        }
-      }
-      return groups;
-    }
-
-    @Override
-    public Collection<User> listAllUsers() throws IOException {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public Collection<Group> listAllGroups() throws IOException {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public User getUserByLoginName(String arg0) throws IOException {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public User getUserById(Long id) throws IOException {
-      if (id == null) {
-        return null;
-      }
-      User u = new UserImpl();
-      u.setUserId(id);
-      return u;
-    }
-
-    @Override
-    public User getUserByEmail(String arg0) throws IOException {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public SecurityProfile getSecurityProfileById(Long arg0) throws IOException {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public Group getGroupByName(String arg0) throws IOException {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public Group getGroupById(Long arg0) throws IOException {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public Collection<String> getDefaultRoles() {
-      throw new UnsupportedOperationException();
-    }
-  };
+  @Autowired
+  private SessionFactory sessionFactory;
 
   @InjectMocks
-  private SQLSecurityProfileDAO dao;
+  private HibernateSecurityProfileDao dao;
 
   @Before
   public void setUp() throws IOException {
     MockitoAnnotations.initMocks(this);
-    dao.setJdbcTemplate(jdbcTemplate);
-    dao.setSecurityManager(securityManager);
+    dao.setSessionFactory(sessionFactory);
   }
 
   @Test
