5651c482fd03c4a9702e4e2f59936d805c8ddc44
Heather Armstrong <heather.armstrong@oicr.on.ca>
2017-01-31 16:54:56 -0500

    remove vestigial watchManager

--- sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateRunDao.java
+++ sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateRunDao.java
@@ -313,14 +313,14 @@ public class HibernateRunDao implements RunStore {
 
   @Override
   public void addWatcher(Run run, User watcher) {
-    log.debug("Adding watcher " + watcher.getLoginName() + " to " + run.getName() + " via WatchManager");
+    log.debug("Adding watcher " + watcher.getLoginName() + " to " + run.getName());
     run.addWatcher(watcher);
     currentSession().update(run);
   }
 
   @Override
   public void removeWatcher(Run run, User watcher) {
-    log.debug("Removing watcher " + watcher.getLoginName() + " from " + run.getWatchableIdentifier() + " via WatchManager");
+    log.debug("Removing watcher " + watcher.getLoginName() + " from " + run.getWatchableIdentifier());
     run.removeWatcher(watcher);
     currentSession().update(run);
   }
