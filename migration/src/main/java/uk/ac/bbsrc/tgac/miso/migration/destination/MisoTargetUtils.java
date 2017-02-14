f4b4176e399f77e181d09149171792d1c7ad6e41
Heather Armstrong <heather.armstrong@oicr.on.ca>
2017-02-14 16:44:13 -0500

    fixup remove commented out bits

--- migration/src/main/java/uk/ac/bbsrc/tgac/miso/migration/destination/MisoTargetUtils.java
+++ migration/src/main/java/uk/ac/bbsrc/tgac/miso/migration/destination/MisoTargetUtils.java
@@ -32,49 +32,8 @@ public class MisoTargetUtils {
   public static SessionFactory makeSessionFactory(DataSource datasource) throws IOException {
     LocalSessionFactoryBean bean = new LocalSessionFactoryBean();
     bean.setDataSource(datasource);
-    // TODO: use setPackagesToScan instead after fixing annotations
     bean.setPackagesToScan("uk.ac.bbsrc.tgac.miso.core.data", "com.eaglegenomics.simlims.core");
-    // bean.setAnnotatedClasses(
-    // new Class[] {
-    // TissueOriginImpl.class,
-    // TissueTypeImpl.class,
-    // SampleClassImpl.class,
-    // SubprojectImpl.class,
-    // DetailedQcStatusImpl.class,
-    // SamplePurposeImpl.class,
-    // SampleGroupImpl.class,
-    // TissueMaterialImpl.class,
-    // SampleAliquotImpl.class,
-    // SampleStockImpl.class,
-    // UserImpl.class,
-    // AbstractSample.class,
-    // IdentityImpl.class,
-    // AbstractProject.class,
-    // PoolOrderImpl.class,
-    // PoolOrderCompletion.class,
-    // ProjectImpl.class,
-    // SampleImpl.class,
-    // SampleDerivedInfo.class,
-    // DetailedSampleImpl.class,
-    // KitDescriptor.class,
-    // SampleValidRelationshipImpl.class,
-    // SampleNumberPerProjectImpl.class,
-    // SampleTissueImpl.class,
-    // SampleTissueProcessing.class,
-    // SampleCVSlideImpl.class,
-    // SampleLCMTubeImpl.class,
-    // SequencingParametersImpl.class,
-    // InstituteImpl.class,
-    // LabImpl.class,
-    // ReferenceGenomeImpl.class,
-    // AbstractLibrary.class,
-    // DetailedLibraryImpl.class,
-    // LibraryDesign.class,
-    // LibraryDesignCode.class,
-    // LibraryType.class,
-    // Index.class,
-    // IndexFamily.class
-    // });
+
     Properties properties = new Properties();
     properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
     properties.setProperty("hibernate.show_sql", "false");
