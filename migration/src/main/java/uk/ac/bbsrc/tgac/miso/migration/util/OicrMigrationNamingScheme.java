1f333217237ff95403427f900ad7b0fedb477e58
djcooke <dillan.cooke@oicr.on.ca>
2017-03-09 13:53:48 -0500

    GLT-850: attempt to generate non-standard migration aliases (#744)

--- migration/src/main/java/uk/ac/bbsrc/tgac/miso/migration/util/OicrMigrationNamingScheme.java
+++ migration/src/main/java/uk/ac/bbsrc/tgac/miso/migration/util/OicrMigrationNamingScheme.java
@@ -1,17 +1,29 @@
 package uk.ac.bbsrc.tgac.miso.migration.util;
 
+import uk.ac.bbsrc.tgac.miso.core.data.DetailedSample;
+import uk.ac.bbsrc.tgac.miso.core.data.Sample;
+import uk.ac.bbsrc.tgac.miso.core.exception.MisoNamingException;
 import uk.ac.bbsrc.tgac.miso.core.service.naming.OicrNamingScheme;
+import uk.ac.bbsrc.tgac.miso.core.service.naming.generation.NameGenerator;
+import uk.ac.bbsrc.tgac.miso.core.service.naming.generation.OicrSampleAliasGenerator;
 import uk.ac.bbsrc.tgac.miso.core.service.naming.validation.NameValidator;
 import uk.ac.bbsrc.tgac.miso.core.service.naming.validation.OicrSampleAliasValidator;
 import uk.ac.bbsrc.tgac.miso.core.service.naming.validation.ValidationResult;
+import uk.ac.bbsrc.tgac.miso.core.util.LimsUtils;
 
 public class OicrMigrationNamingScheme extends OicrNamingScheme {
 
   private final NameValidator sampleAliasValidator = new OicrMigrationSampleAliasValidator();
+  private final NameGenerator<Sample> sampleAliasGenerator = new OicrMigrationSampleAliasGenerator();
 
   @Override
   protected NameValidator getSampleAliasValidator() {
     return sampleAliasValidator;
+  }
+
+  @Override
+  protected NameGenerator<Sample> getSampleAliasGenerator() {
+    return sampleAliasGenerator;
   }
 
   private static class OicrMigrationSampleAliasValidator extends OicrSampleAliasValidator {
@@ -40,6 +52,44 @@ public class OicrMigrationNamingScheme extends OicrNamingScheme {
       } else {
         return ValidationResult.failed("Invalid sample alias: " + value);
       }
+    }
+
+  }
+
+  private static class OicrMigrationSampleAliasGenerator extends OicrSampleAliasGenerator {
+
+    @Override
+    public String generate(Sample sample) throws MisoNamingException {
+      // attempt to ignore up to two levels of non-standard aliases. Since the child-portion of the alias is appended
+      // to the parent's or grandparent's alias in most cases, this will allow the generation of non-standard aliases
+      // when there are non-standard aliases in the hierarchy
+
+      int changed = 0;
+      DetailedSample detailed = null;
+      DetailedSample parent = null;
+      DetailedSample grandparent = null;
+      if (LimsUtils.isDetailedSample(sample)) {
+        detailed = (DetailedSample) sample;
+        parent = detailed.getParent();
+        if (parent != null && parent.hasNonStandardAlias()) {
+          changed = 1;
+          parent.setNonStandardAlias(false);
+          grandparent = parent.getParent();
+          if (grandparent != null && grandparent.hasNonStandardAlias()) {
+            changed = 2;
+            grandparent.setNonStandardAlias(false);
+          }
+        }
+      }
+      String alias = super.generate(sample);
+      if (changed > 0) {
+        detailed.setNonStandardAlias(true);
+        parent.setNonStandardAlias(true);
+      }
+      if (changed > 1) {
+        grandparent.setNonStandardAlias(true);
+      }
+      return alias;
     }
 
   }
