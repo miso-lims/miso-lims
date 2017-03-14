c810f323d4003994ab1e66d567962bcbc2b591ce
Heather Armstrong <callunity@users.noreply.github.com>
2017-03-14 14:52:24 -0400

    GLT-1593 add KAPA Hyper Prep/Agilent SureSelect XT2 kit (#156)

--- sqlstore/src/main/resources/db/migration_afterMigrate/70_kits.sql
+++ sqlstore/src/main/resources/db/migration_afterMigrate/70_kits.sql
@@ -1,3 +1,4 @@
 -- StartNoTest
 CALL addKitDescriptor('QXT',0,'Agilent',0,'LIBRARY','ILLUMINA',NULL);
+CALL addKitDescriptor('KAPA Hyper Prep/Agilent SureSelect XT2', 1, 'Agilent', 1, 'LIBRARY', 'ILLUMINA', 'In-house prep kit');
 -- EndNoTest
