8c4d8dd449c7da8766a5f934c867bf459dfc783c
Andre Masella <andre.masella@oicr.on.ca>
2017-03-28 12:58:03 -0400

    Add OICR genomics lab
    JIRA: GLT-1626

--- sqlstore/src/main/resources/db/migration_afterMigrate/80_institutes.sql
+++ sqlstore/src/main/resources/db/migration_afterMigrate/80_institutes.sql
@@ -5,4 +5,6 @@ CALL addLab('Not Specified', 'Royal Ontario Museum');
 CALL addInstitute('Sheba Medical Center');
 CALL addLab('Not Specified', 'Sheba Medical Center');
 CALL addLab('BioBank', 'Sheba Medical Center');
+
+CALL addLab('Genomics', 'Ontario Institute for Cancer Research');
 -- EndNoTest
