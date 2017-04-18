e4a0a69ff278fc5b67224a6ae019101b7f6676eb
Andre Masella <andre.masella@oicr.on.ca>
2017-04-18 16:22:30 -0400

    Add useful indexes to change logs and group tables

---
+++ sqlstore/src/main/resources/db/migration/V8888__extra_indexes.sql
@@ -0,0 +1,10 @@
+CREATE INDEX BoxChangeLogDerivedInfo ON BoxChangeLog(boxId, changeTime);
+CREATE INDEX ExperimentChangeLogDerivedInfo ON ExperimentChangeLog(experimentId, changeTime);
+CREATE INDEX KitDescriptorChangeLogDerivedInfo ON KitDescriptorChangeLog(kitDescriptorId, changeTime);
+CREATE INDEX LibraryChangeLogDerivedInfo ON LibraryChangeLog(libraryId, changeTime);
+CREATE INDEX PoolChangeLogDerivedInfo ON PoolChangeLog(poolId, changeTime);
+CREATE INDEX RunChangeLogDerivedInfo ON RunChangeLog(runId, changeTime);
+CREATE INDEX SampleChangeLogDerivedInfo ON SampleChangeLog(sampleId, changeTime);
+CREATE INDEX SequencerPartitionContainerChangeLogDerivedInfo ON SequencerPartitionContainerChangeLog(containerId, changeTime);
+CREATE INDEX StudyChangeLogDerivedInfo ON StudyChangeLog(studyId, changeTime);
+CREATE INDEX Group_name ON _Group(name);
