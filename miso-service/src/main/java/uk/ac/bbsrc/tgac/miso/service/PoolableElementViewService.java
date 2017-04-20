a48cd1a0921b2273746515c9ccf19145d87ee454
Andre Masella <andre.masella@oicr.on.ca>
2017-04-20 18:19:10 -0400

    Use PoolableElementView for edit pool page
    JIRA: GLT-1662

---
+++ miso-service/src/main/java/uk/ac/bbsrc/tgac/miso/service/PoolableElementViewService.java
@@ -0,0 +1,8 @@
+package uk.ac.bbsrc.tgac.miso.service;
+
+import uk.ac.bbsrc.tgac.miso.core.data.impl.view.PoolableElementView;
+import uk.ac.bbsrc.tgac.miso.core.util.PaginatedDataSource;
+
+public interface PoolableElementViewService extends PaginatedDataSource<PoolableElementView> {
+
+}
