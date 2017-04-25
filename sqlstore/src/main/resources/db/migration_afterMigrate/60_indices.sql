d76ab6b8d53e981723394294b396abe4e8c3b086
Andre Masella <andre.masella@oicr.on.ca>
2017-04-25 12:11:12 -0400

    Add 10X index family
    JIRA: GLT-1686

--- sqlstore/src/main/resources/db/migration_afterMigrate/60_indices.sql
+++ sqlstore/src/main/resources/db/migration_afterMigrate/60_indices.sql
@@ -1,3 +1,7 @@
 --StartNoTest
 UPDATE IndexFamily SET platformType = 'ILLUMINA' WHERE name = '454' AND platformType = 'LS454';
+
+CALL addIndexFamily('10X Family', 'ILLUMINA', FALSE);
+CALL addIndex('10X Family', 'SI-GA-H8', 'TTGTTGAT', 1);
+CALL addIndex('10X Family', 'SI-GA-H9', 'ACACTGTT', 1);
 --EndNoTest
