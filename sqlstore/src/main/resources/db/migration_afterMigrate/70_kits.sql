ed7417534c1807718b81bb0e698b1ffdde84626f
Andre Masella <andre.masella@oicr.on.ca>
2017-04-25 15:06:01 -0400

    Add 10X scRNA kit
    JIRA: GLT-1689

--- sqlstore/src/main/resources/db/migration_afterMigrate/70_kits.sql
+++ sqlstore/src/main/resources/db/migration_afterMigrate/70_kits.sql
@@ -1,4 +1,5 @@
 -- StartNoTest
 CALL addKitDescriptor('QXT',0,'Agilent',0,'LIBRARY','ILLUMINA',NULL);
 CALL addKitDescriptor('KAPA Hyper Prep and Agilent SureSelect XT2', 1, 'Agilent', 1, 'LIBRARY', 'ILLUMINA', 'In-house prep kit');
+CALL addKitDescriptor('10X scRNA', 2,'10X Genomics', 0, 'LIBRARY', 'ILLUMINA', 'Chromium Single Cell 3'' Chip');
 -- EndNoTest
