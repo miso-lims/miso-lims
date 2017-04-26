9fcd316c16fdb8ff8929c7e0148edbcd00397ccc
Andre Masella <andre.masella@oicr.on.ca>
2017-04-26 13:36:06 -0400

    Try to make derived info snappier while still providing creator

--- sqlstore/src/main/resources/db/migration_afterMigrate/05_standard.sql
+++ sqlstore/src/main/resources/db/migration_afterMigrate/05_standard.sql
@@ -888,22 +888,22 @@ CREATE OR REPLACE VIEW OrderCompletion AS SELECT
 CREATE OR REPLACE VIEW OrderCompletion_Items AS SELECT poolId, parametersId, health, num_partitions FROM OrderCompletion_Backing;
 
 CREATE OR REPLACE VIEW SampleDerivedInfo AS
-  SELECT sampleId, MAX(changeTime) as lastModified, MIN(changeTime) as created, (SELECT userId FROM SampleChangeLog i WHERE i.sampleId = o.sampleId ORDER BY changeTime DESC LIMIT 1) AS creator FROM SampleChangeLog o GROUP BY sampleId;
+  SELECT a.*, userId AS creator FROM SampleChangeLog o JOIN (SELECT sampleId, MAX(changeTime) as lastModified, MIN(changeTime) as created, MIN(sampleChangeLogId) AS firstId FROM SampleChangeLog GROUP BY sampleId) AS a WHERE a.sampleId = o.sampleId AND a.firstId = o.sampleChangeLogId;
   
 CREATE OR REPLACE VIEW RunDerivedInfo AS
-  SELECT runId, MAX(changeTime) as lastModified, MIN(changeTime) as created, (SELECT userId FROM RunChangeLog i WHERE i.runId = o.runId ORDER BY changeTime DESC LIMIT 1) AS creator FROM RunChangeLog o GROUP BY runId;
+  SELECT a.*, userId AS creator FROM RunChangeLog o JOIN (SELECT runId, MAX(changeTime) as lastModified, MIN(changeTime) as created, MIN(runChangeLogId) AS firstId FROM RunChangeLog GROUP BY runId) AS a WHERE a.runId = o.runId AND a.firstId = o.runChangeLogId;
 
 CREATE OR REPLACE VIEW ContainerDerivedInfo AS
-  SELECT containerId, MAX(changeTime) as lastModified, MIN(changeTime) as created, (SELECT userId FROM SequencerPartitionContainerChangeLog i WHERE i.containerId = o.containerId ORDER BY changeTime DESC LIMIT 1) AS creator FROM SequencerPartitionContainerChangeLog o GROUP BY containerId;
+  SELECT a.*, userId AS creator FROM SequencerPartitionContainerChangeLog o JOIN (SELECT containerId, MAX(changeTime) as lastModified, MIN(changeTime) as created, MIN(containerChangeLogId) AS firstId FROM SequencerPartitionContainerChangeLog GROUP BY containerId) AS a WHERE a.containerId = o.containerId AND a.firstId = o.containerChangeLogId;
 
 CREATE OR REPLACE VIEW PoolDerivedInfo AS
-  SELECT poolId, MAX(changeTime) as lastModified, MIN(changeTime) as created, (SELECT userId FROM PoolChangeLog i WHERE i.poolId = o.poolId ORDER BY changeTime DESC LIMIT 1) AS creator FROM PoolChangeLog o GROUP BY poolId;
+  SELECT a.*, userId AS creator FROM PoolChangeLog o JOIN (SELECT poolId, MAX(changeTime) as lastModified, MIN(changeTime) as created, MIN(poolChangeLogId) AS firstId FROM PoolChangeLog GROUP BY poolId) AS a WHERE a.poolId = o.poolId AND a.firstId = o.poolChangeLogId;
 
 CREATE OR REPLACE VIEW LibraryDerivedInfo AS
-  SELECT libraryId, MAX(changeTime) AS lastModified, MIN(changeTime) as created, (SELECT userId FROM LibraryChangeLog i WHERE i.libraryId = o.libraryId ORDER BY changeTime DESC LIMIT 1) AS creator FROM LibraryChangeLog o GROUP BY libraryId;
+  SELECT a.*, userId AS creator FROM LibraryChangeLog o JOIN (SELECT libraryId, MAX(changeTime) as lastModified, MIN(changeTime) as created, MIN(libraryChangeLogId) AS firstId FROM LibraryChangeLog GROUP BY libraryId) AS a WHERE a.libraryId = o.libraryId AND a.firstId = o.libraryChangeLogId;
   
 CREATE OR REPLACE VIEW BoxDerivedInfo AS
-  SELECT boxId, MAX(changeTime) AS lastModified, MIN(changeTime) as created, (SELECT userId FROM BoxChangeLog i WHERE i.boxId = o.boxId ORDER BY changeTime DESC LIMIT 1) AS creator FROM BoxChangeLog o GROUP BY boxId;
+  SELECT a.*, userId AS creator FROM BoxChangeLog o JOIN (SELECT boxId, MAX(changeTime) as lastModified, MIN(changeTime) as created, MIN(boxChangeLogId) AS firstId FROM BoxChangeLog GROUP BY boxId) AS a WHERE a.boxId = o.boxId AND a.firstId = o.boxChangeLogId;
 
 CREATE OR REPLACE VIEW SampleBoxPosition
 AS SELECT s.sampleId, bp.boxId, bp.position
