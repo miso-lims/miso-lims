4c7382dbb19305032d47a1dd81b07e09f5870706
Dillan Cooke <dillan.cooke@oicr.on.ca>
2017-05-26 17:33:52 -0400

    Prepared for release

--- sqlstore/src/main/resources/db/migration/V0270__auto_main.sql
+++ sqlstore/src/main/resources/db/migration/V0270__auto_main.sql
@@ -1,4 +1,8 @@
+-- tissue_nullability
+
 UPDATE SampleTissue SET tissueOriginId = (SELECT tissueOriginId FROM TissueOrigin WHERE description = 'Unknown') WHERE tissueOriginId IS NULL;
 
 ALTER TABLE SampleTissue CHANGE tissueTypeId tissueTypeId bigint(20) NOT NULL;
 ALTER TABLE SampleTissue CHANGE tissueOriginId tissueOriginId bigint(20) NOT NULL;
+
+
