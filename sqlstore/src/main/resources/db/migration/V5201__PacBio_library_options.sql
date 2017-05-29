fa7d4a375ca4d0bc14f00827afa8f6eb20f24895
Heather Armstrong <heather.armstrong@oicr.on.ca>
2017-05-29 10:47:39 -0400

    fixup set "(None)" as default, not as db item

--- sqlstore/src/main/resources/db/migration/V5201__PacBio_library_options.sql
+++ sqlstore/src/main/resources/db/migration/V5201__PacBio_library_options.sql
@@ -1,3 +1,3 @@
 INSERT INTO LibraryType (description, platformType, archived) VALUES ('Whole Genome', 'PACBIO', 0);
 
-INSERT INTO LibrarySelectionType (name, description) VALUES ('SageHLS', 'Sage High Molecular Weight Library System'), ('BluePippin', 'Sage BluePippin size selection'), ('None', 'None');
+INSERT INTO LibrarySelectionType (name, description) VALUES ('SageHLS', 'Sage High Molecular Weight Library System'), ('BluePippin', 'Sage BluePippin size selection');
