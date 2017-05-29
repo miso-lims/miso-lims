509e444955a3ae41683bee5e07ea5e9223c6f903
Heather Armstrong <heather.armstrong@oicr.on.ca>
2017-05-29 09:32:23 -0400

    add PacBio RS II kit

--- sqlstore/src/main/resources/db/migration_afterMigrate/70_kits.sql
+++ sqlstore/src/main/resources/db/migration_afterMigrate/70_kits.sql
@@ -2,4 +2,5 @@
 CALL addKitDescriptor('QXT',0,'Agilent',0,'LIBRARY','ILLUMINA',NULL);
 CALL addKitDescriptor('KAPA Hyper Prep and Agilent SureSelect XT2', 1, 'Agilent', 1, 'LIBRARY', 'ILLUMINA', 'In-house prep kit');
 CALL addKitDescriptor('10X Single Cell RNA', 2,'10X Genomics', 0, 'LIBRARY', 'ILLUMINA', 'Chromium Single Cell 3'' Chip');
+CALL addKitDescriptor('PacBio RS II',1,'PacBio',0,'LIBRARY','PACBIO',NULL);
 -- EndNoTest
