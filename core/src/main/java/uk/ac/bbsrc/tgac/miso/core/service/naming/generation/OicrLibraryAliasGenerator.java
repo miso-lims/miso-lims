2a63c4115fa2b569b4681653f2f2a28ee5abce5d
Dillan Cooke <dillan.cooke@oicr.on.ca>
2017-06-07 17:12:36 -0400

    GLT-1723: updated OICR library alias generator

--- core/src/main/java/uk/ac/bbsrc/tgac/miso/core/service/naming/generation/OicrLibraryAliasGenerator.java
+++ core/src/main/java/uk/ac/bbsrc/tgac/miso/core/service/naming/generation/OicrLibraryAliasGenerator.java
@@ -1,13 +1,16 @@
 package uk.ac.bbsrc.tgac.miso.core.service.naming.generation;
 
+import static uk.ac.bbsrc.tgac.miso.core.util.LimsUtils.*;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import uk.ac.bbsrc.tgac.miso.core.data.DetailedLibrary;
+import uk.ac.bbsrc.tgac.miso.core.data.DetailedSample;
 import uk.ac.bbsrc.tgac.miso.core.data.Library;
+import uk.ac.bbsrc.tgac.miso.core.data.SampleTissue;
 import uk.ac.bbsrc.tgac.miso.core.exception.MisoNamingException;
 
-// TODO: fix/finish - this is the alias generation code as it was (not working) before naming scheme refactor (GLT-551)
 public class OicrLibraryAliasGenerator implements NameGenerator<Library> {
 
   private static final String sampleRegex = "^([A-Z\\d]{3,5}_\\d{3,5}_(?:[A-Z][a-z]|nn)_[A-Zn])_.*";
@@ -15,38 +18,131 @@ public class OicrLibraryAliasGenerator implements NameGenerator<Library> {
 
   @Override
   public String generate(Library library) throws MisoNamingException {
-    if (library.getSample() != null) {
-      Matcher m = samplePattern.matcher(library.getSample().getAlias());
-
-      if (m.matches()) {
-        // try {
-        // int numLibs = requestManager.listAllLibrariesBySampleId(l.getSample().getId()).size();
-        String libraryType = "??";
-        if (library.getLibraryType().getDescription().equals("Paired End")) {
-          libraryType = "PE";
-        } else if (library.getLibraryType().getDescription().equals("Single End")) {
-          libraryType = "SE";
-        }
-        String libraryDesignCode = ((DetailedLibrary) library).getLibraryDesignCode().getCode();
-        String estimateInsertSize = "300";
-        StringBuilder sb = new StringBuilder();
-        sb.append(m.group(1)); // PCSI_0123_Pa_R (project name, patient number, tissue origin, tissue type)
-        sb.append("_").append(libraryType); // PE (library type)
-        sb.append("_").append(estimateInsertSize); // 300 (estimated insert size)
-        sb.append("_").append(libraryDesignCode); // WG (source template type)
-        String libraryAlias = sb.toString();
-        // String la = m.group(1) + "_" + m.group(2) + "-" + (numLibs + 1) + "_" + m.group(3);
-        return libraryAlias;
-        // } catch (IOException e) {
-        // throw new MisoNamingException("Cannot generate Library alias for: " + l.toString(), e);
-        // }
-      } else {
-        throw new MisoNamingException("Cannot generate Library alias for: " + library.toString() + " from supplied sample alias: "
-            + library.getSample().getAlias());
-      }
-    } else {
-      throw new MisoNamingException("This alias generation scheme requires the Library to have a parent Sample set.");
+    if (!isDetailedLibrary(library)) {
+      throw new IllegalArgumentException("Can only generate an alias for detailed samples");
     }
+    DetailedLibrary detailed = (DetailedLibrary) library;
+
+    switch (detailed.getPlatformType()) {
+    case ILLUMINA:
+      return generateIlluminaLibraryAlias(detailed);
+    case PACBIO:
+      return generatePacBioLibraryAlias(detailed);
+    default:
+      throw new MisoNamingException("Alias generation is only available for Illumina and PacBio Libraries");
+    }
+  }
+
+  private String generateIlluminaLibraryAlias(DetailedLibrary library) throws MisoNamingException {
+    // e.g. PROJ_0001_Pa_P_PE_300_WG
+    StringBuilder sb = new StringBuilder();
+    sb.append(getIlluminaSampleAliasPart(library));
+    sb.append("_").append(getLibraryTypeAbbreviation(library));
+    sb.append("_").append(getInsertSize(library));
+    sb.append("_").append(getDesignCode(library));
+    return sb.toString();
+  }
+
+  /**
+   * Get the beginning of the Sample alias, to be used in the Library alias,
+   * e.g. PCSI_0123_Pa_R (project name, patient number, tissue origin, tissue type)
+   * 
+   * @param library the Library that an alias is being generated for
+   * @return the portion of the Sample alias to be used in the Library alias
+   * @throws NullPointerException if library's parent Sample is not set
+   * @throws MisoNamingException if unable to generate an alias for any other reason
+   */
+  private String getIlluminaSampleAliasPart(DetailedLibrary library) throws MisoNamingException {
+    Matcher m = samplePattern.matcher(library.getSample().getAlias());
+    if (!m.matches()) {
+      throw new MisoNamingException("Cannot generate alias due to non-standard alias on parent Sample");
+    }
+    return m.group(1);
+  }
+  
+  /**
+   * @param library the Library that an alias is being generated for
+   * @return the name portion representing the LibraryType
+   * @throws NullPointerException if library's LibraryType is not set
+   * @throws MisoNamingException if unable to generate an alias for any other reason
+   */
+  private String getLibraryTypeAbbreviation(DetailedLibrary library) throws MisoNamingException {
+    String abbr = library.getLibraryType().getAbbreviation();
+    if (abbr == null) {
+      throw new MisoNamingException("Cannot generate alias for LibraryType '" + library.getLibraryType().getDescription() + "'");
+    }
+    return abbr;
+  }
+
+  /**
+   * @param library the Library that an alias is being generated for
+   * @return the name portion represeting the estimated insert size
+   * @throws MisoNamingException if library's dnaSize is not set
+   */
+  private String getInsertSize(Library library) throws MisoNamingException {
+    if (library.getDnaSize() == null) {
+      throw new MisoNamingException("Cannot generate an alias without insert size set");
+    }
+    return library.getDnaSize().toString();
+  }
+
+  /**
+   * @param library the Library that an alias is being generated for
+   * @return the name portion represeting the library design
+   * @throws NullPointerException if library's LibraryDesignCode is missing
+   */
+  private String getDesignCode(DetailedLibrary library) {
+    if (library.getLibraryDesignCode() == null || library.getLibraryDesignCode().getCode() == null) {
+      throw new NullPointerException("LibraryDesignCode missing");
+    }
+    return library.getLibraryDesignCode().getCode();
+  }
+
+  private String generatePacBioLibraryAlias(DetailedLibrary library) throws MisoNamingException {
+    // e.g. PROJ_1_150pM
+    StringBuilder sb = new StringBuilder();
+    sb.append(getProjectShortName(library));
+    sb.append("_").append(getBatchNumber(library));
+    sb.append("_").append(getConcentration(library));
+    return sb.toString();
+  }
+
+  /**
+   * @param library the Library that an alias is being generated for
+   * @return the project's shortName
+   * @throws NullPointerException if the project or its shortName are missing
+   */
+  private String getProjectShortName(Library library) {
+    return library.getSample().getProject().getShortName().toString();
+  }
+
+  /**
+   * @param library the Library that an alias is being generated for
+   * @return the parent tissue's timesReceived
+   * @throws IllegalArgumentException if no related SampleTissue is found
+   * @throws NullPointerException if the tissue sample's timesReceived is missing
+   */
+  private String getBatchNumber(DetailedLibrary library) {
+    for (DetailedSample sample = (DetailedSample) library.getSample(); sample != null; sample = sample.getParent()) {
+      if (isTissueSample(sample)) {
+        SampleTissue tissue = (SampleTissue) deproxify(sample);
+        return tissue.getTimesReceived().toString();
+      }
+    }
+    throw new IllegalArgumentException("No Tissue sample found in hierarchy");
+  }
+
+  /**
+   * @param library the Library that an alias is being generated for
+   * @return the concentration as a String including the units
+   * @throws NullPointerException if PlatformType is missing
+   * @throws MisoNamingException if concentration is not set
+   */
+  private String getConcentration(DetailedLibrary library) throws MisoNamingException {
+    if (library.getInitialConcentration() == null) {
+      throw new MisoNamingException("Cannot generate an alias without concentration set");
+    }
+    return library.getInitialConcentration().intValue() + library.getPlatformType().getLibraryConcentrationUnits();
   }
 
 }
--- core/src/test/java/uk/ac/bbsrc/tgac/miso/core/service/naming/generation/OicrLibraryAliasGeneratorTest.java
+++ core/src/test/java/uk/ac/bbsrc/tgac/miso/core/service/naming/generation/OicrLibraryAliasGeneratorTest.java
@@ -1,7 +1,6 @@
 package uk.ac.bbsrc.tgac.miso.core.service.naming.generation;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -9,10 +8,14 @@ import org.junit.Test;
 import uk.ac.bbsrc.tgac.miso.core.data.DetailedLibrary;
 import uk.ac.bbsrc.tgac.miso.core.data.DetailedSample;
 import uk.ac.bbsrc.tgac.miso.core.data.LibraryDesignCode;
+import uk.ac.bbsrc.tgac.miso.core.data.Project;
+import uk.ac.bbsrc.tgac.miso.core.data.SampleTissue;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.DetailedLibraryImpl;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.DetailedSampleImpl;
-import uk.ac.bbsrc.tgac.miso.core.data.type.LibraryStrategyType;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.ProjectImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.SampleTissueImpl;
 import uk.ac.bbsrc.tgac.miso.core.data.type.LibraryType;
+import uk.ac.bbsrc.tgac.miso.core.data.type.PlatformType;
 
 public class OicrLibraryAliasGeneratorTest {
 
@@ -24,22 +27,44 @@ public class OicrLibraryAliasGeneratorTest {
   }
 
   @Test
-  public void test_alias_generation_from_sample() throws Exception {
+  public void testGenerateIlluminaLibraryAlias() throws Exception {
+    DetailedLibrary library = new DetailedLibraryImpl();
+    library.setPlatformType(PlatformType.ILLUMINA);
+
     DetailedSample sample = new DetailedSampleImpl();
     sample.setAlias("BART_1273_Br_P_nn_1-1_D_1");
-    DetailedLibrary library = new DetailedLibraryImpl();
     library.setSample(sample);
+
     LibraryType libraryType = new LibraryType();
-    libraryType.setDescription("Paired End");
+    libraryType.setAbbreviation("PE");
     library.setLibraryType(libraryType);
-    LibraryStrategyType libraryStrategyType = new LibraryStrategyType();
-    libraryStrategyType.setName("AMPLICON");
-    library.setLibraryStrategyType(libraryStrategyType);
+
+    library.setDnaSize(300);
+
     LibraryDesignCode code = new LibraryDesignCode();
     code.setCode("WG");
     library.setLibraryDesignCode(code);
 
-    assertThat(sut.generate(library), is("BART_1273_Br_P_PE_300_WG"));
+    assertEquals("BART_1273_Br_P_PE_300_WG", sut.generate(library));
+  }
+
+  @Test
+  public void testGeneratePacBioLibraryAlias() throws Exception {
+    DetailedLibrary library = new DetailedLibraryImpl();
+    library.setPlatformType(PlatformType.PACBIO);
+
+    DetailedSample parent = new DetailedSampleImpl();
+    Project proj = new ProjectImpl();
+    proj.setShortName("PROJ");
+    parent.setProject(proj);
+    SampleTissue grandParent = new SampleTissueImpl();
+    grandParent.setTimesReceived(2);
+    parent.setParent(grandParent);
+    library.setSample(parent);
+
+    library.setInitialConcentration(150D);
+
+    assertEquals("PROJ_2_150pM", sut.generate(library));
   }
 
 }
