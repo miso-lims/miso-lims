185361ea0b62842a1c06b53978c420e75c798726
Heather Armstrong <callunity@users.noreply.github.com>
2017-06-09 15:44:05 -0400

    add TissueOrigins (#181)

--- sqlstore/src/main/resources/db/migration_afterMigrate/40_tissue_origins.sql
+++ sqlstore/src/main/resources/db/migration_afterMigrate/40_tissue_origins.sql
@@ -3,4 +3,7 @@ CALL addTissueOrigin('Ab', 'Abdomen');
 CALL addTissueOrigin('Ct', 'Circulating Tumour Cells');
 CALL addTissueOrigin('Pl', 'Plasma');
 CALL addTissueOrigin('Se', 'Serum');
+CALL addTissueOrigin('Bd', 'Bile Duct');
+CALL addTissueOrigin('Co', 'Colon');
+CALL addTissueOrigin('Pt', 'Palate');
 -- EndNoTest
