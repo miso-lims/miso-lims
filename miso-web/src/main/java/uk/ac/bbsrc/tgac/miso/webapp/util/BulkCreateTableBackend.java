d6f1cefe257af3436e4a2ca33a7263ce426008a8
Heather Armstrong <heather.armstrong@oicr.on.ca>
2017-06-09 15:14:55 -0400

    Refactor BulkTableBackend classes for different use cases

---
+++ miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/util/BulkCreateTableBackend.java
@@ -0,0 +1,38 @@
+package uk.ac.bbsrc.tgac.miso.webapp.util;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.ui.ModelMap;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+/**
+ * Create a Handsontable for propagating or editing a particular entity type
+ *
+ * @param <Dto> The DTO for the entity being edited
+ */
+public class BulkCreateTableBackend<Dto> extends BulkTableBackend<Dto> {
+  private final String name;
+  private final Dto dto;
+
+  public BulkCreateTableBackend(String targetType, Class<? extends Dto> dtoClass, String name, Dto dto) {
+    super(targetType, dtoClass);
+    this.name = name;
+    this.dto = dto;
+  }
+
+  public final ModelAndView create(ModelMap model) throws IOException {
+    List<Dto> dtos = Collections.singletonList(dto);
+    return prepare(model, true, "Create " + name, dtos);
+  }
+
+  @Override
+  protected void writeConfiguration(ObjectMapper mapper, ObjectNode config) {
+
+  }
+
+}
---
+++ miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/util/BulkEditTableBackend.java
@@ -0,0 +1,48 @@
+package uk.ac.bbsrc.tgac.miso.webapp.util;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.ui.ModelMap;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Create a Handsontable for propagating or editing a particular entity type
+ *
+ * @param <Model> The database model for the entity being edited
+ * @param <Dto> The DTO for the entity being edited
+ */
+public abstract class BulkEditTableBackend<Model, Dto> extends BulkTableBackend<Dto> {
+  private final String name;
+
+  public BulkEditTableBackend(String targetType, Class<? extends Dto> dtoClass, String name) {
+    super(targetType, dtoClass);
+    this.name = name;
+  }
+
+  /**
+   * Convert a model to its DTO representation
+   */
+  protected abstract Dto asDto(Model model);
+
+  /**
+   * Edit the specified entities.
+   * 
+   * @param idString A comma-delimited string of entity IDs.
+   */
+  public final ModelAndView edit(String idString, ModelMap model) throws IOException {
+    List<Long> ids = parseIds(idString);
+    List<Dto> dtos = new ArrayList<>();
+    for (Model item : load(ids)) {
+      dtos.add(asDto(item));
+    }
+    return prepare(model, false, "Edit " + name, dtos);
+  }
+
+  /**
+   * Read all the specified models from the database by their IDs.
+   */
+  protected abstract Iterable<Model> load(List<Long> modelIds) throws IOException;
+
+}
