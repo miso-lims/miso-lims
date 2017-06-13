65bc4f7ca3a2d6e98120650f10ec8db282208d14
Heather Armstrong <callunity@users.noreply.github.com>
2017-06-13 09:46:59 -0400

    GLT-1833 add NEB Index Primer Set 3 (#180)
    * GLT-1833 add NEB Index Primer Set 3
    
    * GLT-1838 add RKT1 kits and indices

--- sqlstore/src/main/resources/db/migration_afterMigrate/70_kits.sql
+++ sqlstore/src/main/resources/db/migration_afterMigrate/70_kits.sql
@@ -3,4 +3,6 @@ CALL addKitDescriptor('QXT',0,'Agilent',0,'LIBRARY','ILLUMINA',NULL);
 CALL addKitDescriptor('KAPA Hyper Prep and Agilent SureSelect XT2', 1, 'Agilent', 1, 'LIBRARY', 'ILLUMINA', 'In-house prep kit');
 CALL addKitDescriptor('10X Single Cell RNA', 2,'10X Genomics', 0, 'LIBRARY', 'ILLUMINA', 'Chromium Single Cell 3'' Chip');
 CALL addKitDescriptor('PacBio RS II',1,'PacBio',0,'LIBRARY','PACBIO',NULL);
+CALL addKitDescriptor('Lexogen SENSE Total RNA',1,'Lexogen',0,'LIBRARY','ILLUMINA',NULL);
+CALL addKitDescriptor('KAPA RNA Hyper Prep',1,'KAPA',0,'LIBRARY','ILLUMINA',NULL);
 -- EndNoTest
