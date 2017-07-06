ca6564b9f2835c6334dd0603d9b6b46de58b1ced
Andre Masella <andre.masella@oicr.on.ca>
2017-07-06 15:11:29 -0400

    Remove dead JSPs

--- miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/controller/rest/ExperimentRestController.java
+++ miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/controller/rest/ExperimentRestController.java
@@ -12,60 +12,37 @@
  *
  * MISO is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with MISO.  If not, see <http://www.gnu.org/licenses/>.
+ * along with MISO. If not, see <http://www.gnu.org/licenses/>.
  *
  * *********************************************************************
  */
 
-package uk.ac.bbsrc.tgac.miso.webapp.controller;
+package uk.ac.bbsrc.tgac.miso.webapp.controller.rest;
 
 import java.io.IOException;
 import java.util.Collection;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import uk.ac.bbsrc.tgac.miso.core.data.Experiment;
 import uk.ac.bbsrc.tgac.miso.service.ExperimentService;
 
-/**
- * com.eaglegenomics.miso.web
- * <p/>
- * TODO Info
- * 
- * @author Rob Davey
- * @since 0.0.2
- */
 @Controller
-public class ListExperimentsController {
-  protected static final Logger log = LoggerFactory.getLogger(ListExperimentsController.class);
+public class ExperimentRestController extends RestController {
 
   @Autowired
   private ExperimentService experimentService;
 
-  @ModelAttribute("title")
-  public String title() {
-    return "Experiments";
-  }
-
   @RequestMapping(value = "/experiments/rest/", method = RequestMethod.GET)
-  public @ResponseBody Collection<Experiment> jsonRest() throws IOException {
+  public @ResponseBody Collection<Experiment> list() throws IOException {
     return experimentService.listAll();
-  }
-
-  @RequestMapping("/experiments")
-  public ModelAndView listExperiments() throws Exception {
-    return new ModelAndView("/pages/listExperiments.jsp");
   }
 }
