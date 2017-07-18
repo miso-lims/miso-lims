2095a9f97218be4a3ec8c17c41a35c2a4aa89e81
Heather Armstrong <callunity@users.noreply.github.com>
2017-07-18 16:37:59 -0400

    GLT-1947 add 10X library kit (#184)

--- sqlstore/src/main/resources/db/migration_afterMigrate/70_kits.sql
+++ sqlstore/src/main/resources/db/migration_afterMigrate/70_kits.sql
@@ -6,4 +6,5 @@ CALL addKitDescriptor('PacBio RS II',1,'PacBio',0,'LIBRARY','PACBIO',NULL);
 CALL addKitDescriptor('Lexogen SENSE Total RNA',1,'Lexogen',0,'LIBRARY','ILLUMINA',NULL);
 CALL addKitDescriptor('KAPA RNA Hyper Prep',1,'KAPA',0,'LIBRARY','ILLUMINA',NULL);
 CALL addKitDescriptor('NebNext Ultra II Directional RNA', 1, 'New England BioLabs', 'E7760S', 'LIBRARY', 'ILLUMINA', 'WT Library Prep for Illumina');
+CALL addKitDescriptor('10X Genome Linked Reads',2,'10X Genomics','PN-120258','LIBRARY','ILLUMINA','Linked Reads');
 -- EndNoTest
