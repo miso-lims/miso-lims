69739f09680f3c58866d0c7d8b3abc9b6e461e99
Dillan Cooke <dillan.cooke@oicr.on.ca>
2017-07-25 09:51:27 -0400

    Added AbstractElement to be extended by pages and custom elements

--- miso-web/src/it/java/uk/ac/bbsrc/tgac/miso/webapp/integrationtest/page/AbstractPage.java
+++ miso-web/src/it/java/uk/ac/bbsrc/tgac/miso/webapp/integrationtest/page/AbstractPage.java
@@ -1,35 +1,22 @@
 package uk.ac.bbsrc.tgac.miso.webapp.integrationtest.page;
 
-import java.util.List;
-import java.util.function.Function;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.InvalidArgumentException;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-public abstract class AbstractPage {
+import uk.ac.bbsrc.tgac.miso.webapp.integrationtest.page.element.AbstractElement;
 
-  private static final long DEFAULT_WAIT = 15;
-
-  private final WebDriver driver;
+public abstract class AbstractPage extends AbstractElement {
 
   private static final ExpectedCondition<Boolean> pageLoaded = (driver) -> {
     return ((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete");
   };
 
   protected AbstractPage(WebDriver driver) {
-    this.driver = driver;
-  }
-
-  protected WebDriver getDriver() {
-    return driver;
+    super(driver);
   }
 
   protected static void waitExplicitly(long millis) {
@@ -41,35 +28,11 @@ public abstract class AbstractPage {
   }
 
   protected void waitForPageRefresh() {
-    waitUntil(pageLoaded, DEFAULT_WAIT);
+    waitUntil(pageLoaded);
   }
 
   protected void waitForPageRefresh(long seconds) {
     waitUntil(pageLoaded, seconds);
-  }
-
-  private void waitUntil(Function<? super WebDriver, Boolean> condition, long seconds) {
-    waitWithTimeout(seconds).until(condition);
-  }
-
-  protected Wait<WebDriver> waitWithTimeout() {
-    return new WebDriverWait(getDriver(), DEFAULT_WAIT);
-  }
-
-  protected Wait<WebDriver> waitWithTimeout(long seconds) {
-    return new WebDriverWait(getDriver(), seconds);
-  }
-
-  protected WebElement findElementIfExists(By selector) {
-    List<WebElement> elements = driver.findElements(selector);
-    switch (elements.size()) {
-    case 0:
-      return null;
-    case 1:
-      return elements.get(0);
-    default:
-      throw new InvalidArgumentException("Selector yielded multiple elements. Use WebDriver#findElements instead");
-    }
   }
 
   protected void setText(String input, WebElement element) {
@@ -99,4 +62,5 @@ public abstract class AbstractPage {
     Select dropdown = new Select(element);
     return dropdown.getFirstSelectedOption().getText();
   }
+
 }
