d767be1e90a66dbe174dfddc1b63703cf4f81090
Heather Armstrong <callunity@users.noreply.github.com>
2017-07-25 13:55:35 -0400

    GLT-1986 add tissue origins (#186)

--- sqlstore/src/main/resources/db/migration_afterMigrate/40_tissue_origins.sql
+++ sqlstore/src/main/resources/db/migration_afterMigrate/40_tissue_origins.sql
@@ -10,4 +10,15 @@ CALL addTissueOrigin('Bo', 'Bone');
 CALL addTissueOrigin('Fa', 'Fallopian Tube');
 CALL addTissueOrigin('So', 'Soft Tissue');
 CALL addTissueOrigin('Ut', 'Uterus');
+CALL addTissueOrigin('Bl', 'Bladder');
+CALL addTissueOrigin('Cc', 'Cecum');
+CALL addTissueOrigin('Ch', 'Chest Wall');
+CALL addTissueOrigin('Mx', 'Maxilla');
+CALL addTissueOrigin('Pe', 'Pelvic');
+CALL addTissueOrigin('Pg', 'Parotid gland');
+CALL addTissueOrigin('Pm', 'Peritoneum');
+CALL addTissueOrigin('Sb', 'Small Bowel');
+CALL addTissueOrigin('Te', 'Testes');
+CALL addTissueOrigin('Ty', 'Thyroid');
+CALL addTissueOrigin('Vg', 'Vagina');
 -- EndNoTest
