fb3e05fd920c7070dfc2e56d73506c61cd33e479
Dillan Cooke <dillan.cooke@oicr.on.ca>
2017-07-26 13:28:02 -0400

    test using Firefox driver and Xvfb

--- miso-web/src/it/java/uk/ac/bbsrc/tgac/miso/webapp/integrationtest/AbstractIT.java
+++ miso-web/src/it/java/uk/ac/bbsrc/tgac/miso/webapp/integrationtest/AbstractIT.java
@@ -1,7 +1,7 @@
 package uk.ac.bbsrc.tgac.miso.webapp.integrationtest;
 
 import static org.junit.Assert.assertNotNull;
-import io.github.bonigarcia.wdm.PhantomJsDriverManager;
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,7 +13,8 @@ import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -30,18 +31,25 @@ public abstract class AbstractIT {
   private SessionFactory sessionFactory;
 
   private WebDriver driver;
-  private static final String baseUrl = System.getProperty("miso.it.baseUrl");
+  private static final String BASE_URL = System.getProperty("miso.it.baseUrl");
+  private static final DesiredCapabilities CAPABILITIES;
+
+  static {
+    FirefoxOptions opts = new FirefoxOptions();
+    opts.addArguments("--display=" + System.getProperty("miso.it.xvfb.display"));
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setCapability(FirefoxOptions.FIREFOX_OPTIONS, opts);
+    CAPABILITIES = capabilities;
+  }
 
   @BeforeClass
   public static final void setupAbstractClass() {
-    PhantomJsDriverManager.getInstance().setup();
+    FirefoxDriverManager.getInstance().setup();
   }
 
   @Before
   public final void setupAbstractTest() {
-    DesiredCapabilities capabilities = new DesiredCapabilities();
-    capabilities.setCapability("takesScreenshot", false);
-    driver = new PhantomJSDriver(capabilities);
+    driver = new FirefoxDriver(CAPABILITIES);
     // don't allow page load or script execution to take longer than 10 seconds
     driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
     driver.manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS);
@@ -62,7 +70,7 @@ public abstract class AbstractIT {
   }
 
   protected final String getBaseUrl() {
-    return baseUrl;
+    return BASE_URL;
   }
 
   protected final Session getSession() {
