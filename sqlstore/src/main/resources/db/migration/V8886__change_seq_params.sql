bd1682375c54a038247330751c58437fc6d254a3
Andre Masella <andre.masella@oicr.on.ca>
2017-08-08 10:26:17 -0400

    Add support for NextSeq seqeuencing chemistries

--- sqlstore/src/main/resources/db/migration/V8886__change_seq_params.sql
+++ sqlstore/src/main/resources/db/migration/V8886__change_seq_params.sql
@@ -4,6 +4,10 @@ UPDATE SequencingParameters SET chemistry = 'V2' WHERE platformId IN (SELECT pla
 UPDATE SequencingParameters SET chemistry = 'V3' WHERE xpath LIKE '%v3%';
 UPDATE SequencingParameters SET chemistry = 'V4' WHERE xpath LIKE '%v4%';
 UPDATE SequencingParameters SET chemistry = 'RAPID_RUN' WHERE xpath LIKE '%Rapid%';
-UPDATE SequencingParameters SET chemistry = 'V4' WHERE platformId IN (SELECT platformId FROM Platform WHERE name = 'ILLUMINA' AND instrumentModel LIKE '%NextSeq%');
+INSERT INTO SequencingParameters (name, platformId, readLength, paired, createdBy, creationDate, updatedBy, lastUpdated, chemistry)
+  SELECT CONCAT('High ', name), platformId, readLength, paired, createdBy, CURRENT_TIMESTAMP, updatedBy, CURRENT_TIMESTAMP, 'NS_HIGH'
+  FROM SequencingParameters WHERE platformId IN (SELECT platformId FROM Platform WHERE name = 'ILLUMINA' AND instrumentModel LIKE '%NextSeq%') AND chemistry = 'UNKNOWN' AND name NOT LIKE 'Custom%';
+
+UPDATE SequencingParameters SET name = CONCAT('Mid ', name), chemistry = 'NS_MID' WHERE platformId IN (SELECT platformId FROM Platform WHERE name = 'ILLUMINA' AND instrumentModel LIKE '%NextSeq%') AND chemistry = 'UNKNOWN' AND name NOT LIKE 'Custom%';
 
 ALTER TABLE SequencingParameters DROP COLUMN xpath;
