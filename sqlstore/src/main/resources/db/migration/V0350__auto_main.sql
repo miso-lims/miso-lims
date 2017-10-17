07c3d341570ec4e3e35a7a61fa34d437d5f5c75c
Heather Armstrong <heather.armstrong@oicr.on.ca>
2017-10-17 14:13:34 -0400

    Prepared for release

--- sqlstore/src/main/resources/db/migration/V0350__auto_main.sql
+++ sqlstore/src/main/resources/db/migration/V0350__auto_main.sql
@@ -1,4 +1,8 @@
+-- library_receipt
+
 ALTER TABLE Library ADD COLUMN receivedDate DATE DEFAULT NULL;
 
 ALTER TABLE DetailedLibrary ADD COLUMN groupId varchar(10) DEFAULT NULL;
 ALTER TABLE DetailedLibrary ADD COLUMN groupDescription varchar(255) DEFAULT NULL;
+
+
