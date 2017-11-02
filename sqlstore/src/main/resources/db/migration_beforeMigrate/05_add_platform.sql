35a98356002e6432e3daafd220b4961aade2780d
Dillan Cooke <dillan.cooke@oicr.on.ca>
2017-11-02 11:33:28 -0400

    fixed broken stored procedures

--- sqlstore/src/main/resources/db/migration_beforeMigrate/05_add_platform.sql
+++ sqlstore/src/main/resources/db/migration_beforeMigrate/05_add_platform.sql
@@ -6,11 +6,15 @@ CREATE PROCEDURE addPlatform(
   iName varchar(50),
   iInstrumentModel varchar(100),
   iDescription varchar(255),
-  iNumContainers tinyint(4)
+  iNumContainers tinyint(4),
+  iPartitionSize int(11)
 ) BEGIN
   IF NOT EXISTS (SELECT 1 FROM Platform WHERE name = iName and instrumentModel = iInstrumentModel) THEN
     INSERT INTO Platform(name, instrumentModel, description, numContainers)
     VALUES (iName, iInstrumentModel, iDescription, iNumContainers);
+    
+    INSERT INTO PlatformSizes(platform_platformId, partitionSize)
+    VALUES (LAST_INSERT_ID(), iPartitionSize);
   END IF;
 END//
 
