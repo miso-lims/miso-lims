2b5f29d5a5e57552af89526f1874cb90f2b38076
Heather Armstrong <callunity@users.noreply.github.com>
2017-12-13 10:31:53 -0500

    update stored procedures to match TGAC PR#1230 (#210)

--- sqlstore/src/main/resources/db/migration_afterMigrate/65_sequencers.sql
+++ sqlstore/src/main/resources/db/migration_afterMigrate/65_sequencers.sql
@@ -1,5 +1,5 @@
 -- StartNoTest
-CALL addPlatform('ILLUMINA','Illumina NovaSeq 6000',NULL,1,2);
+CALL addInstrumentModel('ILLUMINA','Illumina NovaSeq 6000',NULL,1,2);
 CALL addSequencerReference('A00169','ILLUMINA','Illumina NovaSeq 6000',NULL,'127.0.0.1','2017-11-02',NULL,NULL);
 CALL addSequencingParameters('2×151','ILLUMINA','Illumina NovaSeq 6000',151,1,'UNKNOWN');
 -- EndNoTest
