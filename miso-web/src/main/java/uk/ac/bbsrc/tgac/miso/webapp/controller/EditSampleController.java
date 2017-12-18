63d7669b6079dbc21f994c4ec123fb0df50bd260
Heather Armstrong <heather.armstrong@oicr.on.ca>
2017-12-18 12:03:13 -0500

    anothe ignore cognitive complexity

--- miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/controller/EditSampleController.java
+++ miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/controller/EditSampleController.java
@@ -139,6 +139,7 @@ import uk.ac.bbsrc.tgac.miso.webapp.util.BulkCreateTableBackend;
 import uk.ac.bbsrc.tgac.miso.webapp.util.BulkEditTableBackend;
 import uk.ac.bbsrc.tgac.miso.webapp.util.BulkPropagateTableBackend;
 
+@SuppressWarnings("squid:S3776")
 @Controller
 @RequestMapping("/sample")
 @SessionAttributes("sample")
