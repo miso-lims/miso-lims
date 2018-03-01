9c7f4afb9efeaaa8c17847fcc172c72a4db716d8
Noah Reinhardt <Noah.Reinhardt@oicr.on.ca>
2018-03-01 11:20:21 -0500

    Create backend for storing workflow progress

---
+++ core/src/main/java/uk/ac/bbsrc/tgac/miso/core/data/workflow/Workflow.java
@@ -0,0 +1,7 @@
+package uk.ac.bbsrc.tgac.miso.core.data.workflow;
+
+public interface Workflow {
+  enum WorkflowName {
+    LOADSEQUENCER
+  }
+}
---
+++ core/src/main/java/uk/ac/bbsrc/tgac/miso/core/store/ProgressStore.java
@@ -0,0 +1,13 @@
+package uk.ac.bbsrc.tgac.miso.core.store;
+
+import java.util.List;
+
+import uk.ac.bbsrc.tgac.miso.core.data.workflow.Progress;
+
+public interface ProgressStore {
+  Progress get(long id);
+
+  List<Progress> listByUserId(long id);
+
+  Progress save(Progress progress);
+}
