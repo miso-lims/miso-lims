efe7ab5b4417e7b20999802104f9902bb7cf274c
Dillan Cooke <dillan.cooke@oicr.on.ca>
2018-05-24 15:18:53 -0400

    GLT-2318: added freezer models

---
+++ core/src/main/java/uk/ac/bbsrc/tgac/miso/core/data/impl/StorageLocation.java
@@ -0,0 +1,142 @@
+package uk.ac.bbsrc.tgac.miso.core.data.impl;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.google.common.collect.Sets;
+
+import uk.ac.bbsrc.tgac.miso.core.data.Box;
+
+@Entity
+public class StorageLocation {
+
+  public enum BoxStorageAmount {
+    NONE, SINGLE, MULTIPLE;
+  }
+
+  public enum LocationUnit {
+    ROOM("Room", Collections.emptySet(), BoxStorageAmount.NONE),
+    FREEZER("Freezer", Sets.newHashSet(ROOM), BoxStorageAmount.NONE),
+    SHELF("Shelf", Sets.newHashSet(FREEZER), BoxStorageAmount.NONE),
+    RACK("Rack", Sets.newHashSet(SHELF), BoxStorageAmount.NONE),
+    STACK("Stack", Sets.newHashSet(FREEZER, SHELF, RACK), BoxStorageAmount.NONE),
+    STACK_POSITION("Slot", Sets.newHashSet(STACK), BoxStorageAmount.SINGLE),
+    LOOSE_STORAGE("Loose Storage", Sets.newHashSet(SHELF), BoxStorageAmount.MULTIPLE);
+
+    private final String displayName;
+    private final Set<LocationUnit> parents;
+    private final BoxStorageAmount boxStorageAmount;
+
+    private LocationUnit(String displayName, Set<LocationUnit> parents, BoxStorageAmount boxStorageAmount) {
+      this.displayName = displayName;
+      this.parents = Collections.unmodifiableSet(parents);
+      this.boxStorageAmount = boxStorageAmount;
+    }
+
+    public String getDisplayName() {
+      return displayName;
+    }
+
+    public Set<LocationUnit> getAcceptableParents() {
+      return parents;
+    }
+
+    public BoxStorageAmount getBoxStorageAmount() {
+      return boxStorageAmount;
+    }
+  }
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long locationId;
+
+  @ManyToOne
+  @JoinColumn(name = "parentLocationId", nullable = true)
+  private StorageLocation parentLocation;
+
+  @OneToMany(mappedBy = "parentLocation")
+  private Set<StorageLocation> childLocations;
+
+  @Enumerated(EnumType.STRING)
+  private LocationUnit locationUnit;
+
+  private String alias;
+
+  private String identificationBarcode;
+
+  @OneToMany(targetEntity = BoxImpl.class, mappedBy = "storageLocation")
+  private Set<Box> boxes;
+
+  public long getLocationId() {
+    return locationId;
+  }
+
+  public void setLocationId(long locationId) {
+    this.locationId = locationId;
+  }
+
+  public StorageLocation getParentLocation() {
+    return parentLocation;
+  }
+
+  public void setParentLocation(StorageLocation parentLocation) {
+    this.parentLocation = parentLocation;
+    if (parentLocation.childLocations == null) {
+      parentLocation.childLocations = new HashSet<>();
+    }
+    parentLocation.childLocations.add(this);
+  }
+
+  public Set<StorageLocation> getChildLocations() {
+    return childLocations;
+  }
+
+  public LocationUnit getLocationUnit() {
+    return locationUnit;
+  }
+
+  public void setLocationUnit(LocationUnit locationUnit) {
+    this.locationUnit = locationUnit;
+  }
+
+  public String getAlias() {
+    return alias;
+  }
+
+  public void setAlias(String alias) {
+    this.alias = alias;
+  }
+
+  public String getIdentificationBarcode() {
+    return identificationBarcode;
+  }
+
+  public void setIdentificationBarcode(String identificationBarcode) {
+    this.identificationBarcode = identificationBarcode;
+  }
+
+  public Set<Box> getBoxes() {
+    return boxes;
+  }
+
+  public void setBoxes(Set<Box> boxes) {
+    this.boxes = boxes;
+  }
+
+  public String getDisplayLocation() {
+    return (getParentLocation() == null ? "" : getParentLocation().getDisplayLocation() + ", ")
+        + getLocationUnit().getDisplayName() + " " + getAlias();
+  }
+
+}
