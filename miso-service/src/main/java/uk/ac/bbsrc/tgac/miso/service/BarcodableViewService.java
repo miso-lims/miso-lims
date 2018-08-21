f9df11a368b5cd64d9fc3b7bb3dcb7c22f3ef772
Kashif-S <kashif.sayeed@oicr.on.ca>
2018-08-21 16:21:22 -0400

    Add search by alias to workflows
    Jira: GLT-2364

--- miso-service/src/main/java/uk/ac/bbsrc/tgac/miso/service/BarcodableViewService.java
+++ miso-service/src/main/java/uk/ac/bbsrc/tgac/miso/service/BarcodableViewService.java
@@ -1,11 +1,11 @@
 package uk.ac.bbsrc.tgac.miso.service;
 
-import uk.ac.bbsrc.tgac.miso.core.data.Barcodable;
-import uk.ac.bbsrc.tgac.miso.core.data.impl.view.BarcodableView;
-
 import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
+
+import uk.ac.bbsrc.tgac.miso.core.data.Barcodable;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.view.BarcodableView;
 
 public interface BarcodableViewService {
   List<BarcodableView> searchByBarcode(String barcode);
@@ -24,4 +24,8 @@ public interface BarcodableViewService {
    * Incorrect usage (e.g. view.id.targetType != SAMPLE && Sample s = getEntity(view)) will result in a ClassCastException
    */
   <T extends Barcodable> T getEntity(BarcodableView view) throws IOException;
+
+  List<BarcodableView> searchByAlias(String alias);
+
+  List<BarcodableView> searchByAlias(String alias, Collection<Barcodable.EntityType> typeFilter);
 }
