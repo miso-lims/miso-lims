751954841980de65dc161db30f6cf928cdfdcc98
Andre Masella <andre.masella@oicr.on.ca>
2018-09-18 14:35:55 -0400

    Add BCL2FASTQ sample sheet

--- core/src/main/java/uk/ac/bbsrc/tgac/miso/core/util/SampleSheet.java
+++ core/src/main/java/uk/ac/bbsrc/tgac/miso/core/util/SampleSheet.java
@@ -1,6 +1,9 @@
 package uk.ac.bbsrc.tgac.miso.core.util;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -14,6 +17,43 @@ import uk.ac.bbsrc.tgac.miso.core.data.impl.view.PoolDilution;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.view.PoolableElementView;
 
 public enum SampleSheet {
+  BCL2FASTQ("BCL2FASTQ") {
+
+    @Override
+    public boolean allowedFor(Run run) {
+      return run instanceof IlluminaRun;
+    }
+
+    @Override
+    protected Stream<String> getColumns() {
+      return Stream.of("Sample_ID", "Sample_Name", "I7_Index_ID", "index", "I5_Index_ID", "index2");
+    }
+
+    @Override
+    protected String header(Run run) {
+      IlluminaRun r = (IlluminaRun) run;
+      String reads = "";
+      if (r.getSequencingParameters() != null) {
+        String readLength = Integer.toString(r.getSequencingParameters().getReadLength());
+        reads = String.format("[Reads]\n%s\n%s\n\n", readLength,
+            r.getSequencingParameters().isPaired() ? readLength : "");
+      }
+      return String.format("[Header]\nDate,%s\n\n%s[Data]\n", DateTimeFormatter.ISO_DATE.format(ZonedDateTime.now()), reads);
+    }
+
+    @Override
+    protected void makeColumns(Partition partition, PoolableElementView dilution, String userName, String[] output) {
+      output[0] = dilution.getLibraryName();
+      output[1] = dilution.getLibraryAlias();
+      Optional<Index> firstIndex = dilution.getIndices().stream().filter(i -> i.getPosition() == 1).findFirst();
+      output[2] = firstIndex.map(Index::getName).orElse("");
+      output[3] = firstIndex.map(Index::getSequence).orElse("");
+      Optional<Index> secondIndex = dilution.getIndices().stream().filter(i -> i.getPosition() == 1).findFirst();
+      output[4] = secondIndex.map(Index::getName).orElse("");
+      output[5] = secondIndex.map(Index::getSequence).orElse("");
+    }
+
+  },
   CASAVA_1_7("CASAVA 1.7") {
     @Override
     public boolean allowedFor(Run run) {
