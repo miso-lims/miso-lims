819eb6b4539761dc95792c13bbfed534aee41701
Andre Masella <andre.masella@oicr.on.ca>
2018-11-14 10:32:58 -0500

    Handle instruments which mangle the i5 index
    JIRA: GLT-2616

---
+++ core/src/main/java/uk/ac/bbsrc/tgac/miso/core/data/InstrumentDataManglingPolicy.java
@@ -0,0 +1,6 @@
+package uk.ac.bbsrc.tgac.miso.core.data;
+
+public enum InstrumentDataManglingPolicy {
+  I5_RC, // An Illumina sequencer which sequences the reverse complement of the reverse primer, rather than the primer
+  NONE
+}
--- pinery-miso/src/main/resources/ca/on/oicr/pinery/lims/miso/queryAllRunSamples.sql
+++ pinery-miso/src/main/resources/ca/on/oicr/pinery/lims/miso/queryAllRunSamples.sql
@@ -3,7 +3,11 @@ SELECT part.partitionId
   ,bc1.sequence barcode
   ,bc2.sequence barcode_two
   ,tr.alias targeted_sequencing 
-FROM _Partition part 
+  ,CASE WHEN scm.fallback THEN 'NONE' ELSE (SELECT DISTINCT dataManglingPolicy FROM InstrumentModel JOIN SequencingContainerModel_InstrumentModel ON SequencingContainerModel_InstrumentModel.instrumentModelId = InstrumentModel.instrumentModelId WHERE SequencingContainerModel_InstrumentModel.sequencingContainerModelId = scm.sequencingContainerModelId) END dataManglingPolicy
+FROM SequencingContainerModel scm
+JOIN SequencerPartitionContainer spc ON scm.sequencingContainerModelId = spc.sequencingContainerModelId
+JOIN SequencerPartitionContainer_Partition spcp ON spcp.container_containerId = spc.containerId
+JOIN _Partition part ON part.partitionId = spcp.partitions_partitionId
 JOIN Pool pool ON pool.poolId = part.pool_poolId 
 JOIN Pool_Dilution ele ON ele.pool_poolId = pool.poolId 
 JOIN LibraryDilution ld ON ld.dilutionId = ele.dilution_dilutionId 
