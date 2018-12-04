c88af1e847df1f973a3862e654d47c31c3c1e327
Dillan Cooke <dillan.cooke@oicr.on.ca>
2018-12-04 09:56:14 -0500

    fixup

--- miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/controller/ListRunsController.java
+++ miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/controller/ListRunsController.java
@@ -32,7 +32,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import uk.ac.bbsrc.tgac.miso.service.PlatformService;
+import uk.ac.bbsrc.tgac.miso.service.InstrumentModelService;
 import uk.ac.bbsrc.tgac.miso.webapp.util.TabbedListItemsPage;
 
 @Controller
@@ -40,11 +40,11 @@ public class ListRunsController {
   protected static final Logger log = LoggerFactory.getLogger(ListRunsController.class);
 
   @Autowired
-  private PlatformService platformService;
+  private InstrumentModelService instrumentModelService;
 
   @RequestMapping("/runs")
   public ModelAndView listRuns(ModelMap model) throws Exception {
-    return TabbedListItemsPage.createForPlatformType("run", platformService).list(model);
+    return TabbedListItemsPage.createForPlatformType("run", instrumentModelService).list(model);
   }
 
   @ModelAttribute("title")
