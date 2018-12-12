4afe80643bc2bab62465e9e7cc10dd761755dc71
Dillan Cooke <dillan.cooke@oicr.on.ca>
2018-12-12 11:03:34 -0500

    GLT-2652: added spreadsheet import function to bulk input tables

--- miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/controller/rest/HotRestController.java
+++ miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/controller/rest/HotRestController.java
@@ -1,8 +1,11 @@
 package uk.ac.bbsrc.tgac.miso.webapp.controller.rest;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.Response.Status;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
@@ -10,8 +13,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import uk.ac.bbsrc.tgac.miso.core.data.spreadsheet.DelimitedSpreadsheetWrapper;
+import uk.ac.bbsrc.tgac.miso.core.data.spreadsheet.ExcelSpreadsheetWrapper;
+import uk.ac.bbsrc.tgac.miso.core.data.spreadsheet.OpenDocumentSpreadsheetWrapper;
 import uk.ac.bbsrc.tgac.miso.core.data.spreadsheet.SpreadSheetFormat;
+import uk.ac.bbsrc.tgac.miso.core.data.spreadsheet.SpreadsheetWrapper;
 import uk.ac.bbsrc.tgac.miso.webapp.util.MisoWebUtils;
 
 @Controller
@@ -45,6 +54,73 @@ public class HotRestController extends RestController {
   public HttpEntity<byte[]> downloadSpreadsheet(@RequestParam(name = "format", required = true) String format,
       @RequestBody SpreadsheetDataDto dto, HttpServletResponse response) {
     return MisoWebUtils.generateSpreadsheet(dto.getHeaders(), dto.getRows(), SpreadSheetFormat.valueOf(format), response);
+  }
+
+  public static class ColumnDataDto {
+
+    private String heading;
+
+    private List<String> data = new ArrayList<>();
+
+    public ColumnDataDto(String heading) {
+      this.heading = heading;
+    }
+
+    public String getHeading() {
+      return heading;
+    }
+
+    public void setHeading(String heading) {
+      this.heading = heading;
+    }
+
+    public List<String> getData() {
+      return data;
+    }
+
+    public void setData(List<String> data) {
+      this.data = data;
+    }
+
+  }
+
+  @PostMapping("/import")
+  public @ResponseBody List<ColumnDataDto> importSpreadsheet(@RequestParam("file") MultipartFile file) throws IOException {
+    String extension = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".") + 1).toLowerCase();
+    SpreadsheetWrapper sheet = null;
+    if (extension.equals("xlsx")) {
+      sheet = new ExcelSpreadsheetWrapper(file.getInputStream());
+    } else if (extension.equals("ods")) {
+      sheet = new OpenDocumentSpreadsheetWrapper(file.getInputStream());
+    } else if (extension.equals("csv")) {
+      sheet = new DelimitedSpreadsheetWrapper(file.getInputStream());
+    } else {
+      throw new RestException("Unknown file extension: " + extension + ". Only xlsx, ods, and csv files are accepted", Status.BAD_REQUEST);
+    }
+
+    int rows = sheet.getRowCount();
+    if (rows < 2) {
+      throw new RestException("Spreadsheet contains no data to import", Status.BAD_REQUEST);
+    }
+    List<ColumnDataDto> columns = new ArrayList<>();
+    for (int i = 0; i < sheet.getColumnCount(); i++) {
+      try {
+        columns.add(new ColumnDataDto(sheet.getCellValue(0, i)));
+      } catch (IllegalArgumentException e) {
+        throw new RestException(String.format("Failed to parse heading cell at row 0, column %d", i), Status.BAD_REQUEST);
+      }
+    }
+    for (int rowNum = 1; rowNum < sheet.getRowCount(); rowNum++) {
+      for (int colNum = 0; colNum < sheet.getColumnCount(); colNum++) {
+        try {
+          columns.get(colNum).getData().add(sheet.getCellValue(rowNum, colNum));
+        } catch (IllegalArgumentException e) {
+          throw new RestException(String.format("Failed to parse value of cell at row %d, column %d", rowNum, colNum),
+              Status.BAD_REQUEST);
+        }
+      }
+    }
+    return columns;
   }
 
 }
--- miso-web/src/main/webapp/scripts/lims.js
+++ miso-web/src/main/webapp/scripts/lims.js
@@ -1100,14 +1100,15 @@ Utils.sorting = {
 };
 
 Utils.notes = {
-   addNote: function(entityType, entityId, internalOnly, text) {
-        Utils.ajaxWithDialog('Adding Note', 'POST', window.location.origin + '/miso/rest/note/' + entityType + '/' + entityId, {
-            internalOnly: internalOnly == 'on',
-            text: text
-          }, Utils.page.pageReload);
-   },
-   deleteNote: function(entityType, entityId, noteId) {
-        Utils.ajaxWithDialog('Deleting Note', 'DELETE', window.location.origin + '/miso/rest/note/' + entityType + '/' + entityId + '/' + noteId, null, Utils.page.pageReload);
-   },
+  addNote: function(entityType, entityId, internalOnly, text) {
+    Utils.ajaxWithDialog('Adding Note', 'POST', window.location.origin + '/miso/rest/note/' + entityType + '/' + entityId, {
+      internalOnly: internalOnly == 'on',
+      text: text
+    }, Utils.page.pageReload);
+  },
+  deleteNote: function(entityType, entityId, noteId) {
+    Utils.ajaxWithDialog('Deleting Note', 'DELETE', window.location.origin + '/miso/rest/note/' + entityType + '/' + entityId + '/'
+        + noteId, null, Utils.page.pageReload);
+  },
 
 };
