76aa66e0825d0aa2e68914a20903405ca4688006
Dillan Cooke <dillan.cooke@oicr.on.ca>
2019-01-08 15:55:29 -0500

    GP-1840: include lane QC data in Pinery (#1761)

--- pinery-miso/src/main/resources/ca/on/oicr/pinery/lims/miso/queryAllRunPositions.sql
+++ pinery-miso/src/main/resources/ca/on/oicr/pinery/lims/miso/queryAllRunPositions.sql
@@ -1,5 +1,7 @@
 SELECT p.partitionId
   ,p.partitionNumber
+  ,pqt.analysisSkipped AS analysis_skipped
+  ,pqt.description AS qc_status
   ,r_spc.Run_runId
   ,pool.alias AS pool_name
   ,pool.identificationBarcode AS pool_barcode
@@ -10,3 +12,5 @@ FROM _Partition AS p
 LEFT JOIN Pool pool ON pool.poolId = p.pool_poolId
 JOIN SequencerPartitionContainer_Partition AS spc_p ON spc_p.partitions_partitionId = p.partitionId
 JOIN Run_SequencerPartitionContainer AS r_spc ON r_spc.containers_containerId = spc_p.container_containerId
+LEFT JOIN Run_Partition_QC rpq ON rpq.runId = r_spc.Run_runId AND rpq.partitionId = p.partitionId
+LEFT JOIN PartitionQCType pqt ON pqt.partitionQcTypeId = rpq.partitionQcTypeId
