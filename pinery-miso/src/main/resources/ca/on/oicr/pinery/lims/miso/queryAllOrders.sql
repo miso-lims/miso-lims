faeb798136dc78c355ed0220ade0ebdb728a4442
Andre Masella <andre.masella@oicr.on.ca>
2019-01-25 12:18:42 -0500

    Don't export orders for empty pools

--- pinery-miso/src/main/resources/ca/on/oicr/pinery/lims/miso/queryAllOrders.sql
+++ pinery-miso/src/main/resources/ca/on/oicr/pinery/lims/miso/queryAllOrders.sql
@@ -1,3 +1,3 @@
 SELECT o.poolOrderId orderId, o.creationDate createdDate, o.createdBy createdById, 
 o.lastUpdated modifiedDate, o.updatedBy modifiedById, pool.platformType platform FROM PoolOrder o 
-JOIN Pool pool ON pool.poolId = o.poolId 
+JOIN Pool pool ON pool.poolId = o.poolId WHERE EXISTS(SELECT * FROM Pool_Dilution WHERE Pool_Dilution.pool_poolId = o.poolId)
