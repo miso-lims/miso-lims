ab77b76da5650245c2c4a5e99c2e988ca5c2ad64
Andre Masella <andre.masella@oicr.on.ca>
2019-02-13 12:03:16 -0500

    Adjust JTT7 label layouts
    JIRA: GLT-2722

--- core/src/main/java/uk/ac/bbsrc/tgac/miso/core/service/printing/Layout.java
+++ core/src/main/java/uk/ac/bbsrc/tgac/miso/core/service/printing/Layout.java
@@ -75,9 +75,9 @@ public enum Layout {
     @Override
     public LabelCanvas draw(Driver driver, Barcodable barcodable) {
       LabelCanvas label = driver.start(8.3, 4.08);
-      label.multilineText(0.1, 1, .8, 18, 2, Stream.of(//
+      label.multilineText(0.3, 1, .8, 18, 2, Stream.of(//
           new Pair<>(FontStyle.BOLD, barcodable.getAlias())));
-      label.multilineText(0.1, 3, .8, 12, 2, Stream.of(//
+      label.multilineText(0.3, 3, .8, 12, 2, Stream.of(//
           new Pair<>(FontStyle.REGULAR, LimsUtils.formatDate(barcodable.getBarcodeDate())), //
           new Pair<>(FontStyle.REGULAR, unescapeHtml(barcodable.getBarcodeExtraInfo()))));
       label.barcode2d(5.78, 3.8, 0.1, getBarcode(barcodable));
@@ -92,9 +92,9 @@ public enum Layout {
     @Override
     public LabelCanvas draw(Driver driver, Barcodable barcodable) {
       LabelCanvas label = driver.start(8.3, 4.08);
-      label.multilineText(0.1, 1, .8, 18, 2, Stream.of(//
+      label.multilineText(0.3, 1, .8, 18, 2, Stream.of(//
           new Pair<>(FontStyle.BOLD, barcodable.getAlias())));
-      label.multilineText(0.1, 3, .8, 12, 2, Stream.of(//
+      label.multilineText(0.3, 3, .8, 12, 2, Stream.of(//
           new Pair<>(FontStyle.REGULAR, LimsUtils.formatDate(barcodable.getBarcodeDate())), //
           new Pair<>(FontStyle.REGULAR, unescapeHtml(barcodable.getBarcodeSizeInfo()))));
       label.barcode2d(5.78, 3.8, 0.1, getBarcode(barcodable));
