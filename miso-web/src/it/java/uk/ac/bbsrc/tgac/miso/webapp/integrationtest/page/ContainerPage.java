00346d4e33061597d25794faad1d30953d4df1e7
Dillan Cooke <dillan.cooke@oicr.on.ca>
2019-03-27 15:44:30 -0400

    fixup tests

--- miso-web/src/it/java/uk/ac/bbsrc/tgac/miso/webapp/integrationtest/page/ContainerPage.java
+++ miso-web/src/it/java/uk/ac/bbsrc/tgac/miso/webapp/integrationtest/page/ContainerPage.java
@@ -11,23 +11,36 @@ import org.openqa.selenium.support.PageFactory;
 public class ContainerPage extends FormPage<ContainerPage.Field> {
 
   public static enum Field implements FormPage.FieldElement {
-    ID(By.id("containerId"), FieldType.LABEL), //
+    ID(By.id("id"), By.id("idLabel"), FieldType.LABEL), //
     SERIAL_NUMBER(By.id("identificationBarcode"), FieldType.TEXT), //
-    MODEL(By.id("model"), FieldType.LABEL), //
-    CLUSTERING_KIT(By.id("clusteringKit"), FieldType.DROPDOWN), //
-    MULTIPLEXING_KIT(By.id("multiplexingKit"), FieldType.DROPDOWN);
+    MODEL(By.id("model.id"), By.id("model.idLabel"), FieldType.LABEL), //
+    CLUSTERING_KIT(By.id("clusteringKitId"), FieldType.DROPDOWN), //
+    MULTIPLEXING_KIT(By.id("multiplexingKitId"), FieldType.DROPDOWN);
 
     private final By selector;
+    private final By labelSelector;
     private final FieldType type;
 
     private Field(By selector, FieldType type) {
       this.selector = selector;
+      this.labelSelector = null;
+      this.type = type;
+    }
+
+    private Field(By selector, By labelSelector, FieldType type) {
+      this.selector = selector;
+      this.labelSelector = labelSelector;
       this.type = type;
     }
 
     @Override
     public By getSelector() {
       return selector;
+    }
+
+    @Override
+    public By getLabelSelector() {
+      return labelSelector;
     }
 
     @Override
