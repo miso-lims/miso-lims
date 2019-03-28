fbe0557b678c4da4b1e2c2bb29f1409204bbcbcf
Dillan Cooke <dillan.cooke@oicr.on.ca>
2019-03-28 14:49:58 -0400

    fix

--- sqlstore/src/main/resources/db/migration/V8502__fix_promethion_models.sql
+++ sqlstore/src/main/resources/db/migration/V8502__fix_promethion_models.sql
@@ -4,7 +4,7 @@ CREATE PROCEDURE TempFixModel(iOldName varchar(10), iNewName varchar(10)) BEGIN
   IF EXISTS (SELECT 1 FROM SequencingContainerModel WHERE alias = iOldName) THEN
     UPDATE SequencingContainerModel SET alias = iNewName WHERE alias = iOldName;
   ELSE
-    INSERT INTO SequencingContainerModel (alias, partitionCount, platformType) VALUES (iNewName, 1, 'ILLUMINA');
+    INSERT INTO SequencingContainerModel (alias, partitionCount, platformType) VALUES (iNewName, 1, 'OXFORDNANOPORE');
     INSERT INTO SequencingContainerModel_InstrumentModel (sequencingContainerModelId, instrumentModelId)
     VALUES (
       (SELECT sequencingContainerModelId FROM SequencingContainerModel WHERE alias = iNewName),
