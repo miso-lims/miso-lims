4542f558c9e75a31109fbbd393cd08c3b3023cb7
Heather Armstrong <heather.armstrong@oicr.on.ca>
2019-04-02 14:46:31 -0400

    Prepared for release

--- sqlstore/src/main/resources/db/migration/V0810__auto_main.sql
+++ sqlstore/src/main/resources/db/migration/V0810__auto_main.sql
@@ -1,3 +1,5 @@
+-- fix_promethion_pos
+
 -- StartNoTest
 DELIMITER //
 CREATE PROCEDURE TempFixPos(iOldName varchar(10), iNewName varchar(10)) BEGIN
@@ -87,3 +89,30 @@ DROP PROCEDURE TempAddPos;
 SELECT instrumentModelId INTO @promethion FROM InstrumentModel WHERE alias = 'PromethION';
 DELETE FROM InstrumentPosition WHERE instrumentModelId = @promethion AND alias LIKE 'P___\__';
 -- EndNoTest
+
+
+-- fix_promethion_models
+
+-- StartNoTest
+DELIMITER //
+CREATE PROCEDURE TempFixModel(iOldName varchar(10), iNewName varchar(10)) BEGIN
+  IF EXISTS (SELECT 1 FROM SequencingContainerModel WHERE alias = iOldName) THEN
+    UPDATE SequencingContainerModel SET alias = iNewName WHERE alias = iOldName;
+  ELSE
+    INSERT INTO SequencingContainerModel (alias, partitionCount, platformType) VALUES (iNewName, 1, 'OXFORDNANOPORE');
+    INSERT INTO SequencingContainerModel_InstrumentModel (sequencingContainerModelId, instrumentModelId)
+    VALUES (
+      (SELECT sequencingContainerModelId FROM SequencingContainerModel WHERE alias = iNewName),
+      (SELECT instrumentModelId FROM InstrumentModel WHERE alias = 'PromethION')
+    );
+  END IF;
+END//
+DELIMITER ;
+
+CALL TempFixModel('PRO-001', 'FLO-PRO001');
+CALL TempFixModel('PRO-002', 'FLO-PRO002');
+
+DROP PROCEDURE TempFixModel;
+-- EndNoTest
+
+
