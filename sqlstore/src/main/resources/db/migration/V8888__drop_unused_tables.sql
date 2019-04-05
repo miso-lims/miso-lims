523a7349425b3ef5d818c406ae211b6b467d4b19
Andre Masella <andre.masella@oicr.on.ca>
2019-04-05 11:29:00 -0400

    Drop tables not used any more

---
+++ sqlstore/src/main/resources/db/migration/V8888__drop_unused_tables.sql
@@ -0,0 +1,9 @@
+DROP TABLE ProjectOverview_Note;
+DROP TABLE ProjectOverview_Sample;
+DROP TABLE ProjectOverview;
+DROP TABLE Project_Request;
+DROP TABLE Run_Flowcell;
+DROP TABLE Workflow_State;
+DROP TABLE emPCRDilution;
+DROP TABLE emPCR;
+DROP TABLE persistent_logins;
