e8d72081b82694315ec5c134f8aa4a22d4dfcb68
Alexis Varsava <Alexis.Varsava@oicr.on.ca>
2019-05-21 14:07:40 -0400

    Add runType to all SequencingParameters with matching name

--- sqlstore/src/main/resources/db/migration/V8888__add_run_type.sql
+++ sqlstore/src/main/resources/db/migration/V8888__add_run_type.sql
@@ -1,6 +1,6 @@
 ALTER TABLE SequencingParameters ADD COLUMN runType varchar(255);
 
-UPDATE SequencingParameters SET runType = 'ctc' WHERE parametersId = 101;
-UPDATE SequencingParameters SET runType = 'platform_qc' WHERE parametersId = 102;
-UPDATE SequencingParameters SET runType = 'sequencing_run' WHERE parametersId = 103;
-UPDATE SequencingParameters SET runType = 'mux_scan' WHERE parametersId = 104;
+UPDATE SequencingParameters SET runType = 'ctc' WHERE name = 'Configuration Test Cell';
+UPDATE SequencingParameters SET runType = 'platform_qc' WHERE name = 'Platform Quality Control';
+UPDATE SequencingParameters SET runType = 'sequencing_run' WHERE name = 'Sequencing Run';
+UPDATE SequencingParameters SET runType = 'mux_scan' WHERE name = 'Control Experiment';
