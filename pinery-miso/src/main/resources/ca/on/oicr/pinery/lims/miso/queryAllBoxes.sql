a2244246fe80ea0cfeef4e6ae646c0bd4ace11a8
Alexis Varsava <Alexis.Varsava@oicr.on.ca>
2019-07-15 13:55:23 -0400

    Pinery test fix

--- pinery-miso/src/main/resources/ca/on/oicr/pinery/lims/miso/queryAllBoxes.sql
+++ pinery-miso/src/main/resources/ca/on/oicr/pinery/lims/miso/queryAllBoxes.sql
@@ -1,5 +1,5 @@
 SELECT bp.targetId, bp.targetType, bp.position, b.boxId, b.alias, b.locationBarcode, 
-b.description, bs.rows, bs.columns 
+b.description, bs.boxSizeRows, bs.boxSizeColumns
 FROM BoxPosition bp 
 RIGHT JOIN Box b ON b.boxId = bp.boxId 
 JOIN BoxSize bs ON bs.boxSizeId = b.boxSizeId
