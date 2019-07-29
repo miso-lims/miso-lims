6552e1bb066779e7efe19f9d9adcf118075f6d66
Heather Armstrong <heather.armstrong@oicr.on.ca>
2019-07-29 14:58:33 -0400

    fixup ITs

--- miso-web/src/it/java/uk/ac/bbsrc/tgac/miso/webapp/integrationtest/ListTablesIT.java
+++ miso-web/src/it/java/uk/ac/bbsrc/tgac/miso/webapp/integrationtest/ListTablesIT.java
@@ -247,7 +247,7 @@ public class ListTablesIT extends AbstractIT {
 
   @Test
   public void testListLibrariesWarnings() throws Exception {
-    testWarningNormal(ListTarget.LIBRARIES, "LIB901", "(NEGATIVE VOLUME)", Columns.WARNINGS);
+    testWarningNormal(ListTarget.LIBRARIES, "LIB901", "(Negative Volume)", Columns.WARNINGS);
   }
 
   @Test
@@ -277,9 +277,9 @@ public class ListTablesIT extends AbstractIT {
   @Test
   public void testListPoolsWarnings() throws Exception {
     testWarningTabbed(ListTarget.POOLS, "no indices", "(MISSING INDEX)", Columns.DESCRIPTION);
-    testWarningTabbed(ListTarget.POOLS, "similar index", "(NEAR-DUPLICATE INDICES)", Columns.DESCRIPTION);
+    testWarningTabbed(ListTarget.POOLS, "similar index", "(Near-Duplicate Indices)", Columns.DESCRIPTION);
     testWarningTabbed(ListTarget.POOLS, "same index", "(DUPLICATE INDICES)", Columns.DESCRIPTION);
-    testWarningTabbed(ListTarget.POOLS, "low quality library", "(LOW QUALITY LIBRARIES)", Columns.DESCRIPTION);
+    testWarningTabbed(ListTarget.POOLS, "low quality library", "(Low Quality Libraries)", Columns.DESCRIPTION);
   }
 
   @Test
@@ -316,9 +316,9 @@ public class ListTablesIT extends AbstractIT {
   @Test
   public void testListOrdersWarnings() throws Exception {
     testWarningTabbed(ListTarget.ORDERS_ALL, "no indices", "(MISSING INDEX)", Columns.POOL_DESCRIPTION);
-    testWarningTabbed(ListTarget.ORDERS_ALL, "similar index", "(NEAR-DUPLICATE INDICES)", Columns.POOL_DESCRIPTION);
+    testWarningTabbed(ListTarget.ORDERS_ALL, "similar index", "(Near-Duplicate Indices)", Columns.POOL_DESCRIPTION);
     testWarningTabbed(ListTarget.ORDERS_ALL, "same index", "(DUPLICATE INDICES)", Columns.POOL_DESCRIPTION);
-    testWarningTabbed(ListTarget.ORDERS_ALL, "low quality library", "(LOW QUALITY LIBRARIES)", Columns.POOL_DESCRIPTION);
+    testWarningTabbed(ListTarget.ORDERS_ALL, "low quality library", "(Low Quality Libraries)", Columns.POOL_DESCRIPTION);
   }
 
   @Test
