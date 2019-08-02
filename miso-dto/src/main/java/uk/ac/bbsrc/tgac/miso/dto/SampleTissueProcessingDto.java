b9e24715c5c69c81f0dba1cc69897076da2e5e2d
Alexis Varsava <Alexis.Varsava@oicr.on.ca>
2019-08-02 14:42:22 -0400

    Remove one more annotation

--- miso-dto/src/main/java/uk/ac/bbsrc/tgac/miso/dto/SampleTissueProcessingDto.java
+++ miso-dto/src/main/java/uk/ac/bbsrc/tgac/miso/dto/SampleTissueProcessingDto.java
@@ -1,11 +1,9 @@
 package uk.ac.bbsrc.tgac.miso.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
 import uk.ac.bbsrc.tgac.miso.core.data.SampleTissueProcessing;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonTypeName(value = SampleTissueProcessing.CATEGORY_NAME)
 public class SampleTissueProcessingDto extends SampleTissueDto {
 
