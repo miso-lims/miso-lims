b659bc5301a66a46bc113e02ad1f1cf94e11da88
Alexis Varsava <Alexis.Varsava@oicr.on.ca>
2019-08-02 09:57:14 -0400

    Adjust error msg, release notes

--- miso-service/src/main/java/uk/ac/bbsrc/tgac/miso/service/impl/DefaultPoolService.java
+++ miso-service/src/main/java/uk/ac/bbsrc/tgac/miso/service/impl/DefaultPoolService.java
@@ -298,8 +298,9 @@ public class DefaultPoolService implements PoolService, PaginatedDataSource<Pool
     Set<String> bcIndices = getAllBadIndices(beforeChange);
 
     if(indices.size() > bcIndices.size()){ // If this change introduces new conflicts
-      String errorMessage = "Pools may not contain Library Aliquots with indices with 2 or fewer positions of " +
-              "difference, please address the following conflicts: ";
+      String errorMessage = String.format("Pools may not contain Library Aliquots with indices with %d or " +
+              "fewer positions of difference, please address the following conflicts: ",
+              indexChecker.getWarningMismatches());
       indices.removeAll(bcIndices);
 
       for (String index : indices){
