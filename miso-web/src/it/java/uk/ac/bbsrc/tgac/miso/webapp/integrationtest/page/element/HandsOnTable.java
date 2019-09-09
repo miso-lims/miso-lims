12fb833fbe191caecf449d0c9f0e69eb8c521b73
Alexis Varsava <Alexis.Varsava@oicr.on.ca>
2019-09-09 10:49:11 -0400

    ???

--- miso-web/src/it/java/uk/ac/bbsrc/tgac/miso/webapp/integrationtest/page/element/HandsOnTable.java
+++ miso-web/src/it/java/uk/ac/bbsrc/tgac/miso/webapp/integrationtest/page/element/HandsOnTable.java
@@ -12,10 +12,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.google.common.base.CharMatcher;
 import com.google.common.collect.Lists;
@@ -51,8 +54,9 @@ public class HandsOnTable extends AbstractElement {
   public HandsOnTable(WebDriver driver) {
     super(driver);
     PageFactory.initElements(driver, this);
+    driver.manage().window().maximize();
     setColumnHeadings();
-    activateColumns();
+    activateColumns(driver);
     setColumnHeadings();
     this.inputRows = hotContainer.findElements(inputRowsSelector);
     this.lockedRows = hotContainer.findElements(lockedRowsSelector);
@@ -64,8 +68,12 @@ public class HandsOnTable extends AbstractElement {
             .collect(Collectors.toList());
   }
 
-  private void activateColumns(){
-    hotContainer.findElements(inputCellSelector).stream().forEach(WebElement::click);
+  private void activateColumns(WebDriver driver){
+    JavascriptExecutor executor = (JavascriptExecutor)driver;
+    List<WebElement> cells = hotContainer.findElements(inputCellSelector);
+    for(WebElement cell: cells){
+        executor.executeScript("arguments[0].click()", cell);
+    }
   }
 
   public List<String> getColumnHeadings() {
