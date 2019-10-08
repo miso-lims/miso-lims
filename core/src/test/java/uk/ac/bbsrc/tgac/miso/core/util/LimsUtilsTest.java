ccfb4e506238baf2cfd509e017a5dcbe1be45988
Dillan Cooke <dillan.cooke@oicr.on.ca>
2019-10-08 09:17:31 -0400

    GLT-3013: include 1 decimal place on labels for vol/conc < 10

--- core/src/test/java/uk/ac/bbsrc/tgac/miso/core/util/LimsUtilsTest.java
+++ core/src/test/java/uk/ac/bbsrc/tgac/miso/core/util/LimsUtilsTest.java
@@ -2,9 +2,12 @@ package uk.ac.bbsrc.tgac.miso.core.util;
 
 import static org.junit.Assert.*;
 
+import java.math.BigDecimal;
+
 import org.junit.Test;
 
 import uk.ac.bbsrc.tgac.miso.core.data.SampleSlide;
+import uk.ac.bbsrc.tgac.miso.core.data.VolumeUnit;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.SampleSlideImpl;
 
 public class LimsUtilsTest {
@@ -26,4 +29,27 @@ public class LimsUtilsTest {
     String notNullString = "not null";
     assertEquals(notNullString, LimsUtils.nullifyStringIfBlank("not null"));
   }
+
+  @Test
+  public void testGetDecimalPlacesToDisplay() throws Exception {
+    assertEquals(0, LimsUtils.getDecimalPlacesToDisplay(null));
+    assertEquals(0, LimsUtils.getDecimalPlacesToDisplay(BigDecimal.TEN));
+    assertEquals(0, LimsUtils.getDecimalPlacesToDisplay(new BigDecimal("23.45")));
+    assertEquals(0, LimsUtils.getDecimalPlacesToDisplay(new BigDecimal("-12.34")));
+    assertEquals(1, LimsUtils.getDecimalPlacesToDisplay(BigDecimal.ZERO));
+    assertEquals(1, LimsUtils.getDecimalPlacesToDisplay(BigDecimal.ONE));
+    assertEquals(1, LimsUtils.getDecimalPlacesToDisplay(new BigDecimal("1.23")));
+    assertEquals(1, LimsUtils.getDecimalPlacesToDisplay(new BigDecimal("-2.34")));
+  }
+
+  @Test
+  public void testMakeFormatString() throws Exception {
+    testMakeFormatString("1.2", new BigDecimal("1.23"), VolumeUnit.MICROLITRES.getUnits());
+    testMakeFormatString("23", new BigDecimal("23.45"), VolumeUnit.MICROLITRES.getUnits());
+  }
+
+  private void testMakeFormatString(String expected, BigDecimal input, String units) {
+    assertEquals(expected + units, String.format(LimsUtils.makeFormatString(input), input, units));
+  }
+
 }
