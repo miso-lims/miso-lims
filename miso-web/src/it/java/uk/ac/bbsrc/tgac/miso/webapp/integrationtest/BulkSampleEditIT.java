d43fa2a2ff8baf3b0898f90234d57774a801cad3
Dillan Cooke <dillan.cooke@oicr.on.ca>
2019-11-08 11:32:35 -0500

    fixup tests

--- miso-web/src/it/java/uk/ac/bbsrc/tgac/miso/webapp/integrationtest/BulkSampleEditIT.java
+++ miso-web/src/it/java/uk/ac/bbsrc/tgac/miso/webapp/integrationtest/BulkSampleEditIT.java
@@ -34,12 +34,13 @@ public class BulkSampleEditIT extends AbstractBulkSampleIT {
       SamColumns.TISSUE_ORIGIN, SamColumns.TISSUE_TYPE, SamColumns.PASSAGE_NUMBER, SamColumns.TIMES_RECEIVED, SamColumns.TUBE_NUMBER,
       SamColumns.LAB, SamColumns.SECONDARY_ID, SamColumns.TISSUE_MATERIAL, SamColumns.REGION);
 
-  private static final Set<String> slideColumns = Sets.newHashSet(SamColumns.RECEIVE_DATE, SamColumns.INITIAL_SLIDES, SamColumns.SLIDES,
-      SamColumns.DISCARDS, SamColumns.THICKNESS, SamColumns.STAIN);
+  private static final Set<String> slideColumns = Sets.newHashSet(SamColumns.RECEIVE_DATE, SamColumns.REQUISITION_ID,
+      SamColumns.INITIAL_SLIDES, SamColumns.SLIDES, SamColumns.DISCARDS, SamColumns.THICKNESS, SamColumns.STAIN);
 
-  private static final Set<String> curlsColumns = Sets.newHashSet(SamColumns.RECEIVE_DATE);
+  private static final Set<String> curlsColumns = Sets.newHashSet(SamColumns.RECEIVE_DATE, SamColumns.REQUISITION_ID);
 
-  private static final Set<String> tissuePieceColumns = Sets.newHashSet(SamColumns.RECEIVE_DATE, SamColumns.PIECE_TYPE, SamColumns.SLIDES_CONSUMED);
+  private static final Set<String> tissuePieceColumns = Sets.newHashSet(SamColumns.RECEIVE_DATE, SamColumns.REQUISITION_ID,
+      SamColumns.PIECE_TYPE, SamColumns.SLIDES_CONSUMED);
 
   @Before
   public void setup() {
