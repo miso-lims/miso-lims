4200d1c6b2ce2314443c1b4127964285f9d81f32
Dillan Cooke <dillan.cooke@oicr.on.ca>
2020-01-17 08:28:45 -0500

    GLT-3071: export run purpose in Pinery-MISO

--- pinery-miso/src/main/java/ca/on/oicr/pinery/lims/miso/MisoRunSample.java
+++ pinery-miso/src/main/java/ca/on/oicr/pinery/lims/miso/MisoRunSample.java
@@ -4,7 +4,16 @@ import ca.on.oicr.pinery.lims.DefaultRunSample;
 
 public class MisoRunSample extends DefaultRunSample {
 
+  private Integer runId;
   private Integer partitionId;
+
+  public Integer getRunId() {
+    return runId;
+  }
+
+  public void setRunId(Integer runId) {
+    this.runId = runId;
+  }
 
   public Integer getPartitionId() {
     return partitionId;
@@ -18,6 +27,7 @@ public class MisoRunSample extends DefaultRunSample {
   public int hashCode() {
     final int prime = 31;
     int result = super.hashCode();
+    result = prime * result + ((runId == null) ? 0 : runId.hashCode());
     result = prime * result + ((partitionId == null) ? 0 : partitionId.hashCode());
     return result;
   }
@@ -28,6 +38,9 @@ public class MisoRunSample extends DefaultRunSample {
     if (!super.equals(obj)) return false;
     if (getClass() != obj.getClass()) return false;
     MisoRunSample other = (MisoRunSample) obj;
+    if (runId == null) {
+      if (other.runId != null) return false;
+    } else if (!runId.equals(other.runId)) return false;
     if (partitionId == null) {
       if (other.partitionId != null) return false;
     } else if (!partitionId.equals(other.partitionId)) return false;
