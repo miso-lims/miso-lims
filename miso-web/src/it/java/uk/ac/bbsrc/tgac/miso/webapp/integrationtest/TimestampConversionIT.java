4ffae4d369a4752bf24045fb827f53fdcbe8b78a
Dillan Cooke <dillan.cooke@oicr.on.ca>
2020-02-26 14:56:48 -0500

    GLT-3160: added tests for UI/DB time zone conversions (#2196)

---
+++ miso-web/src/it/java/uk/ac/bbsrc/tgac/miso/webapp/integrationtest/TimestampConversionIT.java
@@ -0,0 +1,79 @@
+package uk.ac.bbsrc.tgac.miso.webapp.integrationtest;
+
+import static org.junit.Assert.*;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TimeZone;
+
+import org.hibernate.type.StringType;
+import org.junit.Before;
+import org.junit.Test;
+
+import uk.ac.bbsrc.tgac.miso.webapp.integrationtest.page.LibraryPage;
+import uk.ac.bbsrc.tgac.miso.webapp.integrationtest.page.TransferPage;
+import uk.ac.bbsrc.tgac.miso.webapp.integrationtest.page.TransferPage.Field;
+import uk.ac.bbsrc.tgac.miso.webapp.integrationtest.page.element.DataTable;
+
+public class TimestampConversionIT extends AbstractIT {
+
+  @Before
+  public void setup() {
+    login();
+  }
+
+  @Test
+  public void verifyConfiguredTimezones() {
+    assertEquals(EASTERN_TIME_ZONE, TimeZone.getDefault());
+
+    Object[] results = (Object[]) getSession().createSQLQuery("SELECT @@session.time_zone, @@system_time_zone")
+        .addScalar("@@session.time_zone", StringType.INSTANCE)
+        .addScalar("@@system_time_zone", StringType.INSTANCE)
+        .uniqueResult();
+
+    assertEquals("Session should default to system time zone", "SYSTEM", results[0]);
+    assertEquals("System time zone should be UTC", "UTC", results[1]);
+  }
+
+  @Test
+  public void testUserEntered() {
+    // Goal: verify that a time displayed after saving is the same as the time entered by the user
+    TransferPage page = TransferPage.getForCreateWithSamples(getDriver(), getBaseUrl(), Collections.singleton(2L));
+    Map<Field, String> fields = new LinkedHashMap<>();
+    fields.put(Field.TRANSFER_TIME, "2020-02-20 12:48:00");
+    fields.put(Field.SENDER_GROUP, "TestGroup1");
+    fields.put(Field.RECIPIENT, "Random Passerby");
+    page.setFields(fields);
+
+    TransferPage page2 = page.save();
+    assertEquals(fields.get(Field.TRANSFER_TIME), page2.getField(Field.TRANSFER_TIME));
+  }
+
+  @Test
+  public void testGenerated() throws ParseException {
+    // Goal: verify that a service-generated timestamp is displayed in the local timezone
+    DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    Date now = new Date();
+
+    LibraryPage page = LibraryPage.get(getDriver(), getBaseUrl(), 1L);
+    DataTable changes = page.getChangeLogTable();
+    for (String changeTimeString : changes.getColumnValues("Time")) {
+      Date changeTime = formatter.parse(changeTimeString);
+      assertTrue("Existing changes should be more than 5 minutes in the past", now.getTime() - changeTime.getTime() > 300000);
+    }
+
+    page.setField(LibraryPage.Field.DESCRIPTION, "updated description");
+
+    LibraryPage page2 = page.save();
+    DataTable changes2 = page2.getChangeLogTable();
+    String latestString = changes2.getColumnValues("Time").stream().max(String::compareTo).orElse(null);
+    Date latest = formatter.parse(latestString);
+    assertTrue("New change should show approximately current time", Math.abs(latest.getTime() - now.getTime()) < 300000L);
+  }
+
+}
---
+++ miso-web/src/it/java/uk/ac/bbsrc/tgac/miso/webapp/integrationtest/page/TransferPage.java
@@ -0,0 +1,70 @@
+package uk.ac.bbsrc.tgac.miso.webapp.integrationtest.page;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleContains;
+
+import java.util.Collection;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.google.common.base.Joiner;
+
+public class TransferPage extends FormPage<TransferPage.Field> {
+
+  public static enum Field implements FormPage.FieldElement {
+    ID(By.id("transferForm_id"), By.id("transferForm_idLabel")), //
+    TRANSFER_TIME(By.id("transferForm_transferTime")), //
+    SENDER_GROUP(By.id("transferForm_senderGroupId")), //
+    RECIPIENT(By.id("transferForm_recipient")), //
+    RECIPIENT_GROUP(By.id("transferForm_recipientGroupId"));
+
+    private final By selector;
+    private final By labelSelector;
+
+    private Field(By selector) {
+      this.selector = selector;
+      this.labelSelector = null;
+    }
+
+    private Field(By selector, By labelSelector) {
+      this.selector = selector;
+      this.labelSelector = labelSelector;
+    }
+
+    @Override
+    public By getSelector() {
+      return selector;
+    }
+
+    @Override
+    public By getLabelSelector() {
+      return labelSelector;
+    }
+  }
+
+  @FindBy(id = "save")
+  private WebElement saveButton;
+
+  public TransferPage(WebDriver driver) {
+    super(driver, "transferForm");
+    PageFactory.initElements(driver, this);
+    waitWithTimeout().until(titleContains("Transfer "));
+  }
+
+  public static TransferPage getForCreateWithSamples(WebDriver driver, String baseUrl, Collection<Long> sampleIds) {
+    String ids = Joiner.on(',').join(sampleIds);
+    driver.get(baseUrl + "miso/transfer/new?sampleIds=" + ids);
+    return new TransferPage(driver);
+  }
+
+  public TransferPage save() {
+    WebElement html = getHtmlElement();
+    saveButton.click();
+    waitForPageRefresh(html);
+    return new TransferPage(getDriver());
+  }
+
+}
