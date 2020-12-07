5d8f4444cb905784118418616afb3967a2697280
Dillan Cooke <dillan.cooke@oicr.on.ca>
2020-12-07 11:25:57 -0500

    GLT-3401: added more QC and project info to Pinery-MISO

---
+++ pinery-miso/src/main/java/ca/on/oicr/pinery/lims/miso/converters/QcConverter.java
@@ -0,0 +1,86 @@
+package ca.on.oicr.pinery.lims.miso.converters;
+
+import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.google.common.collect.Sets;
+
+import ca.on.oicr.pinery.api.Attribute;
+import ca.on.oicr.pinery.api.Sample;
+import ca.on.oicr.pinery.lims.DefaultAttribute;
+
+public enum QcConverter {
+
+  QUBIT(".*Qubit.*", "Qubit (ng/uL)"), //
+  NANODROP("^Nanodrop$", "Nanodrop (ng/uL)"), //
+  HUMAN_QPCR("^Human qPCR$", "qPCR %"), //
+  RIN("RIN.*", "RIN"), //
+  DV200(".*DV200.*", "DV200"), //
+  PDAC("^PDAC Confirmed$", "PDAC Confirmed", "True", "False"), //
+  DRAFT_CLINICAL_REPORT("^Draft Clinical Report$", "Draft Clinical Report", "Pass", "Fail"), //
+  INFORMATICS_REVIEW("^Informatics Review$", "Informatics Review", "Pass", "Fail");
+
+  private final String pattern;
+  private final String attributeName;
+  private final String trueLabel;
+  private final String falseLabel;
+
+  private QcConverter(String pattern, String attributeName) {
+    this.pattern = pattern;
+    this.attributeName = attributeName;
+    this.trueLabel = null;
+    this.falseLabel = null;
+  }
+
+  private QcConverter(String pattern, String attributeName, String trueLabel, String falseLabel) {
+    this.pattern = pattern;
+    this.attributeName = attributeName;
+    this.trueLabel = trueLabel;
+    this.falseLabel = falseLabel;
+  }
+
+  public static void addToSample(ResultSet rs, Sample sample) throws SQLException {
+    String qcType = rs.getString("qcType");
+    for (QcConverter converter : QcConverter.values()) {
+      if (qcType.matches(converter.pattern)) {
+        String result = converter.trueLabel == null ? extractBigDecimalString(rs) : converter.extractBooleanString(rs);
+        if (result != null) {
+          Attribute attr = new DefaultAttribute();
+          attr.setName(converter.attributeName);
+          attr.setValue(result);
+          if (sample.getAttributes() == null) {
+            sample.setAttributes(Sets.newHashSet());
+          }
+          sample.getAttributes().add(attr);
+        }
+        return;
+      }
+    }
+
+  }
+
+  private static String extractBigDecimalString(ResultSet rs) throws SQLException {
+    BigDecimal value = rs.getBigDecimal("results");
+    if (!rs.wasNull()) {
+      String nice = StringUtils.strip(value.toPlainString(), "0");
+      if (nice.startsWith(".")) {
+        nice = "0" + nice;
+      }
+      nice = StringUtils.strip(nice, ".");
+      return nice;
+    }
+    return null;
+  }
+
+  private String extractBooleanString(ResultSet rs) throws SQLException {
+    int value = rs.getInt("results");
+    if (rs.wasNull()) {
+      return null;
+    }
+    return value > 0 ? trueLabel : falseLabel;
+  }
+
+}
---
+++ pinery-miso/src/main/resources/ca/on/oicr/pinery/lims/miso/queryAllSampleQcs.sql
@@ -0,0 +1,27 @@
+SELECT s.name sampleId, qt.name qcType, qc.results
+FROM SampleQC qc
+JOIN QCType qt ON qt.qcTypeId = qc.type
+JOIN Sample s ON s.sampleId = qc.sample_sampleId
+WHERE qc.qcId IN (
+  SELECT MAX(qc.qcId) qcId FROM SampleQC qc
+  JOIN (
+    SELECT sample_sampleId, type, MAX(date) maxDate FROM SampleQC GROUP BY sample_sampleId, type
+  ) maxDates ON maxDates.sample_sampleId = qc.sample_sampleId AND maxDates.type = qc.type
+  WHERE qc.date = maxDates.maxDate
+  GROUP BY qc.sample_sampleId, qc.type
+)
+
+UNION ALL
+
+SELECT l.name sampleId, qt.name, qc.results
+FROM LibraryQC qc
+JOIN QCType qt ON qt.qcTypeId = qc.type
+JOIN Library l ON l.libraryId = qc.library_libraryId
+WHERE qc.qcId IN (
+  SELECT MAX(qc.qcId) qcId FROM LibraryQC qc
+  JOIN (
+    SELECT library_libraryId, type, MAX(date) maxDate FROM LibraryQC GROUP BY library_libraryId, type
+  ) maxDates ON maxDates.library_libraryId = qc.library_libraryId AND maxDates.type = qc.type
+  WHERE qc.date = maxDates.maxDate
+  GROUP BY qc.library_libraryId, qc.type
+)
