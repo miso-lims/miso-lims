e4bcdd6229ac57166334492ba6dd90953fd84113
Alexis Varsava <Alexis.Varsava@oicr.on.ca>
2021-01-14 18:24:43 -0500

    I think this works better

--- sqlstore/src/main/resources/db/migration/V1210__auto_main.sql
+++ sqlstore/src/main/resources/db/migration/V1210__auto_main.sql
@@ -15,8 +15,8 @@ SET Lab.alias = CONCAT(
   IF(Lab.alias = 'Not Specified' OR Lab.alias = inst.alias, '', CONCAT(' - ', Lab.alias))
 );
 
-ALTER TABLE Lab DROP CONSTRAINT `lab_institute-alias_uk`;
 ALTER TABLE Lab DROP FOREIGN KEY lab_institute_fkey;
+ALTER TABLE Lab DROP CONSTRAINT `lab_institute-alias_uk`;
 ALTER TABLE Lab DROP COLUMN instituteId;
 DROP TABLE Institute;
 
