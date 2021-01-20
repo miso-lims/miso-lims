2517da970b46dbf48caf8d39d2801d5e89f9728d
Alexis Varsava <alexis.varsava@oicr.on.ca>
2021-01-20 09:37:11 -0500

    still broken

--- miso-service/src/main/java/uk/ac/bbsrc/tgac/miso/service/impl/DefaultSubprojectService.java
+++ miso-service/src/main/java/uk/ac/bbsrc/tgac/miso/service/impl/DefaultSubprojectService.java
@@ -56,11 +56,12 @@ public class DefaultSubprojectService extends AbstractSaveService<Subproject> im
 
   @Override
   protected void collectValidationErrors(Subproject object, Subproject beforeChange, List<ValidationError> errors) throws IOException {
-    if (ValidationUtils.isChanged(Subproject::getAlias, object, beforeChange) && !multipleForProject(subprojectDao.getByAlias(object.getAlias()), object.getParentProject())) {
+    if (ValidationUtils.isChanged(Subproject::getAlias, object, beforeChange) && multipleForProject(subprojectDao.getByAlias(object.getAlias()), object.getParentProject())) {
       errors.add(ValidationError.forDuplicate("subproject", "alias"));
     }
   }
 
+  // Does not work
   private boolean multipleForProject(List<Subproject> subprojects, Project targetProject){
     for(Subproject subproject: subprojects){
       if (subproject.getParentProject() == targetProject){
