6eec8e7cacee25e32ee4ec6bc058d8c20cf183cd
Dillan Cooke <dillan.cooke@oicr.on.ca>
2021-02-19 08:57:27 -0500

    [GLT-3452] HibernateTissueOriginDao refactoring and tests

---
+++ sqlstore/src/it/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateTissueOriginDaoIT.java
@@ -0,0 +1,59 @@
+package uk.ac.bbsrc.tgac.miso.persistence.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.eaglegenomics.simlims.core.User;
+
+import uk.ac.bbsrc.tgac.miso.AbstractHibernateSaveDaoTest;
+import uk.ac.bbsrc.tgac.miso.core.data.TissueOrigin;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.TissueOriginImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.UserImpl;
+
+public class HibernateTissueOriginDaoIT extends AbstractHibernateSaveDaoTest<TissueOrigin, HibernateTissueOriginDao> {
+
+  public HibernateTissueOriginDaoIT() {
+    super(TissueOriginImpl.class, 1L, 2);
+  }
+
+  @Override
+  public HibernateTissueOriginDao constructTestSubject() {
+    HibernateTissueOriginDao sut = new HibernateTissueOriginDao();
+    sut.setSessionFactory(getSessionFactory());
+    return sut;
+  }
+
+  @Override
+  public TissueOrigin getCreateItem() {
+    TissueOrigin origin = new TissueOriginImpl();
+    origin.setAlias("TO");
+    origin.setDescription("Test Origin");
+    User user = (User) currentSession().get(UserImpl.class, 1L);
+    origin.setChangeDetails(user);
+    return origin;
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public UpdateParameters<TissueOrigin, String> getUpdateParams() {
+    return new UpdateParameters<>(1L, TissueOrigin::getDescription, TissueOrigin::setDescription, "Changed");
+  }
+
+  @Test
+  public void testGetByAlias() throws Exception {
+    String alias = "Test Origin";
+    TissueOrigin origin = getTestSubject().getByAlias(alias);
+    assertNotNull(origin);
+    assertEquals(alias, origin.getAlias());
+  }
+
+  @Test
+  public void testGetUsage() throws Exception {
+    TissueOrigin origin1 = (TissueOrigin) currentSession().get(TissueOriginImpl.class, 1L);
+    assertEquals(3, getTestSubject().getUsage(origin1));
+    TissueOrigin origin2 = (TissueOrigin) currentSession().get(TissueOriginImpl.class, 2L);
+    assertEquals(0, getTestSubject().getUsage(origin2));
+  }
+
+}
