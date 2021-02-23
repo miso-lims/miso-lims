061bd2d3ad6a6a7e3947fd1182d2e1e1640e0c04
Dillan Cooke <dillan.cooke@oicr.on.ca>
2021-02-23 16:04:02 -0500

    [GLT-3452] HibernateChangeLoggableDao tests

---
+++ sqlstore/src/it/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateChangeLoggableDaoIT.java
@@ -0,0 +1,37 @@
+package uk.ac.bbsrc.tgac.miso.persistence.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import uk.ac.bbsrc.tgac.miso.AbstractDAOTest;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.workset.Workset;
+
+public class HibernateChangeLoggableDaoIT extends AbstractDAOTest {
+
+  private HibernateChangeLoggableDao sut;
+
+  @Before
+  public void setup() {
+    sut = new HibernateChangeLoggableDao();
+    sut.setSessionFactory(getSessionFactory());
+  }
+
+  @Test
+  public void testUpdate() {
+    long worksetId = 1L;
+    String newAlias = "Changed";
+    Workset before = (Workset) currentSession().get(Workset.class, worksetId);
+    assertNotNull(before);
+    assertNotEquals(newAlias, before.getAlias());
+    before.setAlias(newAlias);
+    sut.update(before);
+
+    clearSession();
+
+    Workset after = (Workset) currentSession().get(Workset.class, worksetId);
+    assertEquals(newAlias, after.getAlias());
+  }
+
+}
