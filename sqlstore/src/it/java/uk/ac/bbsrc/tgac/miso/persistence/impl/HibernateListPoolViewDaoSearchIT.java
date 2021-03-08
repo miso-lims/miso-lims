5662dcc0c4e4d2f4cef53a8729a62581301932e4
Dillan Cooke <dillan.cooke@oicr.on.ca>
2021-03-08 11:52:59 -0500

    [GLT-3452] HibernateListPoolViewDao tests

---
+++ sqlstore/src/it/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateListPoolViewDaoSearchIT.java
@@ -0,0 +1,28 @@
+package uk.ac.bbsrc.tgac.miso.persistence.impl;
+
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.List;
+
+import uk.ac.bbsrc.tgac.miso.HibernatePaginatedDataSourceIT;
+
+public class HibernateListPoolViewDaoSearchIT extends HibernatePaginatedDataSourceIT {
+
+  private static final EnumSet<SearchType> VALID_SEARCH_TYPES = EnumSet.of(SearchType.QUERY, SearchType.ID, SearchType.IDS,
+      SearchType.INDEX, SearchType.BOX, SearchType.PROJECT, SearchType.PLATFORM_TYPE, SearchType.FREEZER, SearchType.DISTRIBUTION_RECIPIENT,
+      SearchType.CREATED, SearchType.ENTERED, SearchType.UPDATED, SearchType.CREATOR, SearchType.MODIFIER, SearchType.DISTRIBUTED,
+      SearchType.RECEIVED);
+  private static final List<String> SORT_FIELDS = Arrays.asList("alias", "creationDate");
+
+  public HibernateListPoolViewDaoSearchIT() {
+    super(VALID_SEARCH_TYPES, SORT_FIELDS);
+  }
+
+  @Override
+  protected HibernatePaginatedDataSource<?> constructTestSubject() {
+    HibernateListPoolViewDao sut = new HibernateListPoolViewDao();
+    sut.setSessionFactory(getSessionFactory());
+    return sut;
+  }
+
+}
