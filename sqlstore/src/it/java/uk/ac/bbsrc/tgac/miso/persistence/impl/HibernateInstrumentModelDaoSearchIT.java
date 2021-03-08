e3a19a23150de666f880439141ea4594875b6687
Dillan Cooke <dillan.cooke@oicr.on.ca>
2021-03-08 16:38:58 -0500

    [GLT-3452] HibernateInstrumentModelDao tests

---
+++ sqlstore/src/it/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateInstrumentModelDaoSearchIT.java
@@ -0,0 +1,25 @@
+package uk.ac.bbsrc.tgac.miso.persistence.impl;
+
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.List;
+
+import uk.ac.bbsrc.tgac.miso.HibernatePaginatedDataSourceIT;
+
+public class HibernateInstrumentModelDaoSearchIT extends HibernatePaginatedDataSourceIT {
+
+  private static final EnumSet<SearchType> VALID_SEARCH_TYPES = EnumSet.of(SearchType.QUERY, SearchType.ID, SearchType.IDS);
+  private static final List<String> SORT_FIELDS = Arrays.asList("alias");
+
+  public HibernateInstrumentModelDaoSearchIT() {
+    super(VALID_SEARCH_TYPES, SORT_FIELDS, "alias");
+  }
+
+  @Override
+  protected HibernatePaginatedDataSource<?> constructTestSubject() {
+    HibernateInstrumentModelDao sut = new HibernateInstrumentModelDao();
+    sut.setSessionFactory(getSessionFactory());
+    return sut;
+  }
+
+}
