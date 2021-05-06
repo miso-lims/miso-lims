d25f335f43a0770fbf1080f51b9e236dc8aaa979
Dillan Cooke <dcooke@oicr.on.ca>
2021-05-06 11:32:09 -0400

    fix test

---
+++ miso-web/src/it/java/uk/ac/bbsrc/tgac/miso/webapp/integrationtest/page/IdentitySearchPage.java
@@ -0,0 +1,25 @@
+package uk.ac.bbsrc.tgac.miso.webapp.integrationtest.page;
+
+import org.openqa.selenium.WebDriver;
+
+import uk.ac.bbsrc.tgac.miso.webapp.integrationtest.page.element.DataTable;
+
+public class IdentitySearchPage extends HeaderFooterPage {
+  
+  private final DataTable samplesTable;
+
+  public IdentitySearchPage(WebDriver driver) {
+    super(driver);
+    samplesTable = new DataTable(driver, "list_samples_wrapper");
+  }
+
+  public static IdentitySearchPage get(WebDriver driver, String baseUrl) {
+    driver.get(baseUrl + "%smiso/identitysearch");
+    return new IdentitySearchPage(driver);
+  }
+  
+  public DataTable getSamplesTable() {
+    return samplesTable;
+  }
+
+}
