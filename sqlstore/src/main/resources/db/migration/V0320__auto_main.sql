f5e92962b01bbd477754ae30b77728f78492c7d6
Dillan Cooke <dillan.cooke@oicr.on.ca>
2022-01-27 10:03:40 -0500

    [#2621] fix DB migration for MariaDB 10.2.41 (#2624)

--- sqlstore/src/main/resources/db/migration/V0320__auto_main.sql
+++ sqlstore/src/main/resources/db/migration/V0320__auto_main.sql
@@ -64,9 +64,12 @@ UPDATE QCType SET precisionAfterDecimal = 2 WHERE precisionAfterDecimal = 0;
 
 -- printer_rename
 
--- StartNoTest
-UPDATE Printer SET backend = 'BRADY_FTP', configuration = JSON_OBJECT('host', JSON_EXTRACT(CAST(configuration AS JSON), '$.host'), 'pin', JSON_EXTRACT(CAST(configuration AS JSON), '$.password'))  WHERE backend = 'FTP';
--- EndNoTest
+UPDATE Printer SET
+  backend = 'BRADY_FTP',
+  configuration = JSON_OBJECT('host', JSON_EXTRACT(configuration, '$.host'),
+  'pin', JSON_EXTRACT(configuration, '$.password'))
+WHERE backend = 'FTP';
+
 UPDATE Printer SET driver = 'BRADY_BPT_635_488' WHERE backend = 'BRADY_M80';
 UPDATE Printer SET driver = 'BRADY_THT_181_492_3' WHERE backend = 'BRADY_STANDARD';
 
