388489cd27e710e260ecb43b09fc05e4ae0b849f
hyeonjeongkwon <105447603+hyeonjeongkwon@users.noreply.github.com>
2023-02-09 09:36:41 -0500

    changes on after migrate

--- sqlstore/src/main/resources/db/migration_afterMigrate/05_view_instrument.sql
+++ sqlstore/src/main/resources/db/migration_afterMigrate/05_view_instrument.sql
@@ -11,7 +11,7 @@ FROM Instrument inst
 LEFT JOIN InstrumentPosition ipos ON ipos.instrumentModelId = inst.instrumentModelId
 LEFT JOIN (
   SELECT instrumentId, positionId, MIN(startTime) AS outOfServiceTime
-  FROM ServiceRecord JOIN Instrument_ServiceRecord
+  FROM ServiceRecord INNER JOIN Instrument_ServiceRecord instsr ON inst.instrumentId = instsr.instrumentId
   WHERE outOfService = TRUE AND startTime IS NOT NULL AND endTime IS NULL
   GROUP BY instrumentId, positionId
 ) sr ON sr.instrumentId = inst.instrumentId AND (sr.positionId IS NULL OR sr.positionId = ipos.positionId);
