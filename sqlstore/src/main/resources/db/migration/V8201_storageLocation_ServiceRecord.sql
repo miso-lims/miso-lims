85c13670d5fb2310c659f7b212fdfa66eb6b3217
hyeonjeongkwon <105447603+hyeonjeongkwon@users.noreply.github.com>
2023-02-27 08:56:14 -0500

    Delete V8201_storageLocation_ServiceRecord.sql

--- sqlstore/src/main/resources/db/migration/V8201_storageLocation_ServiceRecord.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE StorageLocation_ServiceRecord (
-  recordId bigint(20) NOT NULL,
-  locationId bigint(20) NOT NULL,
-  PRIMARY KEY (recordId, locationId),
-  CONSTRAINT fk_storageLocationServiceRecord_storageLocation FOREIGN KEY (locationId) REFERENCES StorageLocation (locationId),
-  CONSTRAINT fk_storageLocationServiceRecord_serviceRecord FOREIGN KEY (recordId) REFERENCES ServiceRecord (recordId)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
