4572826f3461acef20f2e0e753463a34081956bf
Aryan Gandevia <aryan.gandevia@oicr.on.ca>
2023-06-29 15:47:53 -0400

    .

--- sqlstore/src/main/resources/db/migration/V8225__add_deliverables_type.sql
+++ sqlstore/src/main/resources/db/migration/V8225__add_deliverables_type.sql
@@ -3,9 +3,3 @@ CREATE TABLE Deliverable (
   name varchar(255) NOT NULL,
   PRIMARY KEY(deliverableId)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Default values for stage and prod
-INSERT INTO Deliverable(deliverableId, name) VALUES
-(1, "fastq"),
-(2, "full pipeline"),
-(3, "clinical report");
