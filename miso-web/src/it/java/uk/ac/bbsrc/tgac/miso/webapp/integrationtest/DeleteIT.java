568fb91c3b2fc75433ce68dcbe380805566e658f
Aryan Gandevia <aryan.gandevia@oicr.on.ca>
2023-06-30 12:06:11 -0400

    .

--- miso-web/src/it/java/uk/ac/bbsrc/tgac/miso/webapp/integrationtest/DeleteIT.java
+++ miso-web/src/it/java/uk/ac/bbsrc/tgac/miso/webapp/integrationtest/DeleteIT.java
@@ -418,7 +418,7 @@ public class DeleteIT extends AbstractIT {
 
   @Test
   public void testDeleteDeliverable() {
-    testDelete(ListTarget.DELIVERABLES, null, "deliverable100000", Columns.NAME, Deliverable.class, 100000L);
+    testDelete(ListTarget.DELIVERABLES, null, "deliverable1", Columns.NAME, Deliverable.class, 1L);
   }
 
   private void testDelete(String listTarget, String tab, String search, String selectByColumn, Class<?> clazz,
