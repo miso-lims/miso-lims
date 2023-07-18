550f0838de4ac630f189e87ec6fa3606e8463972
Aryan Gandevia <aryan.gandevia@oicr.on.ca>
2023-07-18 15:10:02 -0400

    .

--- pinery-miso/src/main/resources/ca/on/oicr/pinery/lims/miso/queryAllSampleProjects.sql
+++ pinery-miso/src/main/resources/ca/on/oicr/pinery/lims/miso/queryAllSampleProjects.sql
@@ -17,7 +17,7 @@ SELECT
 FROM Project proj
 JOIN Pipeline pipe ON pipe.pipelineId = proj.pipelineId
 JOIN Project_Contact pc ON pc.projectId = proj.projectId WHERE pc.contactRoleId = 1 LIMIT 1
-Join Contact c ON c.contactId = pc.contactId
+JOIN Contact c ON c.contactId = pc.contactId
 LEFT JOIN (
   SELECT
     project_projectId,
