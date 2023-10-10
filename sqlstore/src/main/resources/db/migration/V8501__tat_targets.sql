a9936830d28770a2edcf9eec3ab4e86a78cb0c51
Dillan Cooke <dillan.cooke@oicr.on.ca>
2023-10-10 14:50:38 -0400

    [GLT-4010] added assay turn-around time target fields (#2827)

---
+++ sqlstore/src/main/resources/db/migration/V8501__tat_targets.sql
@@ -0,0 +1,10 @@
+ALTER TABLE Assay
+  ADD COLUMN caseTargetDays SMALLINT,
+  ADD COLUMN receiptTargetDays SMALLINT,
+  ADD COLUMN extractionTargetDays SMALLINT,
+  ADD COLUMN libraryPreparationTargetDays SMALLINT,
+  ADD COLUMN libraryQualificationTargetDays SMALLINT,
+  ADD COLUMN fullDepthSequencingTargetDays SMALLINT,
+  ADD COLUMN analysisReviewTargetDays SMALLINT,
+  ADD COLUMN releaseApprovalTargetDays SMALLINT,
+  ADD COLUMN releaseTargetDays SMALLINT;
