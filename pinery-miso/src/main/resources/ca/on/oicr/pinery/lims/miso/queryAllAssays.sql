4acec7044b169bcc0c37b7fc340aa03d603ad9d3
Dillan Cooke <dillan.cooke@oicr.on.ca>
2023-10-17 08:27:50 -0400

    [GLT-4011] added turn-around time targets to Pinery

--- pinery-miso/src/main/resources/ca/on/oicr/pinery/lims/miso/queryAllAssays.sql
+++ pinery-miso/src/main/resources/ca/on/oicr/pinery/lims/miso/queryAllAssays.sql
@@ -2,4 +2,13 @@ SELECT assayId
   ,alias AS name
   ,description
   ,version
+  ,caseTargetDays
+  ,receiptTargetDays
+  ,extractionTargetDays
+  ,libraryPreparationTargetDays
+  ,libraryQualificationTargetDays
+  ,fullDepthSequencingTargetDays
+  ,analysisReviewTargetDays
+  ,releaseApprovalTargetDays
+  ,releaseTargetDays
 FROM Assay
