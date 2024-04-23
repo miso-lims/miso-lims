b8259dcd9f3b216ab4bc0869e1ceea27914ba2f4
Dillan Cooke <dillan.cooke@oicr.on.ca>
2024-04-23 10:42:24 -0400

    [GLT-4126] fixed SampleHierarchy build selecting wrong tissue

---
+++ sqlstore/src/main/resources/db/migration/V8505__fix_sample_hierarchy.sql
@@ -0,0 +1,29 @@
+DELIMITER //
+
+CREATE PROCEDURE rebuildSampleHierarchy()
+BEGIN
+  DECLARE vDone BOOLEAN DEFAULT FALSE;
+  DECLARE vSampleId bigint;
+  DECLARE vCursor CURSOR FOR
+    SELECT s.sampleId FROM Sample s
+    JOIN Sample p ON p.sampleId = s.parentId
+    WHERE s.discriminator = 'Tissue'
+    AND p.discriminator = 'Tissue';
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET vDone = TRUE;
+
+  OPEN vCursor;
+  read_loop: LOOP
+    FETCH vCursor INTO vSampleId;
+    IF vDone THEN
+      LEAVE read_loop;
+    END IF;
+    CALL updateSampleHierarchy(vSampleId);
+  END LOOP;
+  CLOSE vCursor;
+END//
+
+DELIMITER ;
+
+CALL rebuildSampleHierarchy();
+
+DROP PROCEDURE rebuildSampleHierarchy;
