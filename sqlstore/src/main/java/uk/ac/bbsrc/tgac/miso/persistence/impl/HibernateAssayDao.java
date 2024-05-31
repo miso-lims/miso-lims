aaee8a1854e9da6c1afbf3dc7105ec219b8bfe26
sinry-d <93559434+sinry-d@users.noreply.github.com>
2024-05-31 11:48:25 -0400

    [GLT-4141] Update HibernateAssayDao to JPA Criteria (#2880)

--- sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateAssayDao.java
+++ sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateAssayDao.java
@@ -4,13 +4,14 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 
-import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import uk.ac.bbsrc.tgac.miso.core.data.impl.Assay;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.AssayMetric;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.Assay_;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.Requisition;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.Requisition_;
 import uk.ac.bbsrc.tgac.miso.persistence.AssayDao;
 
 @Transactional(rollbackFor = Exception.class)
@@ -24,20 +25,20 @@ public class HibernateAssayDao extends HibernateSaveDao<Assay>
 
   @Override
   public Assay getByAliasAndVersion(String alias, String version) throws IOException {
-    return (Assay) currentSession().createCriteria(getEntityClass())
-        .add(Restrictions.eq("alias", alias))
-        .add(Restrictions.eq("version", version))
-        .uniqueResult();
+    QueryBuilder<Assay, Assay> builder = new QueryBuilder<>(currentSession(), Assay.class, Assay.class);
+    builder.addPredicate(builder.getCriteriaBuilder().equal(builder.getRoot().get(Assay_.alias), alias));
+    builder.addPredicate(builder.getCriteriaBuilder().equal(builder.getRoot().get(Assay_.version), version));
+    return builder.getSingleResultOrNull();
   }
 
   @Override
   public long getUsage(Assay assay) throws IOException {
-    return getUsageInCollection(Requisition.class, "assays", assay);
+    return getUsageInCollection(Requisition.class, Requisition_.ASSAYS, assay);
   }
 
   @Override
   public List<Assay> listByIdList(Collection<Long> ids) throws IOException {
-    return listByIdList("assayId", ids);
+    return listByIdList(Assay_.ASSAY_ID, ids);
   }
 
   @Override
