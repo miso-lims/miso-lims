b0e2d2a11883e03cd5522155d36bca68cf151c47
sinry-d <93559434+sinry-d@users.noreply.github.com>
2024-05-31 10:05:02 -0400

    [GLT-4239] Update HibernateArrayModelDao to JPA Criteria (#2878)

--- sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateArrayModelDao.java
+++ sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateArrayModelDao.java
@@ -4,13 +4,13 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 
-import org.hibernate.criterion.Projections;
-import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import uk.ac.bbsrc.tgac.miso.core.data.Array;
 import uk.ac.bbsrc.tgac.miso.core.data.ArrayModel;
+import uk.ac.bbsrc.tgac.miso.core.data.ArrayModel_;
+import uk.ac.bbsrc.tgac.miso.core.data.Array_;
 import uk.ac.bbsrc.tgac.miso.persistence.ArrayModelDao;
 
 @Transactional(rollbackFor = Exception.class)
@@ -23,28 +23,17 @@ public class HibernateArrayModelDao extends HibernateSaveDao<ArrayModel> impleme
 
   @Override
   public ArrayModel getByAlias(String alias) throws IOException {
-    return (ArrayModel) currentSession().createCriteria(ArrayModel.class)
-        .add(Restrictions.eq("alias", alias))
-        .uniqueResult();
+    return getBy(ArrayModel_.ALIAS, alias);
   }
 
   @Override
   public List<ArrayModel> listByIdList(Collection<Long> idList) throws IOException {
-    return listByIdList("id", idList);
-  }
-
-  @Override
-  public List<ArrayModel> list() throws IOException {
-    @SuppressWarnings("unchecked")
-    List<ArrayModel> results = currentSession().createCriteria(ArrayModel.class).list();
-    return results;
+    return listByIdList(ArrayModel_.ID, idList);
   }
 
   @Override
   public long getUsage(ArrayModel model) throws IOException {
-    return (long) currentSession().createCriteria(Array.class)
-        .add(Restrictions.eq("arrayModel", model))
-        .setProjection(Projections.rowCount()).uniqueResult();
+    return getUsageBy(Array.class, Array_.ARRAY_MODEL, model);
   }
 
 }
