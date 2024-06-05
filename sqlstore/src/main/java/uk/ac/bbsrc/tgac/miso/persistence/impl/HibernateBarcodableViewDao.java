63a0341ea5ec23509098bc6a87a55902dfc83c90
Sinry Dong <sinryd@gmail.com>
2024-06-05 13:45:45 -0400

    [GLT-4152] Update HibernateBarcodableViewDao to JPA Criteria

--- sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateBarcodableViewDao.java
+++ sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateBarcodableViewDao.java
@@ -8,9 +8,10 @@ import java.util.Collection;
 import java.util.List;
 import java.util.function.Predicate;
 
+import javax.persistence.criteria.Root;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,14 +20,22 @@ import com.google.common.collect.Lists;
 
 import uk.ac.bbsrc.tgac.miso.core.data.Barcodable.EntityType;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.BoxImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.BoxImpl_;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.LibraryAliquot;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.LibraryAliquot_;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.LibraryImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.LibraryImpl_;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.PoolImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.PoolImpl_;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.SampleImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.SampleImpl_;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.SequencerPartitionContainerImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.SequencerPartitionContainerImpl_;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.SequencingContainerModel;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.SequencingContainerModel_;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.view.BarcodableReference;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.view.BarcodableView;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.view.BarcodableView_;
 import uk.ac.bbsrc.tgac.miso.core.util.ThrowingFunction;
 import uk.ac.bbsrc.tgac.miso.persistence.BarcodableViewDao;
 
@@ -35,7 +44,8 @@ import uk.ac.bbsrc.tgac.miso.persistence.BarcodableViewDao;
 public class HibernateBarcodableViewDao implements BarcodableViewDao {
 
   private final List<ThrowingFunction<String, BarcodableReference, IOException>> INDIVIDUAL_LOOKUPS = Lists
-      .newArrayList(this::getSampleWithBarcode, this::getLibraryWithBarcode, this::getLibraryAliquotWithBarcode, this::getPoolWithBarcode,
+      .newArrayList(this::getSampleWithBarcode, this::getLibraryWithBarcode, this::getLibraryAliquotWithBarcode,
+          this::getPoolWithBarcode,
           this::getBoxWithBarcode, this::getContainerWithBarcode, this::getContainerModelWithBarcode);
 
   @Autowired
@@ -57,11 +67,14 @@ public class HibernateBarcodableViewDao implements BarcodableViewDao {
       throw new IllegalArgumentException("Types must be specified");
     }
 
-    @SuppressWarnings("unchecked")
-    List<BarcodableView> results = currentSession().createCriteria(BarcodableView.class)
-        .add(Restrictions.eq("identificationBarcode", barcode)).list();
+    QueryBuilder<BarcodableView, BarcodableView> builder =
+        new QueryBuilder<>(currentSession(), BarcodableView.class, BarcodableView.class);
+    builder.addPredicate(
+        builder.getCriteriaBuilder().equal(builder.getRoot().get(BarcodableView_.IDENTIFICATION_BARCODE), barcode));
+    List<BarcodableView> results = builder.getResultList();
 
-    Predicate<BarcodableView> matchesTypeFilter = barcodableView -> typeFilter.contains(barcodableView.getId().getTargetType());
+    Predicate<BarcodableView> matchesTypeFilter =
+        barcodableView -> typeFilter.contains(barcodableView.getId().getTargetType());
 
     return results.stream().filter(matchesTypeFilter).collect(toList());
   }
@@ -74,11 +87,13 @@ public class HibernateBarcodableViewDao implements BarcodableViewDao {
       throw new IllegalArgumentException("Types must be specified");
     }
 
-    @SuppressWarnings("unchecked")
-    List<BarcodableView> results = currentSession().createCriteria(BarcodableView.class)
-        .add(Restrictions.eq("alias", alias)).list();
+    QueryBuilder<BarcodableView, BarcodableView> builder =
+        new QueryBuilder<>(currentSession(), BarcodableView.class, BarcodableView.class);
+    builder.addPredicate(builder.getCriteriaBuilder().equal(builder.getRoot().get(BarcodableView_.ALIAS), alias));
+    List<BarcodableView> results = builder.getResultList();
 
-    Predicate<BarcodableView> matchesTypeFilter = barcodableView -> typeFilter.contains(barcodableView.getId().getTargetType());
+    Predicate<BarcodableView> matchesTypeFilter =
+        barcodableView -> typeFilter.contains(barcodableView.getId().getTargetType());
 
     return results.stream().filter(matchesTypeFilter).collect(toList());
   }
@@ -89,13 +104,14 @@ public class HibernateBarcodableViewDao implements BarcodableViewDao {
       throw new IllegalArgumentException("Search is empty");
     }
 
-    @SuppressWarnings("unchecked")
-    List<BarcodableView> results = currentSession().createCriteria(BarcodableView.class)
-        .add(Restrictions.or(
-                Restrictions.eq("identificationBarcode", query),
-                Restrictions.eq("alias", query),
-                Restrictions.eq("name", query)))
-        .list();
+    QueryBuilder<BarcodableView, BarcodableView> builder =
+        new QueryBuilder<>(currentSession(), BarcodableView.class, BarcodableView.class);
+    Root<BarcodableView> root = builder.getRoot();
+    builder.addPredicate(builder.getCriteriaBuilder().or(
+        builder.getCriteriaBuilder().equal(root.get(BarcodableView_.IDENTIFICATION_BARCODE), query),
+        builder.getCriteriaBuilder().equal(root.get(BarcodableView_.ALIAS), query),
+        builder.getCriteriaBuilder().equal(root.get(BarcodableView_.NAME), query)));
+    List<BarcodableView> results = builder.getResultList();
 
     return results;
   }
@@ -115,41 +131,61 @@ public class HibernateBarcodableViewDao implements BarcodableViewDao {
   }
 
   private BarcodableReference getSampleWithBarcode(String identificationBarcode) throws IOException {
-    return getItemWithBarcode(identificationBarcode, SampleImpl.class, "Sample", "alias", "name");
+    return getItemWithBarcode(identificationBarcode, SampleImpl.class, "Sample", SampleImpl_.SAMPLE_ID,
+        SampleImpl_.ALIAS,
+        SampleImpl_.NAME);
   }
 
   private BarcodableReference getLibraryWithBarcode(String identificationBarcode) throws IOException {
-    return getItemWithBarcode(identificationBarcode, LibraryImpl.class, "Library", "alias", "name");
+    return getItemWithBarcode(identificationBarcode, LibraryImpl.class, "Library", LibraryImpl_.LIBRARY_ID,
+        LibraryImpl_.ALIAS,
+        LibraryImpl_.NAME);
   }
 
   private BarcodableReference getLibraryAliquotWithBarcode(String identificationBarcode) throws IOException {
-    return getItemWithBarcode(identificationBarcode, LibraryAliquot.class, "Library aliquot", "alias", "name");
+    return getItemWithBarcode(identificationBarcode, LibraryAliquot.class, "Library aliquot",
+        LibraryAliquot_.ALIQUOT_ID, LibraryAliquot_.ALIAS, LibraryAliquot_.NAME);
   }
 
   private BarcodableReference getPoolWithBarcode(String identificationBarcode) throws IOException {
-    return getItemWithBarcode(identificationBarcode, PoolImpl.class, "Pool", "alias", "name");
+    return getItemWithBarcode(identificationBarcode, PoolImpl.class, "Pool", PoolImpl_.POOL_ID, PoolImpl_.ALIAS,
+        PoolImpl_.NAME);
   }
 
   private BarcodableReference getBoxWithBarcode(String identificationBarcode) throws IOException {
-    return getItemWithBarcode(identificationBarcode, BoxImpl.class, "Box", "alias", "name");
+    return getItemWithBarcode(identificationBarcode, BoxImpl.class, "Box", BoxImpl_.BOX_ID, BoxImpl_.ALIAS,
+        BoxImpl_.NAME);
   }
 
   private BarcodableReference getContainerWithBarcode(String identificationBarcode) throws IOException {
-    return getItemWithBarcode(identificationBarcode, SequencerPartitionContainerImpl.class, "Sequencing container", "identificationBarcode",
+    return getItemWithBarcode(identificationBarcode, SequencerPartitionContainerImpl.class, "Sequencing container",
+        SequencerPartitionContainerImpl_.CONTAINER_ID,
+        SequencerPartitionContainerImpl_.IDENTIFICATION_BARCODE,
         null);
   }
 
   private BarcodableReference getContainerModelWithBarcode(String identificationBarcode) throws IOException {
-    return getItemWithBarcode(identificationBarcode, SequencingContainerModel.class, "Sequencing container model", "alias", null);
+    return getItemWithBarcode(identificationBarcode, SequencingContainerModel.class, "Sequencing container model",
+        SequencingContainerModel_.SEQUENCING_CONTAINER_MODEL_ID,
+        SequencingContainerModel_.ALIAS, null);
   }
 
-  private BarcodableReference getItemWithBarcode(String identificationBarcode, Class<?> implementationClass, String entityTypeLabel,
+  private BarcodableReference getItemWithBarcode(String identificationBarcode, Class<?> implementationClass,
+      String entityTypeLabel, String idField,
       String primaryLabelField, String secondaryLabelField) throws IOException {
-    return (BarcodableReference) currentSession().createCriteria(implementationClass)
-        .add(Restrictions.eq("identificationBarcode", identificationBarcode))
-        .setProjection(BarcodableReference.makeProjectionList(primaryLabelField, secondaryLabelField))
-        .setResultTransformer(new BarcodableReference.ResultTransformer(entityTypeLabel))
-        .uniqueResult();
+
+    QueryBuilder<?, ?> builder = new QueryBuilder<>(currentSession(), implementationClass, Object[].class,
+        new BarcodableReference.ResultTransformer(entityTypeLabel));
+    Root<?> root = builder.getRoot();
+    builder.addPredicate(
+        builder.getCriteriaBuilder().equal(root.get("identificationBarcode"), identificationBarcode));
+
+    if (secondaryLabelField != null) {
+      builder.setColumns(root.get(idField), root.get(primaryLabelField), root.get(secondaryLabelField));
+    } else {
+      builder.setColumns(root.get(idField), root.get(primaryLabelField));
+    }
+    return (BarcodableReference) builder.getSingleResultOrNull();
   }
 
 }
