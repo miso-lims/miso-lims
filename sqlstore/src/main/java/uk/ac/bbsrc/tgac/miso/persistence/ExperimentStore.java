766b5aec5c1cc4979028179f9fdf230fb6e0d30b
Sinry Dong <sinryd@gmail.com>
2024-06-07 16:48:09 -0400

    Removed headers

--- sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/ExperimentStore.java
+++ sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/ExperimentStore.java
@@ -1,23 +1,3 @@
-/*
- * Copyright (c) 2012. The Genome Analysis Centre, Norwich, UK MISO project contacts: Robert Davey @
- * TGAC *********************************************************************
- *
- * This file is part of MISO.
- *
- * MISO is free software: you can redistribute it and/or modify it under the terms of the GNU
- * General Public License as published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * MISO is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with MISO. If not, see
- * <http://www.gnu.org/licenses/>.
- *
- * *********************************************************************
- */
-
 package uk.ac.bbsrc.tgac.miso.persistence;
 
 import java.io.IOException;
@@ -39,7 +19,7 @@ public interface ExperimentStore extends SaveDao<Experiment> {
    * 
    * @param studyId of type long
    * @return Collection<Experiment>
-   * @throws IOException when
+   * @throws IOException when the objects cannot be retrieved
    */
   public Collection<Experiment> listByStudyId(long studyId) throws IOException;
 
