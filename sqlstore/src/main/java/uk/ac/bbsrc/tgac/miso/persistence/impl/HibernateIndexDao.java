ab8580c9caa63dbd27d0ecc8bd274727d5392fc4
Sinry Dong <sinryd@gmail.com>
2024-06-10 12:08:37 -0400

    [GLT-4157] Update HibernateIndexDao to JPA Criteria

--- sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateIndexDao.java
+++ sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateIndexDao.java
@@ -6,29 +6,32 @@ import java.util.Collection;
 import java.util.List;
 import java.util.function.Consumer;
 
-import org.hibernate.Criteria;
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.criterion.Projections;
-import org.hibernate.criterion.Restrictions;
-import org.hibernate.sql.JoinType;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Root;
+import javax.persistence.metamodel.SingularAttribute;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import uk.ac.bbsrc.tgac.miso.core.data.Index;
 import uk.ac.bbsrc.tgac.miso.core.data.IndexFamily;
+import uk.ac.bbsrc.tgac.miso.core.data.IndexFamily_;
+import uk.ac.bbsrc.tgac.miso.core.data.Index_;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.LibraryImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.LibraryImpl_;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.UserImpl;
 import uk.ac.bbsrc.tgac.miso.core.data.type.PlatformType;
 import uk.ac.bbsrc.tgac.miso.core.util.DateType;
 import uk.ac.bbsrc.tgac.miso.persistence.IndexStore;
 
 @Transactional(rollbackFor = Exception.class)
 @Repository
-public class HibernateIndexDao extends HibernateSaveDao<Index> implements IndexStore, HibernatePaginatedDataSource<Index> {
+public class HibernateIndexDao extends HibernateSaveDao<Index>
+    implements IndexStore, JpaCriteriaPaginatedDataSource<Index, Index> {
 
-  private static final String[] SEARCH_PROPERTIES = new String[] { "name", "sequence", "family.name" };
-
-  private static final List<AliasDescriptor> STANDARD_ALIASES = Arrays.asList(new AliasDescriptor("family"));
+  private static final List<SingularAttribute<Index, String>> SEARCH_PROPERTIES =
+      Arrays.asList(Index_.name, Index_.sequence);
 
   public HibernateIndexDao() {
     super(Index.class);
@@ -40,81 +43,84 @@ public class HibernateIndexDao extends HibernateSaveDao<Index> implements IndexS
   }
 
   @Override
-  public Index get(long id) {
-    Query query = currentSession().createQuery("from Index where id = :id");
-    query.setLong("id", id);
-    return (Index) query.uniqueResult();
+  public SingularAttribute<Index, ?> getIdProperty() {
+    return Index_.indexId;
   }
 
   @Override
-  public String getProjectColumn() {
-    return null;
-  }
-
-  @Override
-  public Class<? extends Index> getRealClass() {
+  public Class<Index> getEntityClass() {
     return Index.class;
   }
 
   @Override
-  public String[] getSearchProperties() {
+  public Class<Index> getResultClass() {
+    return Index.class;
+  }
+
+  @Override
+  public List<SingularAttribute<Index, String>> getSearchProperties() {
     return SEARCH_PROPERTIES;
   }
 
   @Override
-  public Iterable<AliasDescriptor> listAliases() {
-    return STANDARD_ALIASES;
-  }
-
-  @Override
-  public String propertyForDate(Criteria criteria, DateType type) {
+  public SingularAttribute<Index, ?> propertyForDate(DateType type) {
     return null;
   }
 
   @Override
-  public String propertyForSortColumn(String original) {
-    return original;
+  public Path<?> propertyForSortColumn(Root<Index> root, String original) {
+    Path<?> family = root.get(Index_.family);
+    if ("family.platformType".equals(original))
+      return family.get(IndexFamily_.PLATFORM_TYPE);
+    if ("family.name".equals(original))
+      return family.get(IndexFamily_.NAME);
+    return root.get(original);
   }
 
   @Override
-  public String propertyForUser(boolean creator) {
+  public SingularAttribute<Index, ? extends UserImpl> propertyForUser(boolean creator) {
     return null;
   }
 
+
+
   @Override
-  public void restrictPaginationByArchived(Criteria criteria, boolean isArchived, Consumer<String> errorHandler) {
-    criteria.add(Restrictions.eq("family.archived", isArchived));
+  public void restrictPaginationByArchived(QueryBuilder<?, Index> builder, boolean isArchived,
+      Consumer<String> errorHandler) {
+    Join<Index, IndexFamily> indexJoin = builder.getJoin(builder.getRoot(), Index_.family);
+    builder.addPredicate(builder.getCriteriaBuilder().equal(indexJoin.get(IndexFamily_.archived), isArchived));
   }
 
   @Override
-  public void restrictPaginationByPlatformType(Criteria criteria, PlatformType platformType, Consumer<String> errorHandler) {
-    criteria.add(Restrictions.eq("family.platformType", platformType));
+  public void restrictPaginationByPlatformType(QueryBuilder<?, Index> builder, PlatformType platformType,
+      Consumer<String> errorHandler) {
+    Join<Index, IndexFamily> indexJoin = builder.getJoin(builder.getRoot(), Index_.family);
+    builder.addPredicate(builder.getCriteriaBuilder().equal(indexJoin.get(IndexFamily_.platformType), platformType));
   }
 
   @Override
   public Index getByFamilyPositionAndName(IndexFamily family, int position, String name) throws IOException {
-    return (Index) currentSession().createCriteria(Index.class)
-        .add(Restrictions.eq("family", family))
-        .add(Restrictions.eq("position", position))
-        .add(Restrictions.eq("name", name))
-        .uniqueResult();
+    QueryBuilder<Index, Index> builder = new QueryBuilder<>(currentSession(), Index.class, Index.class);
+    builder.addPredicate(builder.getCriteriaBuilder().equal(builder.getRoot().get(Index_.family), family));
+    builder.addPredicate(builder.getCriteriaBuilder().equal(builder.getRoot().get(Index_.position), position));
+    builder.addPredicate(builder.getCriteriaBuilder().equal(builder.getRoot().get(Index_.name), name));
+    return builder.getSingleResultOrNull();
   }
 
   @Override
   public long getUsage(Index index) throws IOException {
-    return (long) currentSession().createCriteria(LibraryImpl.class)
-        .createAlias("index1", "index1")
-        .createAlias("index2", "index2", JoinType.LEFT_OUTER_JOIN)
-        .add(Restrictions.or(
-            Restrictions.eq("index1.id", index.getId()),
-            Restrictions.eq("index2.id", index.getId())))
-        .setProjection(Projections.rowCount())
-        .uniqueResult();
+    LongQueryBuilder<LibraryImpl> builder = new LongQueryBuilder<>(currentSession(), LibraryImpl.class);
+    Join<LibraryImpl, Index> index1Join = builder.getJoin(builder.getRoot(), LibraryImpl_.index1);
+    Join<LibraryImpl, Index> index2Join = builder.getJoin(builder.getRoot(), LibraryImpl_.index2);
+    builder.addPredicate(builder.getCriteriaBuilder().or(
+        builder.getCriteriaBuilder().equal(index1Join.get(Index_.indexId), index.getId()),
+        builder.getCriteriaBuilder().equal(index2Join.get(Index_.indexId), index.getId())));
+    return builder.getCount();
   }
 
   @Override
   public List<Index> listByIdList(Collection<Long> ids) throws IOException {
-    return listByIdList("indexId", ids);
+    return listByIdList(Index_.INDEX_ID, ids);
   }
 
 }
