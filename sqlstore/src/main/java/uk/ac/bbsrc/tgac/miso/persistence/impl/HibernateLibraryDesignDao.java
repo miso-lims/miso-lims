04a9ff7361b4231399916f2b04bad9e12f0bad25
Sinry Dong <sinryd@gmail.com>
2024-06-13 10:41:56 -0400

    [GLT-4164] Update Hibernate LibraryDesignDao to JPA Criteria

--- sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateLibraryDesignDao.java
+++ sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateLibraryDesignDao.java
@@ -5,14 +5,18 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-import org.hibernate.Criteria;
-import org.hibernate.criterion.Restrictions;
+import javax.persistence.criteria.Join;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import uk.ac.bbsrc.tgac.miso.core.data.LibraryDesign;
+import uk.ac.bbsrc.tgac.miso.core.data.LibraryDesign_;
 import uk.ac.bbsrc.tgac.miso.core.data.SampleClass;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.DetailedLibraryImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.DetailedLibraryImpl_;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.SampleClassImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.SampleClassImpl_;
 import uk.ac.bbsrc.tgac.miso.persistence.LibraryDesignDao;
 
 @Repository
@@ -25,31 +29,36 @@ public class HibernateLibraryDesignDao extends HibernateSaveDao<LibraryDesign> i
 
   @Override
   public LibraryDesign getByNameAndSampleClass(String name, SampleClass sampleClass) throws IOException {
-    return (LibraryDesign) currentSession().createCriteria(LibraryDesign.class)
-        .add(Restrictions.eq("name", name))
-        .add(Restrictions.eq("sampleClass", sampleClass))
-        .uniqueResult();
+    QueryBuilder<LibraryDesign, LibraryDesign> builder =
+        new QueryBuilder<>(currentSession(), LibraryDesign.class, LibraryDesign.class);
+    builder.addPredicate(builder.getCriteriaBuilder().equal(builder.getRoot().get(LibraryDesign_.name), name));
+    builder.addPredicate(
+        builder.getCriteriaBuilder().equal(builder.getRoot().get(LibraryDesign_.sampleClass), sampleClass));
+    return builder.getSingleResultOrNull();
   }
 
   @Override
   public List<LibraryDesign> listByClass(SampleClass sampleClass) throws IOException {
-    if (sampleClass == null) return Collections.emptyList();
-    Criteria criteria = currentSession().createCriteria(LibraryDesign.class);
-    criteria.createAlias("sampleClass", "sampleClass");
-    criteria.add(Restrictions.eq("sampleClass.id", sampleClass.getId()));
-    @SuppressWarnings("unchecked")
-    List<LibraryDesign> rules = criteria.list();
-    return rules;
+    if (sampleClass == null)
+      return Collections.emptyList();
+
+    QueryBuilder<LibraryDesign, LibraryDesign> builder =
+        new QueryBuilder<>(currentSession(), LibraryDesign.class, LibraryDesign.class);
+    Join<LibraryDesign, SampleClassImpl> sampleClassJoin =
+        builder.getJoin(builder.getRoot(), LibraryDesign_.sampleClass);
+    builder.addPredicate(
+        builder.getCriteriaBuilder().equal(sampleClassJoin.get(SampleClassImpl_.sampleClassId), sampleClass.getId()));
+    return builder.getResultList();
   }
 
   @Override
   public List<LibraryDesign> listByIdList(Collection<Long> idList) throws IOException {
-    return listByIdList("libraryDesignId", idList);
+    return listByIdList(LibraryDesign_.LIBRARY_DESIGN_ID, idList);
   }
 
   @Override
   public long getUsage(LibraryDesign design) throws IOException {
-    return getUsageBy(DetailedLibraryImpl.class, "libraryDesign", design);
+    return getUsageBy(DetailedLibraryImpl.class, DetailedLibraryImpl_.LIBRARY_DESIGN, design);
   }
 
 }
