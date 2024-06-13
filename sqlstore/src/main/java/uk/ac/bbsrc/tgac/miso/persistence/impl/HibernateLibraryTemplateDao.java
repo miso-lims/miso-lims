df56ce22b67c60857fed344a80a06fb4e7ec48e9
Sinry Dong <sinryd@gmail.com>
2024-06-13 15:21:43 -0400

    [GLT-4166] Update HibernateLibraryTemplateDao to JPA Criteria

--- sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateLibraryTemplateDao.java
+++ sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateLibraryTemplateDao.java
@@ -1,27 +1,32 @@
 package uk.ac.bbsrc.tgac.miso.persistence.impl;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Consumer;
 
-import org.hibernate.Criteria;
-import org.hibernate.criterion.Restrictions;
+import javax.persistence.criteria.Join;
+import javax.persistence.metamodel.SingularAttribute;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import uk.ac.bbsrc.tgac.miso.core.data.impl.LibraryTemplate;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.LibraryTemplate_;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.ProjectImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.ProjectImpl_;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.UserImpl;
 import uk.ac.bbsrc.tgac.miso.core.util.DateType;
 import uk.ac.bbsrc.tgac.miso.persistence.LibraryTemplateStore;
 
 @Repository
 @Transactional(rollbackFor = Exception.class)
 public class HibernateLibraryTemplateDao extends HibernateSaveDao<LibraryTemplate>
-    implements LibraryTemplateStore, HibernatePaginatedDataSource<LibraryTemplate> {
+    implements LibraryTemplateStore, JpaCriteriaPaginatedDataSource<LibraryTemplate, LibraryTemplate> {
 
-  private static final String[] SEARCH_PROPERTIES = new String[] {"alias"};
+  private static final List<SingularAttribute<LibraryTemplate, String>> SEARCH_PROPERTIES =
+      Arrays.asList(LibraryTemplate_.alias);
 
   public HibernateLibraryTemplateDao() {
     super(LibraryTemplate.class);
@@ -33,53 +38,46 @@ public class HibernateLibraryTemplateDao extends HibernateSaveDao<LibraryTemplat
   }
 
   @Override
-  public String getProjectColumn() {
-    return "project.id";
+  public SingularAttribute<LibraryTemplate, ?> getIdProperty() {
+    return LibraryTemplate_.libraryTemplateId;
   }
 
   @Override
-  public Class<? extends LibraryTemplate> getRealClass() {
+  public Class<LibraryTemplate> getEntityClass() {
     return LibraryTemplate.class;
   }
 
   @Override
-  public String[] getSearchProperties() {
+  public Class<LibraryTemplate> getResultClass() {
+    return LibraryTemplate.class;
+  }
+
+  @Override
+  public List<SingularAttribute<LibraryTemplate, String>> getSearchProperties() {
     return SEARCH_PROPERTIES;
   }
 
   @Override
-  public Iterable<AliasDescriptor> listAliases() {
-    return Collections.emptyList();
-  }
-
-  @Override
-  public String propertyForDate(Criteria criteria, DateType type) {
+  public SingularAttribute<LibraryTemplate, ?> propertyForDate(DateType type) {
     return null;
   }
 
   @Override
-  public String propertyForSortColumn(String original) {
-    return original;
-  }
-
-  @Override
-  public String propertyForUser(boolean creator) {
+  public SingularAttribute<LibraryTemplate, ? extends UserImpl> propertyForUser(boolean creator) {
     return null;
   }
 
   @Override
   public LibraryTemplate getByAlias(String alias) throws IOException {
-    return getBy("alias", alias);
+    return getBy(LibraryTemplate_.ALIAS, alias);
   }
 
   @Override
   public List<LibraryTemplate> listByProject(long projectId) {
-    @SuppressWarnings("unchecked")
-    List<LibraryTemplate> list = currentSession().createCriteria(LibraryTemplate.class)
-        .createAlias("projects", "project")
-        .add(Restrictions.eq("project.id", projectId))
-        .list();
-    return list;
+    QueryBuilder<LibraryTemplate, LibraryTemplate> builder = getQueryBuilder();
+    Join<LibraryTemplate, ProjectImpl> projectJoin = builder.getJoin(builder.getRoot(), LibraryTemplate_.projects);
+    builder.addPredicate(builder.getCriteriaBuilder().equal(projectJoin.get(ProjectImpl_.id), projectId));
+    return builder.getResultList();
   }
 
   @Override
@@ -88,9 +86,10 @@ public class HibernateLibraryTemplateDao extends HibernateSaveDao<LibraryTemplat
   }
 
   @Override
-  public void restrictPaginationByProjectId(Criteria criteria, long projectId, Consumer<String> errorHandler) {
-    criteria.createAlias("projects", "project");
-    criteria.add(Restrictions.eq("project.id", projectId));
+  public void restrictPaginationByProjectId(QueryBuilder<?, LibraryTemplate> builder, long projectId,
+      Consumer<String> errorHandler) {
+    Join<LibraryTemplate, ProjectImpl> projectJoin = builder.getJoin(builder.getRoot(), LibraryTemplate_.projects);
+    builder.addPredicate(builder.getCriteriaBuilder().equal(projectJoin.get(ProjectImpl_.id), projectId));
   }
 
 }
