5d986e8bcb3f9c0469820c637f46075059ad8425
sinry-d <93559434+sinry-d@users.noreply.github.com>
2024-06-14 09:19:51 -0400

    [GLT-4166] Update HibernateLibraryTemplateDao to JPA Criteria (#2898)

--- sqlstore/src/it/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateLibraryTemplateDaoSearchIT.java
+++ sqlstore/src/it/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateLibraryTemplateDaoSearchIT.java
@@ -5,6 +5,7 @@ import java.util.EnumSet;
 import java.util.List;
 
 import uk.ac.bbsrc.tgac.miso.PaginationFilterSinkIT;
+import uk.ac.bbsrc.tgac.miso.core.util.PaginatedDataSource;
 
 public class HibernateLibraryTemplateDaoSearchIT extends PaginationFilterSinkIT {
 
@@ -17,7 +18,7 @@ public class HibernateLibraryTemplateDaoSearchIT extends PaginationFilterSinkIT 
   }
 
   @Override
-  protected HibernatePaginatedDataSource<?> constructTestSubject() {
+  protected PaginatedDataSource<?> constructTestSubject() {
     HibernateLibraryTemplateDao sut = new HibernateLibraryTemplateDao();
     sut.setSessionFactory(getSessionFactory());
     return sut;
