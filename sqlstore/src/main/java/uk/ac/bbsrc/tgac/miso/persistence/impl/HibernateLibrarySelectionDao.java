fd588a4fc2ada631c34d50261e23e188267f5a5d
sinry-d <93559434+sinry-d@users.noreply.github.com>
2024-06-14 08:59:13 -0400

    [GLT-4165]Update HibernateLibrarySelectionDao to JPA Criteria (#2897)

--- sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateLibrarySelectionDao.java
+++ sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateLibrarySelectionDao.java
@@ -4,14 +4,15 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 
-import org.hibernate.criterion.Projections;
-import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import uk.ac.bbsrc.tgac.miso.core.data.LibraryDesign;
+import uk.ac.bbsrc.tgac.miso.core.data.LibraryDesign_;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.LibraryImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.LibraryImpl_;
 import uk.ac.bbsrc.tgac.miso.core.data.type.LibrarySelectionType;
+import uk.ac.bbsrc.tgac.miso.core.data.type.LibrarySelectionType_;
 import uk.ac.bbsrc.tgac.miso.persistence.LibrarySelectionDao;
 
 @Transactional(rollbackFor = Exception.class)
@@ -25,25 +26,23 @@ public class HibernateLibrarySelectionDao extends HibernateSaveDao<LibrarySelect
 
   @Override
   public LibrarySelectionType getByName(String name) throws IOException {
-    return (LibrarySelectionType) currentSession().createCriteria(LibrarySelectionType.class)
-        .add(Restrictions.eq("name", name))
-        .uniqueResult();
+    return getBy(LibrarySelectionType_.NAME, name);
   }
 
   @Override
   public long getUsageByLibraries(LibrarySelectionType type) throws IOException {
-    return getUsageBy(LibraryImpl.class, "librarySelectionType", type);
+    return getUsageBy(LibraryImpl.class, LibraryImpl_.LIBRARY_SELECTION_TYPE, type);
   }
 
   @Override
   public long getUsageByLibraryDesigns(LibrarySelectionType type) throws IOException {
-    return getUsageBy(LibraryDesign.class, "librarySelectionType", type);
+    return getUsageBy(LibraryDesign.class, LibraryDesign_.LIBRARY_SELECTION_TYPE, type);
   }
 
 
   @Override
   public List<LibrarySelectionType> listByIdList(Collection<Long> idList) throws IOException {
-    return listByIdList("librarySelectionTypeId", idList);
+    return listByIdList(LibrarySelectionType_.LIBRARY_SELECTION_TYPE_ID, idList);
   }
 
 }
