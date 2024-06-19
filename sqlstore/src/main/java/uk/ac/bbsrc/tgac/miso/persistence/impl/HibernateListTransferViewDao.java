f974efcff98ac3cdb7fa56534e6dbdc0dbf5a570
Sinry Dong <sinryd@gmail.com>
2024-06-19 15:53:00 -0400

    [GLT-4172] Update HibernateListTransferViewDao to JPA Criteria

--- sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateListTransferViewDao.java
+++ sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateListTransferViewDao.java
@@ -1,30 +1,55 @@
 package uk.ac.bbsrc.tgac.miso.persistence.impl;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 import java.util.function.Consumer;
 
-import org.hibernate.Criteria;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaBuilder.In;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Root;
+import javax.persistence.metamodel.SingularAttribute;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Restrictions;
-import org.hibernate.sql.JoinType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.eaglegenomics.simlims.core.Group;
 
+import uk.ac.bbsrc.tgac.miso.core.data.impl.UserImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.view.ParentSubproject;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.view.ParentSubproject_;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.view.transfer.ListTransferView;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.view.transfer.ListTransferViewLibrary;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.view.transfer.ListTransferViewLibraryAliquot;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.view.transfer.ListTransferViewLibraryAliquotParent;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.view.transfer.ListTransferViewLibraryAliquotParent_;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.view.transfer.ListTransferViewLibraryAliquot_;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.view.transfer.ListTransferViewLibraryParent;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.view.transfer.ListTransferViewLibraryParent_;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.view.transfer.ListTransferViewLibrary_;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.view.transfer.ListTransferViewPool;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.view.transfer.ListTransferViewPool_;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.view.transfer.ListTransferViewProject;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.view.transfer.ListTransferViewProject_;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.view.transfer.ListTransferViewSample;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.view.transfer.ListTransferViewSampleParent;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.view.transfer.ListTransferViewSampleParent_;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.view.transfer.ListTransferViewSample_;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.view.transfer.ListTransferView_;
 import uk.ac.bbsrc.tgac.miso.core.util.DateType;
 import uk.ac.bbsrc.tgac.miso.core.util.TransferType;
 import uk.ac.bbsrc.tgac.miso.persistence.ListTransferViewDao;
-import uk.ac.bbsrc.tgac.miso.persistence.util.DbUtils;
 
 @Transactional(rollbackFor = Exception.class)
 @Repository
 public class HibernateListTransferViewDao
-    implements ListTransferViewDao, HibernatePaginatedDataSource<ListTransferView> {
+    implements ListTransferViewDao, JpaCriteriaPaginatedDataSource<ListTransferView, ListTransferView> {
 
   @Autowired
   private SessionFactory sessionFactory;
@@ -44,86 +69,113 @@ public class HibernateListTransferViewDao
   }
 
   @Override
-  public String getProjectColumn() {
-    return null;
+  public SingularAttribute<ListTransferView, ?> getIdProperty() {
+    return ListTransferView_.transferId;
   }
 
   @Override
-  public Class<? extends ListTransferView> getRealClass() {
+  public Class<ListTransferView> getEntityClass() {
     return ListTransferView.class;
   }
 
   @Override
-  public String[] getSearchProperties() {
-    return new String[0];
+  public Class<ListTransferView> getResultClass() {
+    return ListTransferView.class;
   }
 
   @Override
-  public Iterable<AliasDescriptor> listAliases() {
+  public List<SingularAttribute<ListTransferView, String>> getSearchProperties() {
     return Collections.emptyList();
   }
 
   @Override
-  public String propertyForDate(Criteria criteria, DateType type) {
+  public SingularAttribute<ListTransferView, ?> propertyForDate(DateType type) {
     switch (type) {
       case ENTERED:
-        return "created";
+        return ListTransferView_.created;
       case CREATE:
       case RECEIVE:
-        return "transferTime";
+        return ListTransferView_.transferTime;
       case UPDATE:
-        return "lastModified";
+        return ListTransferView_.lastModified;
       default:
         return null;
     }
   }
 
   @Override
-  public String propertyForSortColumn(String original) {
-    return original;
+  public Path<?> propertyForSortColumn(Root<ListTransferView> root, String original) {
+    if ("id".equals(original)) {
+      return root.get(ListTransferView_.transferId);
+    } else {
+      return root.get(original);
+    }
   }
 
   @Override
-  public String propertyForUser(boolean creator) {
-    return creator ? "creator" : "lastModifier";
+  public SingularAttribute<ListTransferView, ? extends UserImpl> propertyForUser(boolean creator) {
+    return creator ? ListTransferView_.creator : ListTransferView_.lastModifier;
   }
 
   @Override
-  public void restrictPaginationByPending(Criteria criteria, Consumer<String> errorHandler) {
-    criteria.createAlias("samples", "sample", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("libraries", "library", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("libraryAliquots", "aliquot", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("pools", "pool", JoinType.LEFT_OUTER_JOIN)
-        .add(Restrictions.or(
-            Restrictions.and(Restrictions.isNotEmpty("samples"),
-                Restrictions.or(Restrictions.isNull("sample.received"), Restrictions.isNull("sample.qcPassed"))),
-            Restrictions.and(Restrictions.isNotEmpty("libraries"),
-                Restrictions.or(Restrictions.isNull("library.received"), Restrictions.isNull("library.qcPassed"))),
-            Restrictions.and(Restrictions.isNotEmpty("libraryAliquots"),
-                Restrictions.or(Restrictions.isNull("aliquot.received"), Restrictions.isNull("aliquot.qcPassed"))),
-            Restrictions.and(Restrictions.isNotEmpty("pools"),
-                Restrictions.or(Restrictions.isNull("pool.received"), Restrictions.isNull("pool.qcPassed")))));
+  public void restrictPaginationByPending(QueryBuilder<?, ListTransferView> builder, Consumer<String> errorHandler) {
+    Join<ListTransferView, ListTransferViewSample> sampleJoin =
+        builder.getJoin(builder.getRoot(), ListTransferView_.samples);
+    Join<ListTransferView, ListTransferViewLibrary> libraryJoin =
+        builder.getJoin(builder.getRoot(), ListTransferView_.libraries);
+    Join<ListTransferView, ListTransferViewLibraryAliquot> aliquotJoin =
+        builder.getJoin(builder.getRoot(), ListTransferView_.libraryAliquots);
+    Join<ListTransferView, ListTransferViewPool> poolJoin =
+        builder.getJoin(builder.getRoot(), ListTransferView_.pools);
+
+    CriteriaBuilder criteriaBuilder = builder.getCriteriaBuilder();
+
+    builder.addPredicate(criteriaBuilder.or(
+        criteriaBuilder.and(criteriaBuilder.isNotEmpty(builder.getRoot().get(ListTransferView_.samples)),
+            criteriaBuilder.or(
+                criteriaBuilder.isNull(sampleJoin.get(ListTransferViewSample_.received)),
+                criteriaBuilder.isNull(sampleJoin.get(ListTransferViewSample_.qcPassed)))),
+        criteriaBuilder.and(criteriaBuilder.isNotEmpty(builder.getRoot().get(ListTransferView_.libraries)),
+            criteriaBuilder.or(
+                criteriaBuilder.isNull(libraryJoin.get(ListTransferViewLibrary_.received)),
+                criteriaBuilder.isNull(libraryJoin.get(ListTransferViewLibrary_.qcPassed)))),
+        criteriaBuilder.and(criteriaBuilder.isNotEmpty(builder.getRoot().get(ListTransferView_.libraryAliquots)),
+            criteriaBuilder.or(
+                criteriaBuilder.isNull(aliquotJoin.get(ListTransferViewLibraryAliquot_.received)),
+                criteriaBuilder.isNull(aliquotJoin.get(ListTransferViewLibraryAliquot_.qcPassed)))),
+        criteriaBuilder.and(criteriaBuilder.isNotEmpty(builder.getRoot().get(ListTransferView_.pools)),
+            criteriaBuilder.or(
+                criteriaBuilder.isNull(poolJoin.get(ListTransferViewPool_.received)),
+                criteriaBuilder.isNull(poolJoin.get(ListTransferViewPool_.qcPassed))))));
   }
 
   @Override
-  public void restrictPaginationByRecipientGroups(Criteria criteria, Collection<Group> groups,
+  public void restrictPaginationByRecipientGroups(QueryBuilder<?, ListTransferView> builder, Collection<Group> groups,
       Consumer<String> errorHandler) {
-    criteria.add(Restrictions.in("recipientGroup", groups));
+    In<Group> inClause = builder.getCriteriaBuilder().in(builder.getRoot().get(ListTransferView_.recipientGroup));
+    for (Group group : groups) {
+      inClause.value(group);
+    }
+    builder.addPredicate(inClause);
   }
 
   @Override
-  public void restrictPaginationByTransferType(Criteria criteria, TransferType transferType,
+  public void restrictPaginationByTransferType(QueryBuilder<?, ListTransferView> builder, TransferType transferType,
       Consumer<String> errorHandler) {
     switch (transferType) {
       case DISTRIBUTION:
-        criteria.add(Restrictions.isNotNull("recipient"));
+        builder
+            .addPredicate(builder.getCriteriaBuilder().isNotNull(builder.getRoot().get(ListTransferView_.recipient)));
         break;
       case INTERNAL:
-        criteria.add(Restrictions.isNotNull("senderGroup"))
-            .add(Restrictions.isNotNull("recipientGroup"));
+        builder.addPredicate(
+            builder.getCriteriaBuilder().isNotNull(builder.getRoot().get(ListTransferView_.senderGroup)));
+        builder.addPredicate(
+            builder.getCriteriaBuilder().isNotNull(builder.getRoot().get(ListTransferView_.recipientGroup)));
         break;
       case RECEIPT:
-        criteria.add(Restrictions.isNotNull("senderLab"));
+        builder.addPredicate(
+            builder.getCriteriaBuilder().isNotNull(builder.getRoot().get(ListTransferView_.senderLab)));
         break;
       default:
         throw new IllegalArgumentException("Unhandled transfer type: " + transferType);
@@ -131,39 +183,73 @@ public class HibernateListTransferViewDao
   }
 
   @Override
-  public void restrictPaginationByProject(Criteria criteria, String project, Consumer<String> errorHandler) {
-    criteria.createAlias("samples", "sample", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("sample.sample", "parentSample", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("parentSample.project", "sampleProject", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("libraries", "library", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("library.library", "parentLibrary", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("parentLibrary.sample", "librarySample", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("librarySample.project", "libraryProject", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("libraryAliquots", "aliquot", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("aliquot.aliquot", "parentAliquot", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("parentAliquot.library", "aliquotLibrary", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("aliquotLibrary.sample", "aliquotSample", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("aliquotSample.project", "aliquotProject", JoinType.LEFT_OUTER_JOIN)
-        .add(DbUtils.textRestriction(project, "sampleProject.name", "sampleProject.code", "libraryProject.name",
-            "libraryProject.code", "aliquotProject.name", "aliquotProject.code"));
+  public void restrictPaginationByProject(QueryBuilder<?, ListTransferView> builder, String project,
+      Consumer<String> errorHandler) {
+
+    Join<ListTransferView, ListTransferViewSample> sample =
+        builder.getJoin(builder.getRoot(), ListTransferView_.samples);
+    Join<ListTransferViewSample, ListTransferViewSampleParent> parentSample =
+        builder.getJoin(sample, ListTransferViewSample_.sample);
+    Join<ListTransferViewSampleParent, ListTransferViewProject> sampleProject =
+        builder.getJoin(parentSample, ListTransferViewSampleParent_.project);
+    Join<ListTransferView, ListTransferViewLibrary> library =
+        builder.getJoin(builder.getRoot(), ListTransferView_.libraries);
+    Join<ListTransferViewLibrary, ListTransferViewLibraryParent> parentLibrary =
+        builder.getJoin(library, ListTransferViewLibrary_.library);
+    Join<ListTransferViewLibraryParent, ListTransferViewSampleParent> librarySample =
+        builder.getJoin(parentLibrary, ListTransferViewLibraryParent_.sample);
+    Join<ListTransferViewSampleParent, ListTransferViewProject> libraryProject =
+        builder.getJoin(librarySample, ListTransferViewSampleParent_.project);
+    Join<ListTransferView, ListTransferViewLibraryAliquot> aliquot =
+        builder.getJoin(builder.getRoot(), ListTransferView_.libraryAliquots);
+    Join<ListTransferViewLibraryAliquot, ListTransferViewLibraryAliquotParent> parentAliquot =
+        builder.getJoin(aliquot, ListTransferViewLibraryAliquot_.aliquot);
+    Join<ListTransferViewLibraryAliquotParent, ListTransferViewLibraryParent> aliquotLibrary =
+        builder.getJoin(parentAliquot, ListTransferViewLibraryAliquotParent_.library);
+    Join<ListTransferViewLibraryParent, ListTransferViewSampleParent> aliquotSample =
+        builder.getJoin(aliquotLibrary, ListTransferViewLibraryParent_.sample);
+    Join<ListTransferViewSampleParent, ListTransferViewProject> aliquotProject =
+        builder.getJoin(aliquotSample, ListTransferViewSampleParent_.project);
+
+    builder.addTextRestriction(Arrays.asList(
+        sampleProject.get(ListTransferViewProject_.name), sampleProject.get(ListTransferViewProject_.code),
+        libraryProject.get(ListTransferViewProject_.name), libraryProject.get(ListTransferViewProject_.code),
+        aliquotProject.get(ListTransferViewProject_.name), aliquotProject.get(ListTransferViewProject_.code)), project);
   }
 
   @Override
-  public void restrictPaginationBySubproject(Criteria criteria, String subproject, Consumer<String> errorHandler) {
-    criteria.createAlias("samples", "sample", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("sample.sample", "parentSample", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("parentSample.subproject", "sampleSubproject", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("libraries", "library", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("library.library", "parentLibrary", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("parentLibrary.sample", "librarySample", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("librarySample.subproject", "librarySubproject", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("libraryAliquots", "aliquot", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("aliquot.aliquot", "parentAliquot", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("parentAliquot.library", "aliquotLibrary", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("aliquotLibrary.sample", "aliquotSample", JoinType.LEFT_OUTER_JOIN)
-        .createAlias("aliquotSample.subproject", "aliquotSubproject", JoinType.LEFT_OUTER_JOIN)
-        .add(DbUtils.textRestriction(subproject, "sampleSubproject.alias", "librarySubproject.alias",
-            "aliquotSubproject.alias"));
+  public void restrictPaginationBySubproject(QueryBuilder<?, ListTransferView> builder, String subproject,
+      Consumer<String> errorHandler) {
+
+    Join<ListTransferView, ListTransferViewSample> sample =
+        builder.getJoin(builder.getRoot(), ListTransferView_.samples);
+    Join<ListTransferViewSample, ListTransferViewSampleParent> parentSample =
+        builder.getJoin(sample, ListTransferViewSample_.sample);
+    Join<ListTransferViewSampleParent, ParentSubproject> sampleSubproject =
+        builder.getJoin(parentSample, ListTransferViewSampleParent_.subproject);
+    Join<ListTransferView, ListTransferViewLibrary> library =
+        builder.getJoin(builder.getRoot(), ListTransferView_.libraries);
+    Join<ListTransferViewLibrary, ListTransferViewLibraryParent> parentLibrary =
+        builder.getJoin(library, ListTransferViewLibrary_.library);
+    Join<ListTransferViewLibraryParent, ListTransferViewSampleParent> librarySample =
+        builder.getJoin(parentLibrary, ListTransferViewLibraryParent_.sample);
+    Join<ListTransferViewSampleParent, ParentSubproject> librarySubproject =
+        builder.getJoin(librarySample, ListTransferViewSampleParent_.subproject);
+    Join<ListTransferView, ListTransferViewLibraryAliquot> aliquot =
+        builder.getJoin(builder.getRoot(), ListTransferView_.libraryAliquots);
+    Join<ListTransferViewLibraryAliquot, ListTransferViewLibraryAliquotParent> parentAliquot =
+        builder.getJoin(aliquot, ListTransferViewLibraryAliquot_.aliquot);
+    Join<ListTransferViewLibraryAliquotParent, ListTransferViewLibraryParent> aliquotLibrary =
+        builder.getJoin(parentAliquot, ListTransferViewLibraryAliquotParent_.library);
+    Join<ListTransferViewLibraryParent, ListTransferViewSampleParent> aliquotSample =
+        builder.getJoin(aliquotLibrary, ListTransferViewLibraryParent_.sample);
+    Join<ListTransferViewSampleParent, ParentSubproject> aliquotSubproject =
+        builder.getJoin(aliquotSample, ListTransferViewSampleParent_.subproject);
+
+    builder.addTextRestriction(Arrays.asList(
+        sampleSubproject.get(ParentSubproject_.alias),
+        librarySubproject.get(ParentSubproject_.alias),
+        aliquotSubproject.get(ParentSubproject_.alias)), subproject);
   }
 
 }
