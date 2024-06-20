7c457c32476bd1fc89e8583de4ff135058adf50f
Sinry Dong <sinryd@gmail.com>
2024-06-20 09:45:57 -0400

    [GLT-4175] Update Hibernate MetricDao to JPA Criteria

--- sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateMetricDao.java
+++ sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateMetricDao.java
@@ -4,14 +4,18 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 
-import org.hibernate.criterion.Projections;
-import org.hibernate.criterion.Restrictions;
+import javax.persistence.criteria.Join;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import uk.ac.bbsrc.tgac.miso.core.data.impl.Assay;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.AssayMetric;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.AssayMetric_;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.Assay_;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.Metric;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.MetricSubcategory;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.Metric_;
 import uk.ac.bbsrc.tgac.miso.core.data.type.MetricCategory;
 import uk.ac.bbsrc.tgac.miso.persistence.MetricDao;
 
@@ -26,24 +30,26 @@ public class HibernateMetricDao extends HibernateSaveDao<Metric> implements Metr
   @Override
   public Metric getByAliasAndCategory(String alias, MetricCategory category, MetricSubcategory subcategory)
       throws IOException {
-    return (Metric) currentSession().createCriteria(getEntityClass())
-        .add(Restrictions.eq("alias", alias))
-        .add(Restrictions.eq("category", category))
-        .add(Restrictions.eqOrIsNull("subcategory", subcategory))
-        .uniqueResult();
+    QueryBuilder<Metric, Metric> builder = new QueryBuilder<>(currentSession(), Metric.class, Metric.class);
+    builder.addPredicate(builder.getCriteriaBuilder().equal(builder.getRoot().get(Metric_.alias), alias));
+    builder.addPredicate(builder.getCriteriaBuilder().equal(builder.getRoot().get(Metric_.category), category));
+    builder.addPredicate(builder.getCriteriaBuilder().or(
+        builder.getCriteriaBuilder().equal(builder.getRoot().get(Metric_.subcategory), subcategory),
+        builder.getCriteriaBuilder().isNull(builder.getRoot().get(Metric_.subcategory))));
+    return builder.getSingleResultOrNull();
   }
 
   @Override
   public long getUsage(Metric metric) throws IOException {
-    return (long) currentSession().createCriteria(Assay.class)
-        .createAlias("assayMetrics", "assayMetric")
-        .add(Restrictions.eq("assayMetric.metric", metric))
-        .setProjection(Projections.rowCount()).uniqueResult();
+    LongQueryBuilder<Assay> builder = new LongQueryBuilder<>(currentSession(), Assay.class);
+    Join<Assay, AssayMetric> metricJoin = builder.getJoin(builder.getRoot(), Assay_.assayMetrics);
+    builder.addPredicate(builder.getCriteriaBuilder().equal(metricJoin.get(AssayMetric_.metric), metric));
+    return builder.getCount();
   }
 
   @Override
   public List<Metric> listByIdList(Collection<Long> ids) throws IOException {
-    return listByIdList("metricId", ids);
+    return listByIdList(Metric_.METRIC_ID, ids);
   }
 
 }
