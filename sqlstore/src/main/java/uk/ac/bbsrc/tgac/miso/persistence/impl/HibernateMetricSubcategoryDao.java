2f115bb02a48db1c1626261ee8bcc660cb1f67af
sinry-d <93559434+sinry-d@users.noreply.github.com>
2024-06-25 11:31:00 -0400

    [GLT-4176] Update HibernateMetricSubcategoryDao to JPA Criteria (#2909)

--- sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateMetricSubcategoryDao.java
+++ sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateMetricSubcategoryDao.java
@@ -4,12 +4,13 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 
-import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import uk.ac.bbsrc.tgac.miso.core.data.impl.Metric;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.MetricSubcategory;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.MetricSubcategory_;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.Metric_;
 import uk.ac.bbsrc.tgac.miso.core.data.type.MetricCategory;
 import uk.ac.bbsrc.tgac.miso.persistence.MetricSubcategoryDao;
 
@@ -23,20 +24,22 @@ public class HibernateMetricSubcategoryDao extends HibernateSaveDao<MetricSubcat
 
   @Override
   public MetricSubcategory getByAliasAndCategory(String alias, MetricCategory category) throws IOException {
-    return (MetricSubcategory) currentSession().createCriteria(getEntityClass())
-        .add(Restrictions.eq("alias", alias))
-        .add(Restrictions.eq("category", category))
-        .uniqueResult();
+    QueryBuilder<MetricSubcategory, MetricSubcategory> builder =
+        new QueryBuilder<>(currentSession(), MetricSubcategory.class, MetricSubcategory.class);
+    builder.addPredicate(builder.getCriteriaBuilder().equal(builder.getRoot().get(MetricSubcategory_.alias), alias));
+    builder
+        .addPredicate(builder.getCriteriaBuilder().equal(builder.getRoot().get(MetricSubcategory_.category), category));
+    return builder.getSingleResultOrNull();
   }
 
   @Override
   public long getUsage(MetricSubcategory subcategory) throws IOException {
-    return getUsageBy(Metric.class, "subcategory", subcategory);
+    return getUsageBy(Metric.class, Metric_.SUBCATEGORY, subcategory);
   }
 
   @Override
   public List<MetricSubcategory> listByIdList(Collection<Long> ids) throws IOException {
-    return listByIdList("subcategoryId", ids);
+    return listByIdList(MetricSubcategory_.SUBCATEGORY_ID, ids);
   }
 
 }
