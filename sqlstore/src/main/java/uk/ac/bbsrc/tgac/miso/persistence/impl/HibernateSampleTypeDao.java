77750d833336724e216f6b1ba4a9ecb5b120b3c5
sinry-d <93559434+sinry-d@users.noreply.github.com>
2024-07-02 10:04:05 -0400

    [GLT-4199] Update HibernateSampleTypeDao to JPA Criteria (#2927)

--- sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateSampleTypeDao.java
+++ sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateSampleTypeDao.java
@@ -4,13 +4,13 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 
-import org.hibernate.criterion.Projections;
-import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import uk.ac.bbsrc.tgac.miso.core.data.SampleType;
+import uk.ac.bbsrc.tgac.miso.core.data.SampleType_;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.SampleImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.SampleImpl_;
 import uk.ac.bbsrc.tgac.miso.persistence.SampleTypeDao;
 
 @Repository
@@ -23,19 +23,20 @@ public class HibernateSampleTypeDao extends HibernateSaveDao<SampleType> impleme
 
   @Override
   public SampleType getByName(String name) throws IOException {
-    return getBy("name", name);
+    return getBy(SampleType_.NAME, name);
   }
 
   @Override
   public long getUsage(SampleType sampleType) throws IOException {
-    return (long) currentSession().createCriteria(SampleImpl.class)
-        .add(Restrictions.eq("sampleType", sampleType.getName()))
-        .setProjection(Projections.rowCount()).uniqueResult();
+    LongQueryBuilder<SampleImpl> builder = new LongQueryBuilder<>(currentSession(), SampleImpl.class);
+    builder.addPredicate(
+        builder.getCriteriaBuilder().equal(builder.getRoot().get(SampleImpl_.sampleType), sampleType.getName()));
+    return builder.getCount();
   }
 
   @Override
   public List<SampleType> listByIdList(Collection<Long> ids) throws IOException {
-    return listByIdList("typeId", ids);
+    return listByIdList(SampleType_.TYPE_ID, ids);
   }
 
 }
