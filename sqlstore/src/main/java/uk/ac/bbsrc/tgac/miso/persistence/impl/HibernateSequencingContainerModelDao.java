2d08f21e14a835708802b55fe962e289ea619ab5
sinry-d <93559434+sinry-d@users.noreply.github.com>
2024-07-03 15:57:18 -0400

    [GLT-4203] Update HibernateSequencingContainerModelDao to JPA Criteria (#2931)
    * [GLT-4203] Update HibernateSequencingContainerModelDao to JPA Criteria
    
    * More refactoring

--- sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateSequencingContainerModelDao.java
+++ sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateSequencingContainerModelDao.java
@@ -4,16 +4,23 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 
-import org.hibernate.Criteria;
-import org.hibernate.criterion.MatchMode;
-import org.hibernate.criterion.Projections;
-import org.hibernate.criterion.Restrictions;
+import javax.persistence.criteria.Join;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import uk.ac.bbsrc.tgac.miso.core.data.InstrumentModel;
+import uk.ac.bbsrc.tgac.miso.core.data.InstrumentModel_;
+import uk.ac.bbsrc.tgac.miso.core.data.Run;
+import uk.ac.bbsrc.tgac.miso.core.data.Run_;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.InstrumentImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.InstrumentImpl_;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.RunPosition;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.RunPosition_;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.SequencerPartitionContainerImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.SequencerPartitionContainerImpl_;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.SequencingContainerModel;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.SequencingContainerModel_;
 import uk.ac.bbsrc.tgac.miso.core.data.type.PlatformType;
 import uk.ac.bbsrc.tgac.miso.core.util.LimsUtils;
 import uk.ac.bbsrc.tgac.miso.persistence.SequencingContainerModelStore;
@@ -30,24 +37,38 @@ public class HibernateSequencingContainerModelDao extends HibernateSaveDao<Seque
   @Override
   public SequencingContainerModel find(InstrumentModel instrumentModel, String search, int partitionCount) {
     SequencingContainerModel model;
-    Criteria criteria = currentSession().createCriteria(SequencingContainerModel.class);
-    criteria.createAlias("instrumentModels", "instrumentModel");
-    criteria.add(Restrictions.eq("instrumentModel.id", instrumentModel.getId()));
-    criteria.add(Restrictions.eq("partitionCount", partitionCount));
+    QueryBuilder<SequencingContainerModel, SequencingContainerModel> builder =
+        new QueryBuilder<>(currentSession(), SequencingContainerModel.class, SequencingContainerModel.class);
+    Join<SequencingContainerModel, InstrumentModel> instrument =
+        builder.getJoin(builder.getRoot(), SequencingContainerModel_.instrumentModels);
+    builder.addPredicate(builder.getCriteriaBuilder().equal(instrument.get(InstrumentModel_.instrumentModelId),
+        instrumentModel.getId()));
+    builder.addPredicate(builder.getCriteriaBuilder()
+        .equal(builder.getRoot().get(SequencingContainerModel_.partitionCount), partitionCount));
+
     if (LimsUtils.isStringEmptyOrNull(search)) {
-      criteria.add(Restrictions.eq("fallback", true));
-      model = (SequencingContainerModel) criteria.uniqueResult();
+      builder.addPredicate(
+          builder.getCriteriaBuilder().equal(builder.getRoot().get(SequencingContainerModel_.fallback), true));
+      model = (SequencingContainerModel) builder.getSingleResultOrNull();
     } else {
-      criteria.add(Restrictions.or(Restrictions.eq("alias", search), Restrictions.eq("identificationBarcode", search)));
-      model = (SequencingContainerModel) criteria.uniqueResult();
+      builder.addPredicate(builder.getCriteriaBuilder().or(
+          builder.getCriteriaBuilder().equal(builder.getRoot().get(SequencingContainerModel_.alias), search),
+          builder.getCriteriaBuilder().equal(builder.getRoot().get(SequencingContainerModel_.identificationBarcode),
+              search)));
+      model = (SequencingContainerModel) builder.getSingleResultOrNull();
       if (model == null) {
         // remove search restriction and get fallback option if search did not retrieve anything
-        Criteria fallback = currentSession().createCriteria(SequencingContainerModel.class);
-        fallback.createAlias("instrumentModels", "instrumentModel");
-        fallback.add(Restrictions.eq("instrumentModel.id", instrumentModel.getId()));
-        fallback.add(Restrictions.eq("partitionCount", partitionCount));
-        fallback.add(Restrictions.eq("fallback", true));
-        model = (SequencingContainerModel) fallback.uniqueResult();
+        QueryBuilder<SequencingContainerModel, SequencingContainerModel> fallback =
+            new QueryBuilder<>(currentSession(), SequencingContainerModel.class, SequencingContainerModel.class);
+        Join<SequencingContainerModel, InstrumentModel> instrumentJoin =
+            fallback.getJoin(fallback.getRoot(), SequencingContainerModel_.instrumentModels);
+        fallback.addPredicate(fallback.getCriteriaBuilder()
+            .equal(instrumentJoin.get(InstrumentModel_.instrumentModelId), instrumentModel.getId()));
+        fallback.addPredicate(fallback.getCriteriaBuilder()
+            .equal(fallback.getRoot().get(SequencingContainerModel_.partitionCount), partitionCount));
+        fallback.addPredicate(
+            fallback.getCriteriaBuilder().equal(fallback.getRoot().get(SequencingContainerModel_.fallback), true));
+        model = (SequencingContainerModel) fallback.getSingleResultOrNull();
       }
     }
     return model;
@@ -55,50 +76,63 @@ public class HibernateSequencingContainerModelDao extends HibernateSaveDao<Seque
 
   @Override
   public List<SequencingContainerModel> find(PlatformType platform, String search) throws IOException {
-    @SuppressWarnings("unchecked")
-    List<SequencingContainerModel> results = currentSession().createCriteria(SequencingContainerModel.class)
-        .add(Restrictions.eq("platformType", platform))
-        .add(Restrictions.or(Restrictions.ilike("alias", search, MatchMode.START), Restrictions.eq("identificationBarcode", search)))
-        .list();
-    return results;
+    QueryBuilder<SequencingContainerModel, SequencingContainerModel> builder =
+        new QueryBuilder<>(currentSession(), SequencingContainerModel.class, SequencingContainerModel.class);
+    builder.addPredicate(
+        builder.getCriteriaBuilder().equal(builder.getRoot().get(SequencingContainerModel_.platformType), platform));
+    builder.addPredicate(builder.getCriteriaBuilder().or(
+        builder.getCriteriaBuilder().like(builder.getRoot().get(SequencingContainerModel_.alias), search + '%'),
+        builder.getCriteriaBuilder().equal(builder.getRoot().get(SequencingContainerModel_.identificationBarcode),
+            search)));
+    return builder.getResultList();
   }
 
   @Override
   public SequencingContainerModel getByPlatformAndAlias(PlatformType platform, String alias) throws IOException {
-    return (SequencingContainerModel) currentSession().createCriteria(SequencingContainerModel.class)
-        .add(Restrictions.eq("platformType", platform))
-        .add(Restrictions.eq("alias", alias))
-        .uniqueResult();
+    QueryBuilder<SequencingContainerModel, SequencingContainerModel> builder =
+        new QueryBuilder<>(currentSession(), SequencingContainerModel.class, SequencingContainerModel.class);
+    builder.addPredicate(
+        builder.getCriteriaBuilder().equal(builder.getRoot().get(SequencingContainerModel_.platformType), platform));
+    builder.addPredicate(
+        builder.getCriteriaBuilder().equal(builder.getRoot().get(SequencingContainerModel_.alias), alias));
+    return builder.getSingleResultOrNull();
   }
 
   @Override
-  public SequencingContainerModel getByPlatformAndBarcode(PlatformType platform, String identificationBarcode) throws IOException {
-    return (SequencingContainerModel) currentSession().createCriteria(SequencingContainerModel.class)
-        .add(Restrictions.eq("platformType", platform))
-        .add(Restrictions.eq("identificationBarcode", identificationBarcode))
-        .uniqueResult();
+  public SequencingContainerModel getByPlatformAndBarcode(PlatformType platform, String identificationBarcode)
+      throws IOException {
+    QueryBuilder<SequencingContainerModel, SequencingContainerModel> builder =
+        new QueryBuilder<>(currentSession(), SequencingContainerModel.class, SequencingContainerModel.class);
+    builder.addPredicate(
+        builder.getCriteriaBuilder().equal(builder.getRoot().get(SequencingContainerModel_.platformType), platform));
+    builder.addPredicate(builder.getCriteriaBuilder()
+        .equal(builder.getRoot().get(SequencingContainerModel_.identificationBarcode), identificationBarcode));
+    return builder.getSingleResultOrNull();
   }
 
   @Override
   public long getUsage(SequencingContainerModel model) throws IOException {
-    return getUsageBy(SequencerPartitionContainerImpl.class, "model", model);
+    return getUsageBy(SequencerPartitionContainerImpl.class, SequencerPartitionContainerImpl_.model, model);
   }
 
   @Override
   public long getUsage(SequencingContainerModel containerModel, InstrumentModel instrumentModel) throws IOException {
-    return (long) currentSession().createCriteria(SequencerPartitionContainerImpl.class)
-        .add(Restrictions.eq("model", containerModel))
-        .createAlias("runPositions", "runPosition")
-        .createAlias("runPosition.run", "run")
-        .createAlias("run.sequencer", "sequencer")
-        .add(Restrictions.eq("sequencer.instrumentModel", instrumentModel))
-        .setProjection(Projections.rowCount())
-        .uniqueResult();
+    LongQueryBuilder<SequencerPartitionContainerImpl> builder =
+        new LongQueryBuilder<>(currentSession(), SequencerPartitionContainerImpl.class);
+    Join<SequencerPartitionContainerImpl, RunPosition> runPosition =
+        builder.getJoin(builder.getRoot(), SequencerPartitionContainerImpl_.runPositions);
+    Join<RunPosition, Run> run = builder.getJoin(runPosition, RunPosition_.run);
+    Join<Run, InstrumentImpl> sequencer = builder.getJoin(run, Run_.sequencer);
+    builder.addPredicate(builder.getCriteriaBuilder()
+        .equal(builder.getRoot().get(SequencerPartitionContainerImpl_.model), containerModel));
+    builder.addPredicate(
+        builder.getCriteriaBuilder().equal(sequencer.get(InstrumentImpl_.instrumentModel), instrumentModel));
+    return builder.getCount();
   }
 
   @Override
   public List<SequencingContainerModel> listByIdList(Collection<Long> idList) throws IOException {
-    return listByIdList("sequencingContainerModelId", idList);
+    return listByIdList(SequencingContainerModel_.SEQUENCING_CONTAINER_MODEL_ID, idList);
   }
 
 }
