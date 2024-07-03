1b2472cd8d74df4368d29c8a421abbe165eaa9ec
sinry-d <93559434+sinry-d@users.noreply.github.com>
2024-07-03 15:22:06 -0400

    [GLT-4200] Update HibernateSampleValidRelationshipDao to JPA Criteria (#2929)

--- sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/SampleValidRelationshipDao.java
+++ sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/SampleValidRelationshipDao.java
@@ -1,16 +1,11 @@
 package uk.ac.bbsrc.tgac.miso.persistence;
 
 import java.io.IOException;
-import java.util.List;
 
 import uk.ac.bbsrc.tgac.miso.core.data.SampleClass;
 import uk.ac.bbsrc.tgac.miso.core.data.SampleValidRelationship;
 
-public interface SampleValidRelationshipDao {
-
-  public List<SampleValidRelationship> list();
-
-  public SampleValidRelationship get(Long id);
+public interface SampleValidRelationshipDao extends ProviderDao<SampleValidRelationship> {
 
   public SampleValidRelationship getByClasses(SampleClass parent, SampleClass child) throws IOException;
 
--- sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateSampleValidRelationshipDao.java
+++ sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateSampleValidRelationshipDao.java
@@ -1,57 +1,34 @@
 package uk.ac.bbsrc.tgac.miso.persistence.impl;
 
 import java.io.IOException;
-import java.util.List;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Restrictions;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import uk.ac.bbsrc.tgac.miso.core.data.SampleClass;
 import uk.ac.bbsrc.tgac.miso.core.data.SampleValidRelationship;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.SampleValidRelationshipImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.SampleValidRelationshipImpl_;
 import uk.ac.bbsrc.tgac.miso.persistence.SampleValidRelationshipDao;
 
 @Repository
 @Transactional(rollbackFor = Exception.class)
-public class HibernateSampleValidRelationshipDao implements SampleValidRelationshipDao {
+public class HibernateSampleValidRelationshipDao extends HibernateProviderDao<SampleValidRelationship>
+    implements SampleValidRelationshipDao {
 
-  protected static final Logger log = LoggerFactory.getLogger(HibernateSampleValidRelationshipDao.class);
-
-  @Autowired
-  private SessionFactory sessionFactory;
-
-  public void setSessionFactory(SessionFactory sessionFactory) {
-    this.sessionFactory = sessionFactory;
-  }
-
-  private Session currentSession() {
-    return sessionFactory.getCurrentSession();
-  }
-
-  @Override
-  public List<SampleValidRelationship> list() {
-    @SuppressWarnings("unchecked")
-    List<SampleValidRelationship> records = currentSession().createCriteria(SampleValidRelationshipImpl.class).list();
-    return records;
-  }
-
-  @Override
-  public SampleValidRelationship get(Long id) {
-    return (SampleValidRelationship) currentSession().get(SampleValidRelationshipImpl.class, id);
+  public HibernateSampleValidRelationshipDao() {
+    super(SampleValidRelationship.class, SampleValidRelationshipImpl.class);
   }
 
   @Override
   public SampleValidRelationship getByClasses(SampleClass parent, SampleClass child) throws IOException {
-    return (SampleValidRelationship) currentSession().createCriteria(SampleValidRelationshipImpl.class)
-        .add(Restrictions.eq("parent", parent))
-        .add(Restrictions.eq("child", child))
-        .uniqueResult();
+    QueryBuilder<SampleValidRelationship, SampleValidRelationshipImpl> builder =
+        new QueryBuilder<>(currentSession(), SampleValidRelationshipImpl.class, SampleValidRelationship.class);
+    builder.addPredicate(
+        builder.getCriteriaBuilder().equal(builder.getRoot().get(SampleValidRelationshipImpl_.parent), parent));
+    builder.addPredicate(
+        builder.getCriteriaBuilder().equal(builder.getRoot().get(SampleValidRelationshipImpl_.child), child));
+    return builder.getSingleResultOrNull();
   }
 
   @Override
