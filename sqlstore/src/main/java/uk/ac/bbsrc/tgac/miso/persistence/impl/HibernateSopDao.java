836679f96729cec54d70724db87b0e8fee5b8329
sinry-d <93559434+sinry-d@users.noreply.github.com>
2024-07-22 15:37:09 -0400

    [GLT-4209] Update HibernateSopDao to JPA Criteria (#2938)

--- sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateSopDao.java
+++ sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateSopDao.java
@@ -1,29 +1,36 @@
 package uk.ac.bbsrc.tgac.miso.persistence.impl;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Consumer;
 
-import org.hibernate.Criteria;
-import org.hibernate.criterion.Restrictions;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Root;
+import javax.persistence.metamodel.SingularAttribute;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import uk.ac.bbsrc.tgac.miso.core.data.Run;
+import uk.ac.bbsrc.tgac.miso.core.data.Run_;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.LibraryImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.LibraryImpl_;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.SampleImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.SampleImpl_;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.Sop;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.Sop.SopCategory;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.Sop_;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.UserImpl;
 import uk.ac.bbsrc.tgac.miso.core.util.DateType;
 import uk.ac.bbsrc.tgac.miso.persistence.SopDao;
 
 @Repository
 @Transactional(rollbackFor = Exception.class)
-public class HibernateSopDao extends HibernateSaveDao<Sop> implements HibernatePaginatedDataSource<Sop>, SopDao {
+public class HibernateSopDao extends HibernateSaveDao<Sop> implements JpaCriteriaPaginatedDataSource<Sop, Sop>, SopDao {
 
-  private final static String[] SEARCH_PROPERTIES = new String[] { "alias" };
+  private final static List<SingularAttribute<Sop, String>> SEARCH_PROPERTIES = Arrays.asList(Sop_.alias);
 
   public HibernateSopDao() {
     super(Sop.class);
@@ -31,40 +38,38 @@ public class HibernateSopDao extends HibernateSaveDao<Sop> implements HibernateP
 
   @Override
   public Sop get(SopCategory category, String alias, String version) throws IOException {
-    return (Sop) currentSession().createCriteria(Sop.class)
-        .add(Restrictions.eq("category", category))
-        .add(Restrictions.eq("alias", alias))
-        .add(Restrictions.eq("version", version))
-        .uniqueResult();
+    QueryBuilder<Sop, Sop> builder = getQueryBuilder();
+    builder.addPredicate(builder.getCriteriaBuilder().equal(builder.getRoot().get(Sop_.category), category));
+    builder.addPredicate(builder.getCriteriaBuilder().equal(builder.getRoot().get(Sop_.alias), alias));
+    builder.addPredicate(builder.getCriteriaBuilder().equal(builder.getRoot().get(Sop_.version), version));
+    return builder.getSingleResultOrNull();
   }
 
   @Override
   public List<Sop> listByCategory(SopCategory category) throws IOException {
-    @SuppressWarnings("unchecked")
-    List<Sop> results = currentSession().createCriteria(Sop.class)
-        .add(Restrictions.eq("category", category))
-        .list();
-    return results;
+    QueryBuilder<Sop, Sop> builder = getQueryBuilder();
+    builder.addPredicate(builder.getCriteriaBuilder().equal(builder.getRoot().get(Sop_.category), category));
+    return builder.getResultList();
   }
 
   @Override
   public List<Sop> listByIdList(Collection<Long> ids) throws IOException {
-    return listByIdList("sopId", ids);
+    return listByIdList(Sop_.SOP_ID, ids);
   }
 
   @Override
   public long getUsageBySamples(Sop sop) throws IOException {
-    return getUsageBy(SampleImpl.class, "sop", sop);
+    return getUsageBy(SampleImpl.class, SampleImpl_.sop, sop);
   }
 
   @Override
   public long getUsageByLibraries(Sop sop) throws IOException {
-    return getUsageBy(LibraryImpl.class, "sop", sop);
+    return getUsageBy(LibraryImpl.class, LibraryImpl_.sop, sop);
   }
 
   @Override
   public long getUsageByRuns(Sop sop) throws IOException {
-    return getUsageBy(Run.class, "sop", sop);
+    return getUsageBy(Run.class, Run_.sop, sop);
   }
 
   @Override
@@ -73,43 +78,49 @@ public class HibernateSopDao extends HibernateSaveDao<Sop> implements HibernateP
   }
 
   @Override
-  public String getProjectColumn() {
-    return null;
+  public SingularAttribute<Sop, ?> getIdProperty() {
+    return Sop_.sopId;
   }
 
   @Override
-  public Class<? extends Sop> getRealClass() {
+  public Class<Sop> getEntityClass() {
     return Sop.class;
   }
 
   @Override
-  public String[] getSearchProperties() {
+  public Class<Sop> getResultClass() {
+    return Sop.class;
+  }
+
+  @Override
+  public List<SingularAttribute<Sop, String>> getSearchProperties() {
     return SEARCH_PROPERTIES;
   }
 
   @Override
-  public Iterable<AliasDescriptor> listAliases() {
-    return Collections.emptySet();
-  }
-
-  @Override
-  public String propertyForDate(Criteria criteria, DateType type) {
+  public SingularAttribute<Sop, ?> propertyForDate(DateType type) {
     return null;
   }
 
   @Override
-  public String propertyForSortColumn(String original) {
-    return original;
+  public Path<?> propertyForSortColumn(Root<Sop> root, String original) {
+    switch (original) {
+      case "id":
+        return root.get(Sop_.sopId);
+      default:
+        return root.get(original);
+    }
   }
 
   @Override
-  public String propertyForUser(boolean creator) {
+  public SingularAttribute<Sop, ? extends UserImpl> propertyForUser(boolean creator) {
     return null;
   }
 
   @Override
-  public void restrictPaginationByCategory(Criteria item, SopCategory category, Consumer<String> errorHandler) {
-    item.add(Restrictions.eq("category", category));
+  public void restrictPaginationByCategory(QueryBuilder<?, Sop> builder, SopCategory category,
+      Consumer<String> errorHandler) {
+    builder.addPredicate(builder.getCriteriaBuilder().equal(builder.getRoot().get(Sop_.category), category));
   }
 
 }
