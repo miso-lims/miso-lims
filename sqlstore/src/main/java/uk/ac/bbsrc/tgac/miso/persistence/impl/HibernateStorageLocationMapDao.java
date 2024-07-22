3905df5cf7c8ac7164e1f67c3f3a0f3343982767
sinry-d <93559434+sinry-d@users.noreply.github.com>
2024-07-22 16:03:38 -0400

    [GLT-4213] Update HibernateStorageLocationMapDao to JPA Criteria (#2942)

--- sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateStorageLocationMapDao.java
+++ sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateStorageLocationMapDao.java
@@ -1,74 +1,33 @@
 package uk.ac.bbsrc.tgac.miso.persistence.impl;
 
 import java.io.IOException;
-import java.util.List;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Projections;
-import org.hibernate.criterion.Restrictions;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import uk.ac.bbsrc.tgac.miso.core.data.impl.StorageLocation;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.StorageLocationMap;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.StorageLocationMap_;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.StorageLocation_;
 import uk.ac.bbsrc.tgac.miso.persistence.StorageLocationMapDao;
 
 @Transactional(rollbackFor = Exception.class)
 @Repository
-public class HibernateStorageLocationMapDao implements StorageLocationMapDao {
+public class HibernateStorageLocationMapDao extends HibernateSaveDao<StorageLocationMap>
+    implements StorageLocationMapDao {
 
-  @Autowired
-  private SessionFactory sessionFactory;
-
-  public SessionFactory getSessionFactory() {
-    return sessionFactory;
-  }
-
-  public void setSessionFactory(SessionFactory sessionFactory) {
-    this.sessionFactory = sessionFactory;
-  }
-
-  private Session currentSession() {
-    return getSessionFactory().getCurrentSession();
-  }
-
-  @Override
-  public StorageLocationMap get(long id) throws IOException {
-    return (StorageLocationMap) currentSession().get(StorageLocationMap.class, id);
+  public HibernateStorageLocationMapDao() {
+    super(StorageLocationMap.class);
   }
 
   @Override
   public StorageLocationMap getByFilename(String filename) throws IOException {
-    return (StorageLocationMap) currentSession().createCriteria(StorageLocationMap.class)
-        .add(Restrictions.eq("filename", filename))
-        .uniqueResult();
-  }
-
-  @Override
-  public List<StorageLocationMap> list() throws IOException {
-    @SuppressWarnings("unchecked")
-    List<StorageLocationMap> results = currentSession().createCriteria(StorageLocationMap.class).list();
-    return results;
-  }
-
-  @Override
-  public long create(StorageLocationMap map) throws IOException {
-    return (long) currentSession().save(map);
-  }
-
-  @Override
-  public long update(StorageLocationMap map) throws IOException {
-    currentSession().update(map);
-    return map.getId();
+    return getBy(StorageLocationMap_.filename, filename);
   }
 
   @Override
   public long getUsage(StorageLocationMap map) throws IOException {
-    return (long) currentSession().createCriteria(StorageLocation.class)
-        .add(Restrictions.eq("map", map))
-        .setProjection(Projections.rowCount()).uniqueResult();
+    return getUsageBy(StorageLocation.class, StorageLocation_.map, map);
   }
 
 }
