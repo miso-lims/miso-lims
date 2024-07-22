467ec8c14e44d87916e9bfecc723b93d9ed5ed6d
sinry-d <93559434+sinry-d@users.noreply.github.com>
2024-07-22 16:03:06 -0400

    Update HibernateStainDao to JPA Criteria (#2940)

--- sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateStainDao.java
+++ sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateStainDao.java
@@ -4,16 +4,13 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Projections;
-import org.hibernate.criterion.Restrictions;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import uk.ac.bbsrc.tgac.miso.core.data.Stain;
+import uk.ac.bbsrc.tgac.miso.core.data.Stain_;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.SampleSlideImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.SampleSlideImpl_;
 import uk.ac.bbsrc.tgac.miso.persistence.StainDao;
 
 @Transactional(rollbackFor = Exception.class)
@@ -26,21 +23,17 @@ public class HibernateStainDao extends HibernateSaveDao<Stain> implements StainD
 
   @Override
   public Stain getByName(String name) throws IOException {
-    return (Stain) currentSession().createCriteria(Stain.class)
-        .add(Restrictions.eq("name", name))
-        .uniqueResult();
+    return getBy(Stain_.name, name);
   }
 
   @Override
   public long getUsage(Stain stain) throws IOException {
-    return (long) currentSession().createCriteria(SampleSlideImpl.class)
-        .add(Restrictions.eq("stain", stain))
-        .setProjection(Projections.rowCount()).uniqueResult();
+    return getUsageBy(SampleSlideImpl.class, SampleSlideImpl_.stain, stain);
   }
 
   @Override
   public List<Stain> listByIdList(Collection<Long> ids) throws IOException {
-    return listByIdList("stainId", ids);
+    return listByIdList(Stain_.STAIN_ID, ids);
   }
 
 }
