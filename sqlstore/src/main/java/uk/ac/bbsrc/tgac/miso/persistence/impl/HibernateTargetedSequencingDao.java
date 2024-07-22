c9c1b23727dc4bffcd2fb32561bd49d6eca4ce80
sinry-d <93559434+sinry-d@users.noreply.github.com>
2024-07-22 16:31:19 -0400

    [GLT-4216] Update HibernateTargetedSequencingDao to JPA Criteria (#2945)

--- sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateTargetedSequencingDao.java
+++ sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateTargetedSequencingDao.java
@@ -1,49 +1,50 @@
 package uk.ac.bbsrc.tgac.miso.persistence.impl;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
-import org.hibernate.Criteria;
-import org.hibernate.Session;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Root;
+import javax.persistence.metamodel.SingularAttribute;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import uk.ac.bbsrc.tgac.miso.core.data.impl.LibraryAliquot;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.LibraryAliquot_;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.TargetedSequencing;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.TargetedSequencing_;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.UserImpl;
 import uk.ac.bbsrc.tgac.miso.core.util.DateType;
 import uk.ac.bbsrc.tgac.miso.persistence.TargetedSequencingStore;
 
 @Repository
 @Transactional(rollbackFor = Exception.class)
 public class HibernateTargetedSequencingDao extends HibernateSaveDao<TargetedSequencing>
-    implements TargetedSequencingStore, HibernatePaginatedDataSource<TargetedSequencing> {
+    implements TargetedSequencingStore, JpaCriteriaPaginatedDataSource<TargetedSequencing, TargetedSequencing> {
 
-  private static final String[] SEARCH_PROPERTIES = new String[] { "alias" };
+  private static final List<SingularAttribute<TargetedSequencing, String>> SEARCH_PROPERTIES =
+      Arrays.asList(TargetedSequencing_.alias);
 
   public HibernateTargetedSequencingDao() {
     super(TargetedSequencing.class);
   }
 
   @Override
-  public Session currentSession() {
-    return super.currentSession();
-  }
-
-  @Override
   public TargetedSequencing getByAlias(String alias) throws IOException {
-    return getBy("alias", alias);
+    return getBy(TargetedSequencing_.alias, alias);
   }
 
   @Override
   public List<TargetedSequencing> listByIdList(Collection<Long> idList) throws IOException {
-    return listByIdList("targetedSequencingId", idList);
+    return listByIdList(TargetedSequencing_.TARGETED_SEQUENCING_ID, idList);
   }
 
   @Override
   public long getUsage(TargetedSequencing targetedSequencing) throws IOException {
-    return getUsageBy(LibraryAliquot.class, "targetedSequencing", targetedSequencing);
+    return getUsageBy(LibraryAliquot.class, LibraryAliquot_.targetedSequencing, targetedSequencing);
   }
 
   @Override
@@ -52,37 +53,42 @@ public class HibernateTargetedSequencingDao extends HibernateSaveDao<TargetedSeq
   }
 
   @Override
-  public String getProjectColumn() {
-    return null;
+  public SingularAttribute<TargetedSequencing, ?> getIdProperty() {
+    return TargetedSequencing_.targetedSequencingId;
   }
 
   @Override
-  public Class<? extends TargetedSequencing> getRealClass() {
+  public Class<TargetedSequencing> getEntityClass() {
     return TargetedSequencing.class;
   }
 
   @Override
-  public String[] getSearchProperties() {
+  public Class<TargetedSequencing> getResultClass() {
+    return TargetedSequencing.class;
+  }
+
+  @Override
+  public List<SingularAttribute<TargetedSequencing, String>> getSearchProperties() {
     return SEARCH_PROPERTIES;
   }
 
   @Override
-  public Iterable<AliasDescriptor> listAliases() {
-    return Collections.emptyList();
-  }
-
-  @Override
-  public String propertyForDate(Criteria criteria, DateType type) {
+  public SingularAttribute<TargetedSequencing, ?> propertyForDate(DateType type) {
     return null;
   }
 
   @Override
-  public String propertyForSortColumn(String original) {
-    return original;
+  public Path<?> propertyForSortColumn(Root<TargetedSequencing> root, String original) {
+    switch (original) {
+      case "id":
+        return root.get(TargetedSequencing_.targetedSequencingId);
+      default:
+        return root.get(original);
+    }
   }
 
   @Override
-  public String propertyForUser(boolean creator) {
+  public SingularAttribute<TargetedSequencing, ? extends UserImpl> propertyForUser(boolean creator) {
     return null;
   }
 }
