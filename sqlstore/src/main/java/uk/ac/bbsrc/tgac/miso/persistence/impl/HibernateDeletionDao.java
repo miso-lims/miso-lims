b4ec837a7296be3c3eb6e30c4d37d085a53353a2
sinry-d <93559434+sinry-d@users.noreply.github.com>
2024-07-23 14:02:17 -0400

    [GLT-4242] Update HibernateDeletionDao to JPA Criteria (#2952)

--- sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateDeletionDao.java
+++ sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateDeletionDao.java
@@ -1,9 +1,13 @@
 package uk.ac.bbsrc.tgac.miso.persistence.impl;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
-import org.hibernate.Criteria;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Root;
+import javax.persistence.metamodel.SingularAttribute;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,14 +18,18 @@ import com.eaglegenomics.simlims.core.User;
 
 import uk.ac.bbsrc.tgac.miso.core.data.Deletable;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.Deletion;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.Deletion_;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.UserImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.UserImpl_;
 import uk.ac.bbsrc.tgac.miso.core.store.DeletionStore;
 import uk.ac.bbsrc.tgac.miso.core.util.DateType;
 
 @Transactional(rollbackFor = Exception.class)
 @Repository
-public class HibernateDeletionDao implements DeletionStore, HibernatePaginatedDataSource<Deletion> {
+public class HibernateDeletionDao implements DeletionStore, JpaCriteriaPaginatedDataSource<Deletion, Deletion> {
 
-  private static final String[] SEARCH_PROPERTIES = new String[] { "description" };
+  private static final List<SingularAttribute<Deletion, String>> SEARCH_PROPERTIES =
+      Arrays.asList(Deletion_.description);
 
   @Autowired
   private SessionFactory sessionFactory;
@@ -53,37 +61,42 @@ public class HibernateDeletionDao implements DeletionStore, HibernatePaginatedDa
   }
 
   @Override
-  public String propertyForUser(boolean creator) {
-    return creator ? "user" : null;
+  public SingularAttribute<Deletion, ? extends UserImpl> propertyForUser(boolean creator) {
+    return creator ? Deletion_.user : null;
   }
 
   @Override
-  public String propertyForDate(Criteria criteria, DateType type) {
-    return "changeTime";
+  public SingularAttribute<Deletion, ?> propertyForDate(DateType type) {
+    return Deletion_.changeTime;
   }
 
   @Override
-  public String[] getSearchProperties() {
+  public List<SingularAttribute<Deletion, String>> getSearchProperties() {
     return SEARCH_PROPERTIES;
   }
 
   @Override
-  public String getProjectColumn() {
-    return null;
+  public Path<?> propertyForSortColumn(Root<Deletion> root, String original) {
+    switch (original) {
+      case "userName":
+        return root.get(Deletion_.user).get(UserImpl_.fullName);
+      default:
+        return root.get(original);
+    }
   }
 
   @Override
-  public String propertyForSortColumn(String original) {
-    return original;
+  public SingularAttribute<Deletion, ?> getIdProperty() {
+    return Deletion_.id;
   }
 
   @Override
-  public Iterable<AliasDescriptor> listAliases() {
-    return Collections.emptyList();
+  public Class<Deletion> getEntityClass() {
+    return Deletion.class;
   }
 
   @Override
-  public Class<? extends Deletion> getRealClass() {
+  public Class<Deletion> getResultClass() {
     return Deletion.class;
   }
 
