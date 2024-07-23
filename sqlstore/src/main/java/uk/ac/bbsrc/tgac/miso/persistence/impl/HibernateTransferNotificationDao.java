068cfaa513e0704e59eedc48003981d521cdf0bd
sinry-d <93559434+sinry-d@users.noreply.github.com>
2024-07-23 08:51:22 -0400

    [GLT-4220] Update HibernateTransferNotificationDao to JPA Criteria (#2946)

--- sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateTransferNotificationDao.java
+++ sqlstore/src/main/java/uk/ac/bbsrc/tgac/miso/persistence/impl/HibernateTransferNotificationDao.java
@@ -6,18 +6,22 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
-import org.hibernate.criterion.Order;
-import org.hibernate.criterion.Restrictions;
+import javax.persistence.criteria.Join;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import uk.ac.bbsrc.tgac.miso.core.data.impl.UserImpl;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.UserImpl_;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.transfer.Transfer;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.transfer.TransferNotification;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.transfer.TransferNotification_;
 import uk.ac.bbsrc.tgac.miso.persistence.TransferNotificationStore;
 
 @Repository
 @Transactional(rollbackFor = Exception.class)
-public class HibernateTransferNotificationDao extends HibernateSaveDao<TransferNotification> implements TransferNotificationStore {
+public class HibernateTransferNotificationDao extends HibernateSaveDao<TransferNotification>
+    implements TransferNotificationStore {
 
   private static final long ONE_MINUTE = 60000;
 
@@ -27,11 +31,11 @@ public class HibernateTransferNotificationDao extends HibernateSaveDao<TransferN
 
   @Override
   public List<TransferNotification> listByTransfer(Transfer transfer) throws IOException {
-    @SuppressWarnings("unchecked")
-    List<TransferNotification> results = currentSession().createCriteria(TransferNotification.class)
-        .add(Restrictions.eq("transfer", transfer))
-        .list();
-    return results;
+    QueryBuilder<TransferNotification, TransferNotification> builder =
+        new QueryBuilder<>(currentSession(), TransferNotification.class, TransferNotification.class);
+    builder.addPredicate(
+        builder.getCriteriaBuilder().equal(builder.getRoot().get(TransferNotification_.transfer), transfer));
+    return builder.getResultList();
   }
 
   @Override
@@ -41,14 +45,13 @@ public class HibernateTransferNotificationDao extends HibernateSaveDao<TransferN
     }
     Date time = new Date(Calendar.getInstance().getTimeInMillis() - holdMinutes * ONE_MINUTE);
 
-    @SuppressWarnings("unchecked")
-    List<TransferNotification> results = currentSession().createCriteria(TransferNotification.class)
-        .add(Restrictions.isNull("sentTime"))
-        .add(Restrictions.le("created", time))
-        .addOrder(Order.asc("created"))
-        .setMaxResults(limit)
-        .list();
-    return results;
+    QueryBuilder<TransferNotification, TransferNotification> builder =
+        new QueryBuilder<>(currentSession(), TransferNotification.class, TransferNotification.class);
+    builder.addPredicate(builder.getCriteriaBuilder().isNull(builder.getRoot().get(TransferNotification_.sentTime)));
+    builder.addPredicate(
+        builder.getCriteriaBuilder().lessThanOrEqualTo(builder.getRoot().get(TransferNotification_.created), time));
+    builder.addSort(builder.getRoot().get(TransferNotification_.created), true);
+    return builder.getResultList(limit, 0);
   }
 
   @Override
@@ -56,16 +59,17 @@ public class HibernateTransferNotificationDao extends HibernateSaveDao<TransferN
     if (limit < 1) {
       return Collections.emptyList();
     }
-    @SuppressWarnings("unchecked")
-    List<TransferNotification> results = currentSession().createCriteria(TransferNotification.class)
-        .add(Restrictions.eq("sendSuccess", false))
-        .add(Restrictions.isNull("failureSentTime"))
-        .createAlias("creator", "creator")
-        .add(Restrictions.isNotNull("creator.email"))
-        .addOrder(Order.asc("sentTime"))
-        .setMaxResults(limit)
-        .list();
-    return results;
+
+    QueryBuilder<TransferNotification, TransferNotification> builder =
+        new QueryBuilder<>(currentSession(), TransferNotification.class, TransferNotification.class);
+    builder.addPredicate(
+        builder.getCriteriaBuilder().equal(builder.getRoot().get(TransferNotification_.sendSuccess), false));
+    builder.addPredicate(
+        builder.getCriteriaBuilder().isNull(builder.getRoot().get(TransferNotification_.failureSentTime)));
+    Join<TransferNotification, UserImpl> creator = builder.getJoin(builder.getRoot(), TransferNotification_.creator);
+    builder.addPredicate(builder.getCriteriaBuilder().isNotNull(creator.get(UserImpl_.email)));
+    builder.addSort(builder.getRoot().get(TransferNotification_.sentTime), true);
+    return builder.getResultList(limit, 0);
   }
 
   @Override
