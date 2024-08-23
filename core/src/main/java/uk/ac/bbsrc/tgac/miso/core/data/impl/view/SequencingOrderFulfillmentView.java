df7a4652b2af50e47d4b1c0763a435d29ca9bfdf
Sinry Dong <sinryd@gmail.com>
2024-08-23 10:43:36 -0400

    Fix tests

--- core/src/main/java/uk/ac/bbsrc/tgac/miso/core/data/impl/view/SequencingOrderFulfillmentView.java
+++ core/src/main/java/uk/ac/bbsrc/tgac/miso/core/data/impl/view/SequencingOrderFulfillmentView.java
@@ -1,54 +1,10 @@
 package uk.ac.bbsrc.tgac.miso.core.data.impl.view;
 
-import java.io.Serializable;
-import java.util.Objects;
-
-import org.hibernate.annotations.Immutable;
-
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
-import uk.ac.bbsrc.tgac.miso.core.util.LimsUtils;
 
 @Entity
-@Immutable
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-public class SequencingOrderFulfillmentView implements Serializable {
+public class SequencingOrderFulfillmentView extends AbstractSequencingOrderFulfillmentView {
 
   private static final long serialVersionUID = 1L;
-
-  @Id
-  private String orderSummaryId;
-
-  private int fulfilled;
-
-  public String getId() {
-    return orderSummaryId;
-  }
-
-  public void setId(String id) {
-    this.orderSummaryId = id;
-  }
-
-  public int getFulfilled() {
-    return fulfilled;
-  }
-
-  public void setFulfilled(int fulfilled) {
-    this.fulfilled = fulfilled;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(orderSummaryId, fulfilled);
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    return LimsUtils.equals(this, obj,
-        SequencingOrderFulfillmentView::getId,
-        SequencingOrderFulfillmentView::getFulfilled);
-  }
 
 }
--- core/src/main/java/uk/ac/bbsrc/tgac/miso/core/data/impl/view/SequencingOrderNoContainerModelFulfillmentView.java
+++ core/src/main/java/uk/ac/bbsrc/tgac/miso/core/data/impl/view/SequencingOrderNoContainerModelFulfillmentView.java
@@ -2,8 +2,8 @@ package uk.ac.bbsrc.tgac.miso.core.data.impl.view;
 
 import jakarta.persistence.Entity;
 
-@Entity(name = "SequencingOrderNoContainerModelFulfillmentView")
-public class SequencingOrderNoContainerModelFulfillmentView extends SequencingOrderFulfillmentView {
+@Entity
+public class SequencingOrderNoContainerModelFulfillmentView extends AbstractSequencingOrderFulfillmentView {
 
   private static final long serialVersionUID = 1L;
 
