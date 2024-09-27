58df71a06464ac855aad4518b2b1a64a30f2641f
Dillan Cooke <dillan.cooke@oicr.on.ca>
2024-09-27 14:13:58 -0400

    jakarta namespace fixups after rebase

--- miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/controller/view/MisoContextController.java
+++ miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/controller/view/MisoContextController.java
@@ -1,6 +1,6 @@
 package uk.ac.bbsrc.tgac.miso.webapp.controller.view;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
