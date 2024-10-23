cb1216d7d46cd45adf9e1a6ac6df7e0a34ef8d8d
Christopher-Ho <christopherho190@gmail.com>
2024-10-23 14:19:30 -0400

    Added stubbing for tests

--- integration-tools/src/test/java/uk/ac/bbsrc/tgac/miso/integration/test/dp5mirage/DP5MirageScanTests.java
+++ integration-tools/src/test/java/uk/ac/bbsrc/tgac/miso/integration/test/dp5mirage/DP5MirageScanTests.java
@@ -1,7 +1,5 @@
 package uk.ac.bbsrc.tgac.miso.integration.test.dp5mirage;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.ArrayList;
 import java.util.List;
 import uk.ac.bbsrc.tgac.miso.integration.dp5mirage.DP5MirageScan;
@@ -10,16 +8,6 @@ import uk.ac.bbsrc.tgac.miso.integration.test.BoxScanTests;
 
 public class DP5MirageScanTests extends BoxScanTests<DP5MirageScan> {
 
-  /**
-   * Returns a new BoxScan, representing a scan of a full box of tubes. Implementations must return
-   * a BoxScan implementation object containing 2 rows, 2 columns, and the following barcodes:
-   * <OL>
-   * <LI>A01: 11111</LI>
-   * <LI>B01: 22222</LI>
-   * <LI>A02: 33333</LI>
-   * <LI>B02: 44444</LI>
-   * </OL>
-   */
   @Override
   protected DP5MirageScan getFullScan() {
     List<DP5MirageScanPosition> records = new ArrayList<>();
@@ -31,14 +19,8 @@ public class DP5MirageScanTests extends BoxScanTests<DP5MirageScan> {
     return new DP5MirageScan(records);
   }
 
-  /**
-   * Returns a new BoxScan, representing a scan of an empty box of tubes. Implementations must
-   * return a BoxScan implementation object containing 2 rows, 2 columns, and no tubes.
-   */
   @Override
   protected DP5MirageScan getEmptyScan() {
-    ObjectMapper mapper = new ObjectMapper();
-    JsonNode emptyWrappedScan = null;
     List<DP5MirageScanPosition> records = new ArrayList<>();
     records.add(new DP5MirageScanPosition("null", "EMPTY", 1, 1, 1, 1));
     records.add(new DP5MirageScanPosition("null", "EMPTY", 1, 2, 1, 2));
@@ -48,17 +30,6 @@ public class DP5MirageScanTests extends BoxScanTests<DP5MirageScan> {
     return new DP5MirageScan(records);
   }
 
-  /**
-   * Returns a new BoxScan, representing a scan of a full box of tubes with failed reads.
-   * Implementations must return a BoxScan implementation object containing 2 rows, 2 columns, and
-   * the following barcodes:
-   * <OL>
-   * <LI>A01: 11111</LI>
-   * <LI>B01: (failed read)</LI>
-   * <LI>A02: (failed read)</LI>
-   * <LI>B02: 44444</LI>
-   * </OL>
-   */
   @Override
   protected DP5MirageScan getErredScan() {
     List<DP5MirageScanPosition> records = new ArrayList<>();
