d679e71b7354658f8ebd5da051264035a867639b
Dillan Cooke <dillan.cooke@oicr.on.ca>
2025-04-14 13:50:38 -0400

    [GLT-4380] updated OICR naming scheme to support Ultima libraries

--- core/src/main/java/uk/ac/bbsrc/tgac/miso/core/service/naming/generation/OicrLibraryAliquotAliasGenerator.java
+++ core/src/main/java/uk/ac/bbsrc/tgac/miso/core/service/naming/generation/OicrLibraryAliquotAliasGenerator.java
@@ -1,5 +1,6 @@
 package uk.ac.bbsrc.tgac.miso.core.service.naming.generation;
 
+import java.io.IOException;
 import java.time.LocalDate;
 
 import uk.ac.bbsrc.tgac.miso.core.data.DetailedLibrary;
@@ -9,10 +10,26 @@ import uk.ac.bbsrc.tgac.miso.core.data.impl.DetailedLibraryAliquot;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.LibraryAliquot;
 import uk.ac.bbsrc.tgac.miso.core.data.type.LibraryType;
 import uk.ac.bbsrc.tgac.miso.core.data.type.PlatformType;
+import uk.ac.bbsrc.tgac.miso.core.exception.MisoNamingException;
 import uk.ac.bbsrc.tgac.miso.core.util.LimsUtils;
 
 public class OicrLibraryAliquotAliasGenerator
     extends OicrBaseLibraryAliasGenerator<LibraryAliquot, DetailedLibraryAliquot> {
+
+  @Override
+  public String generate(LibraryAliquot item) throws MisoNamingException, IOException {
+    if (!isDetailed(item)) {
+      throw new IllegalArgumentException("Can only generate an alias for detailed samples");
+    }
+    DetailedLibraryAliquot detailedItem = (DetailedLibraryAliquot) item;
+    if (getPlatformType(detailedItem) != PlatformType.ULTIMA) {
+      super.generate(item);
+    }
+    // Ultima library aliquot e.g. PROJ_0001_Pa_P_WG_1-1
+    String partial = detailedItem.getLibrary().getAlias() + "-";
+    int siblingNumber = getSiblingNumberGenerator().getFirstAvailableSiblingNumber(LibraryAliquot.class, partial);
+    return partial + siblingNumber;
+  }
 
   @Override
   protected boolean isDetailed(LibraryAliquot item) {
