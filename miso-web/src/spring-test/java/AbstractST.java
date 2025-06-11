ae173aa3ec52ca288b9d5a60f13ce2a6804fe25c
PrabhjotChopra <pchopra@oicr.on.ca>
2025-06-11 11:10:07 -0400

    tentative changes

--- miso-web/src/spring-test/java/AbstractST.java
+++ miso-web/src/spring-test/java/AbstractST.java
@@ -1,28 +1,45 @@
 package uk.ac.bbsrc.tgac.miso.webapp.springtest;
 
+<<<<<<< HEAD
 import java.io.File;
 
 import javax.sql.DataSource;
 
 import org.junit.Before;
+=======
+import javax.sql.DataSource;
+
+>>>>>>> e36728ea0 (tentative changes)
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
+<<<<<<< HEAD
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+=======
+>>>>>>> e36728ea0 (tentative changes)
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 
+<<<<<<< HEAD
+=======
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+
+>>>>>>> e36728ea0 (tentative changes)
 @RunWith(SpringRunner.class)
 @ContextConfiguration("/it-context.xml")
 @PropertySource("/tomcat-config/miso.it.properties")
 public abstract class AbstractST {
+<<<<<<< HEAD
   private static final Logger log = LoggerFactory.getLogger(AbstractST.class);
 
   private static final String SCRIPT_DIR = System.getProperty("basedir") + "/src/it/resources/db/migration/";
@@ -30,6 +47,18 @@ public abstract class AbstractST {
   private static final String CLEAR_DATA_SCRIPT = "clear_test_data.sql";
   private static final String DETAILED_SCRIPT = "integration_test_data.sql";
 
+=======
+  private static final Logger log = LoggerFactory.getLogger(AbstractCT.class);
+
+  private static final String SCRIPT_DIR = System.getProperty("basedir") + "/src/it/resources/db/migration/";
+
+
+  private static final String CLEAR_DATA_SCRIPT = "clear_test_data.sql";
+  private static final String PLAIN_SCRIPT = "plainSample_integration_test_data.sql";
+  private static final String DETAILED_SCRIPT = "integration_test_data.sql";
+
+
+>>>>>>> e36728ea0 (tentative changes)
   private static Boolean constantsComplete = false;
 
   @PersistenceContext
@@ -38,16 +67,32 @@ public abstract class AbstractST {
   @Autowired
   private DataSource dataSource;
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> e36728ea0 (tentative changes)
   @Before
   public final void setupAbstractTest() {
 
     // reset test data for each test
     Resource clearData = new FileSystemResource(getScript(CLEAR_DATA_SCRIPT));
+<<<<<<< HEAD
     Resource testData = new FileSystemResource(getScript(DETAILED_SCRIPT));
+=======
+    Resource testData = new FileSystemResource(getScript(isDetailedSampleMode() ? DETAILED_SCRIPT : PLAIN_SCRIPT));
+>>>>>>> e36728ea0 (tentative changes)
     ResourceDatabasePopulator populator = new ResourceDatabasePopulator(clearData, testData);
     populator.execute(dataSource);
   }
 
+<<<<<<< HEAD
+=======
+
+  protected boolean isDetailedSampleMode() {
+    return true;
+  }
+
+>>>>>>> e36728ea0 (tentative changes)
   private File getScript(String filename) {
     File script = new File(SCRIPT_DIR + filename);
     if (!script.exists()) {
@@ -56,4 +101,8 @@ public abstract class AbstractST {
     return script;
   }
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> e36728ea0 (tentative changes)
 }
