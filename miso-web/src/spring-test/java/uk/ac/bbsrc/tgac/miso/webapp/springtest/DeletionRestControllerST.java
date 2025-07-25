95a5694f162a401d84359c6d8d8f371b7ed17530
PrabhjotChopra <pchopra@oicr.on.ca>
2025-07-25 10:54:08 -0400

    added deletion rest test

---
+++ miso-web/src/spring-test/java/uk/ac/bbsrc/tgac/miso/webapp/springtest/DeletionRestControllerST.java
@@ -0,0 +1,55 @@
+package uk.ac.bbsrc.tgac.miso.webapp.springtest;
+
+import org.junit.Test;
+
+import org.springframework.web.servlet.*;
+
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+import javax.ws.rs.core.MediaType;
+
+import org.checkerframework.checker.units.qual.Temperature;
+import org.junit.Before;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import org.springframework.test.web.servlet.ResultActions;
+import com.jayway.jsonpath.JsonPath;
+
+import static org.hamcrest.Matchers.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import org.springframework.test.web.servlet.MvcResult;
+import uk.ac.bbsrc.tgac.miso.dto.Dtos;
+
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.View;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.security.test.context.support.WithMockUser;
+import uk.ac.bbsrc.tgac.miso.core.data.type.StatusType;
+import static org.junit.Assert.*;
+import java.util.Collections;
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+import org.springframework.test.web.servlet.MockMvc;
+import java.util.Date;
+
+
+public class DeletionRestControllerST extends AbstractST {
+
+  private static final String CONTROLLER_BASE = "/rest/deletions";
+
+  @Test
+  public void testDatatable() throws Exception {
+    ResultActions result = testDtRequest(CONTROLLER_BASE + "/dt", Arrays.asList(1));
+    result.andExpect(jsonPath("$.aaData[0].description").value("first sample"))
+        .andExpect(jsonPath("$.aaData[0].targetType").value("Sample"))
+        .andExpect(jsonPath("$.aaData[0].targetId").value(1));
+  }
+}
