36a46850a9de5b6e0674b03b7a045880bc94a382
PrabhjotChopra <pchopra@oicr.on.ca>
2025-07-25 11:08:35 -0400

    added deliverable rest tests

---
+++ miso-web/src/spring-test/java/uk/ac/bbsrc/tgac/miso/webapp/springtest/DeliverableRestControllerST.java
@@ -0,0 +1,123 @@
+package uk.ac.bbsrc.tgac.miso.webapp.springtest;
+
+import org.junit.Test;
+
+import org.springframework.web.servlet.*;
+
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+import javax.ws.rs.core.MediaType;
+
+import org.checkerframework.checker.units.qual.Temperature;
+import org.junit.Before;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import org.springframework.test.web.servlet.ResultActions;
+import com.jayway.jsonpath.JsonPath;
+
+import static org.hamcrest.Matchers.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import org.springframework.test.web.servlet.MvcResult;
+import uk.ac.bbsrc.tgac.miso.dto.Dtos;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.Deliverable;
+import uk.ac.bbsrc.tgac.miso.dto.DeliverableDto;
+
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.View;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.security.test.context.support.WithMockUser;
+import uk.ac.bbsrc.tgac.miso.core.data.type.StatusType;
+import static org.junit.Assert.*;
+import java.util.Collections;
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+import org.springframework.test.web.servlet.MockMvc;
+import java.util.Date;
+
+
+public class DeliverableRestControllerST extends AbstractST {
+
+  private static final String CONTROLLER_BASE = "/rest/deliverables";
+  private static final Class<Deliverable> controllerClass = Deliverable.class;
+
+  private List<DeliverableDto> makeCreateDtos() {
+    DeliverableDto del1 = new DeliverableDto();
+    del1.setName("del1");
+    del1.setCategoryId(1L);
+
+    DeliverableDto del2 = new DeliverableDto();
+    del2.setName("del2");
+    del2.setCategoryId(2L);
+
+    List<DeliverableDto> dtos = new ArrayList<DeliverableDto>();
+    dtos.add(del1);
+    dtos.add(del2);
+    return dtos;
+  }
+
+  @Test
+  @WithMockUser(username = "admin", password = "admin", roles = {"INTERNAL", "ADMIN"})
+  public void testBulkCreateAsync() throws Exception {
+    List<Deliverable> deliverables = baseTestBulkCreateAsync(CONTROLLER_BASE, controllerClass, makeCreateDtos());
+    assertEquals("del1", deliverables.get(0).getName());
+    assertEquals("del2", deliverables.get(1).getName());
+  }
+
+  @Test
+  public void testBulkCreateFail() throws Exception {
+    testBulkCreateAsyncUnauthorized(CONTROLLER_BASE, controllerClass, makeCreateDtos());
+  }
+
+
+  @Test
+  @WithMockUser(username = "admin", password = "admin", roles = {"INTERNAL", "ADMIN"})
+  public void testBulkUpdateAsync() throws Exception {
+    Deliverable del1 = currentSession().get(controllerClass, 1);
+    Deliverable del2 = currentSession().get(controllerClass, 2);
+    del1.setName("deliver 1");
+    del2.setName("deliver 2");
+
+    List<DeliverableDto> dtos = new ArrayList<DeliverableDto>();
+    dtos.add(Dtos.asDto(del1));
+    dtos.add(Dtos.asDto(del2));
+
+    List<Deliverable> deliverables =
+        (List<Deliverable>) baseTestBulkUpdateAsync(CONTROLLER_BASE, controllerClass, dtos,
+            Arrays.asList(1, 2));
+
+    assertEquals("deliver 1", deliverables.get(0).getName());
+    assertEquals("deliver 2", deliverables.get(1).getName());
+  }
+
+  @Test
+  public void testBulkUpdateFail() throws Exception {
+    Deliverable del1 = currentSession().get(controllerClass, 1);
+    Deliverable del2 = currentSession().get(controllerClass, 2);
+    del1.setName("deliver 1");
+    del2.setName("deliver 2");
+
+    List<DeliverableDto> dtos = new ArrayList<DeliverableDto>();
+    dtos.add(Dtos.asDto(del1));
+    dtos.add(Dtos.asDto(del2));
+    testBulkUpdateAsyncUnauthorized(CONTROLLER_BASE, controllerClass, dtos);
+  }
+
+  @Test
+  @WithMockUser(username = "admin", password = "admin", roles = {"INTERNAL", "ADMIN"})
+  public void testDelete() throws Exception {
+    testBulkDelete(controllerClass, 3, CONTROLLER_BASE);
+  }
+
+  @Test
+  public void testDeleteFail() throws Exception {
+    testDeleteUnauthorized(controllerClass, 3, CONTROLLER_BASE);
+  }
+}
