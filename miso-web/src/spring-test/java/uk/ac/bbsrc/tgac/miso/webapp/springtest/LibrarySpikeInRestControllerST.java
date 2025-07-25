65de250817141e432ef1451850f5a98b9aaf6975
PrabhjotChopra <pchopra@oicr.on.ca>
2025-07-25 12:40:40 -0400

    added library spikein rest tests

---
+++ miso-web/src/spring-test/java/uk/ac/bbsrc/tgac/miso/webapp/springtest/LibrarySpikeInRestControllerST.java
@@ -0,0 +1,134 @@
+package uk.ac.bbsrc.tgac.miso.webapp.springtest;
+
+import org.junit.Test;
+
+import org.springframework.web.servlet.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+import javax.ws.rs.core.MediaType;
+
+import org.checkerframework.checker.units.qual.Temperature;
+import org.junit.Before;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import org.springframework.test.web.servlet.ResultActions;
+import com.jayway.jsonpath.JsonPath;
+
+import jakarta.transaction.Transactional;
+
+import static org.hamcrest.Matchers.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import org.springframework.test.web.servlet.MvcResult;
+import uk.ac.bbsrc.tgac.miso.dto.Dtos;
+import uk.ac.bbsrc.tgac.miso.core.data.LibrarySpikeIn;
+import uk.ac.bbsrc.tgac.miso.dto.LibrarySpikeInDto;
+
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.View;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.security.test.context.support.WithMockUser;
+import uk.ac.bbsrc.tgac.miso.core.data.type.StatusType;
+import java.util.Collections;
+
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+import org.springframework.test.web.servlet.MockMvc;
+import java.util.Date;
+
+
+public class LibrarySpikeInRestControllerST extends AbstractST {
+
+  private static final String CONTROLLER_BASE = "/rest/libraryspikeins";
+  private static final Class<LibrarySpikeIn> controllerClass = LibrarySpikeIn.class;
+
+  private List<LibrarySpikeInDto> makeCreateDtos() {
+
+    List<LibrarySpikeInDto> dtos = new ArrayList<LibrarySpikeInDto>();
+    LibrarySpikeInDto one = new LibrarySpikeInDto();
+    one.setAlias("one");
+
+    LibrarySpikeInDto two = new LibrarySpikeInDto();
+    two.setAlias("two");
+
+    dtos.add(one);
+    dtos.add(two);
+
+    return dtos;
+  }
+
+  @Test
+  @WithMockUser(username = "admin", password = "admin", roles = {"INTERNAL", "ADMIN"})
+  public void testBulkCreateAsync() throws Exception {
+    List<LibrarySpikeIn> librarySpikeIns =
+        baseTestBulkCreateAsync(CONTROLLER_BASE, controllerClass, makeCreateDtos());
+    assertEquals(librarySpikeIns.get(0).getAlias(), "one");
+    assertEquals(librarySpikeIns.get(1).getAlias(), "two");
+  }
+
+  @Test
+  public void testBulkCreateFail() throws Exception {
+    // LibrarySpikeIn creation is for admin only, so this test is expecting failure due to
+    // insufficent permission
+    testBulkCreateAsyncUnauthorized(CONTROLLER_BASE, controllerClass, makeCreateDtos());
+  }
+
+  @Test
+  @WithMockUser(username = "admin", password = "admin", roles = {"INTERNAL", "ADMIN"})
+  public void testBulkUpdateAsync() throws Exception {
+    // only admin can update these
+    LibrarySpikeInDto one = Dtos.asDto(currentSession().get(LibrarySpikeIn.class, 1));
+    LibrarySpikeInDto three = Dtos.asDto(currentSession().get(LibrarySpikeIn.class, 3));
+    one.setAlias("one");
+    three.setAlias("three");
+
+    List<LibrarySpikeInDto> dtos = new ArrayList<LibrarySpikeInDto>();
+    dtos.add(one);
+    dtos.add(three);
+
+
+    List<LibrarySpikeIn> librarySpikeIns =
+        (List<LibrarySpikeIn>) baseTestBulkUpdateAsync(CONTROLLER_BASE, controllerClass, dtos,
+            Arrays.asList(1, 3));
+    assertEquals("one", librarySpikeIns.get(0).getAlias());
+    assertEquals("three", librarySpikeIns.get(1).getAlias());
+  }
+
+  @Test
+  public void testBulkUpdateAsyncFail() throws Exception {
+    // only admin can update these
+    LibrarySpikeInDto one = Dtos.asDto(currentSession().get(LibrarySpikeIn.class, 1));
+    LibrarySpikeInDto three = Dtos.asDto(currentSession().get(LibrarySpikeIn.class, 3));
+    one.setAlias("one");
+    three.setAlias("three");
+
+    List<LibrarySpikeInDto> dtos = new ArrayList<LibrarySpikeInDto>();
+    dtos.add(one);
+    dtos.add(three);
+
+    testBulkUpdateAsyncUnauthorized(CONTROLLER_BASE, controllerClass, dtos);
+  }
+
+  @Test
+  @WithMockUser(username = "admin", password = "admin", roles = {"INTERNAL", "ADMIN"})
+  public void testDeleteLibrarySpikeIn() throws Exception {
+    testBulkDelete(controllerClass, 3, CONTROLLER_BASE);
+  }
+
+  @Test
+  public void testDeleteFail() throws Exception {
+    testDeleteUnauthorized(controllerClass, 3, CONTROLLER_BASE);
+  }
+}
