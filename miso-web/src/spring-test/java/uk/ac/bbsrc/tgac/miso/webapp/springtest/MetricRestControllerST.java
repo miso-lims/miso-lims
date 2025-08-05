6cfb263e14789f2fa0a1c1139913eab27ee6c698
PrabhjotChopra <pchopra@oicr.on.ca>
2025-08-05 14:39:05 -0400

    test fixes

--- miso-web/src/spring-test/java/uk/ac/bbsrc/tgac/miso/webapp/springtest/MetricRestControllerST.java
+++ miso-web/src/spring-test/java/uk/ac/bbsrc/tgac/miso/webapp/springtest/MetricRestControllerST.java
@@ -2,58 +2,24 @@ package uk.ac.bbsrc.tgac.miso.webapp.springtest;
 
 import org.junit.Test;
 
-import org.springframework.web.servlet.*;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
-import javax.ws.rs.core.MediaType;
-
-import org.checkerframework.checker.units.qual.Temperature;
-import org.junit.Before;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import org.springframework.test.web.servlet.ResultActions;
-import com.jayway.jsonpath.JsonPath;
-
-import jakarta.transaction.Transactional;
-
-import static org.hamcrest.Matchers.*;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-
-import org.springframework.test.web.servlet.MvcResult;
-import uk.ac.bbsrc.tgac.miso.dto.Dtos;
-import uk.ac.bbsrc.tgac.miso.core.data.impl.Metric;
-import uk.ac.bbsrc.tgac.miso.dto.MetricDto;
-
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.servlet.View;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.security.test.context.support.WithMockUser;
-
-import uk.ac.bbsrc.tgac.miso.core.data.type.MetricCategory;
-import uk.ac.bbsrc.tgac.miso.core.data.type.StatusType;
-import java.util.Collections;
-
 import static org.junit.Assert.*;
-
 import java.util.List;
-import java.util.Arrays;
 import java.util.ArrayList;
 
-import org.springframework.test.web.servlet.MockMvc;
-import java.util.Date;
+import uk.ac.bbsrc.tgac.miso.dto.Dtos;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.Metric;
+import uk.ac.bbsrc.tgac.miso.core.data.type.MetricCategory;
+import uk.ac.bbsrc.tgac.miso.core.data.type.ThresholdType;
+import uk.ac.bbsrc.tgac.miso.dto.MetricDto;
+
 
 
 public class MetricRestControllerST extends AbstractST {
 
   private static final String CONTROLLER_BASE = "/rest/metrics";
-  private static final Class<Metric> controllerClass = Metric.class;
+  private static final Class<Metric> entityClass = Metric.class;
 
   private List<MetricDto> makeCreateDtos() {
 
@@ -67,8 +33,8 @@ public class MetricRestControllerST extends AbstractST {
 
     MetricDto two = new MetricDto();
     two.setAlias("two");
-    two.setCategory("ANALYSIS_REVIEW");
-    two.setThresholdType("BETWEEN");
+    two.setCategory("EXTRACTION");
+    two.setThresholdType("GE");
     two.setUnits("%");
 
     dtos.add(one);
@@ -80,16 +46,25 @@ public class MetricRestControllerST extends AbstractST {
   @Test
   @WithMockUser(username = "admin", password = "admin", roles = {"INTERNAL", "ADMIN"})
   public void testBulkCreateAsync() throws Exception {
-    List<Metric> codes = baseTestBulkCreateAsync(CONTROLLER_BASE, controllerClass, makeCreateDtos());
-    assertEquals(codes.get(0).getAlias(), "one");
-    assertEquals(codes.get(1).getAlias(), "two");
+    List<Metric> metrics = baseTestBulkCreateAsync(CONTROLLER_BASE, entityClass, makeCreateDtos());
+    assertEquals("one", metrics.get(0).getAlias());
+    assertEquals(MetricCategory.ANALYSIS_REVIEW, metrics.get(0).getCategory());
+    assertEquals(ThresholdType.BETWEEN, metrics.get(0).getThresholdType());
+    assertEquals("%", metrics.get(0).getUnits());
+
+
+    assertEquals("two", metrics.get(1).getAlias());
+    assertEquals(MetricCategory.EXTRACTION, metrics.get(1).getCategory());
+    assertEquals(ThresholdType.GE, metrics.get(1).getThresholdType());
+    assertEquals("%", metrics.get(1).getUnits());
+
   }
 
   @Test
   public void testBulkCreateFail() throws Exception {
     // Metric creation is for admin only, so this test is expecting failure due to
     // insufficent permission
-    testBulkCreateAsyncUnauthorized(CONTROLLER_BASE, controllerClass, makeCreateDtos());
+    testBulkCreateAsyncUnauthorized(CONTROLLER_BASE, entityClass, makeCreateDtos());
   }
 
   @Test
@@ -107,7 +82,7 @@ public class MetricRestControllerST extends AbstractST {
     dtos.add(m2);
 
     List<Metric> metrics =
-        (List<Metric>) baseTestBulkUpdateAsync(CONTROLLER_BASE, controllerClass, dtos, MetricDto::getId);
+        (List<Metric>) baseTestBulkUpdateAsync(CONTROLLER_BASE, entityClass, dtos, MetricDto::getId);
     assertEquals("m1", metrics.get(0).getAlias());
     assertEquals("m2", metrics.get(1).getAlias());
   }
@@ -124,17 +99,17 @@ public class MetricRestControllerST extends AbstractST {
     dtos.add(m1);
     dtos.add(m2);
 
-    testBulkUpdateAsyncUnauthorized(CONTROLLER_BASE, controllerClass, dtos);
+    testBulkUpdateAsyncUnauthorized(CONTROLLER_BASE, entityClass, dtos);
   }
 
   @Test
   @WithMockUser(username = "admin", password = "admin", roles = {"INTERNAL", "ADMIN"})
   public void testDeleteMetric() throws Exception {
-    testBulkDelete(controllerClass, 4, CONTROLLER_BASE);
+    testBulkDelete(entityClass, 4, CONTROLLER_BASE);
   }
 
   @Test
   public void testDeleteFail() throws Exception {
-    testDeleteUnauthorized(controllerClass, 4, CONTROLLER_BASE);
+    testDeleteUnauthorized(entityClass, 4, CONTROLLER_BASE);
   }
 }
