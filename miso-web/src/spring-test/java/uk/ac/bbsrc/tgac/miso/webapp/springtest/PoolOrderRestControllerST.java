342779e74f33a28af24a44e9617af1111a85738f
PrabhjotChopra <pchopra@oicr.on.ca>
2025-08-05 14:54:49 -0400

    test fixes

--- miso-web/src/spring-test/java/uk/ac/bbsrc/tgac/miso/webapp/springtest/PoolOrderRestControllerST.java
+++ miso-web/src/spring-test/java/uk/ac/bbsrc/tgac/miso/webapp/springtest/PoolOrderRestControllerST.java
@@ -3,43 +3,23 @@ package uk.ac.bbsrc.tgac.miso.webapp.springtest;
 import org.junit.Test;
 
 import org.springframework.web.servlet.*;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
 import javax.ws.rs.core.MediaType;
 
-import org.checkerframework.checker.units.qual.Temperature;
-import org.junit.Before;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import org.springframework.test.web.servlet.ResultActions;
 import com.jayway.jsonpath.JsonPath;
 
-import jakarta.transaction.Transactional;
 
 import static org.hamcrest.Matchers.*;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-
 import org.springframework.test.web.servlet.MvcResult;
 import uk.ac.bbsrc.tgac.miso.dto.Dtos;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.LibraryAliquot;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.PoolOrder;
 import uk.ac.bbsrc.tgac.miso.dto.PoolOrderDto;
-
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.servlet.View;
-import com.fasterxml.jackson.databind.SerializationFeature;
-
-import org.springframework.security.access.method.P;
 import org.springframework.security.test.context.support.WithMockUser;
-
-import java.util.Collections;
 
 import static org.junit.Assert.*;
 
@@ -47,14 +27,12 @@ import java.util.List;
 import java.util.Arrays;
 import java.util.ArrayList;
 
-import org.springframework.test.web.servlet.MockMvc;
-import java.util.Date;
 
 
 public class PoolOrderRestControllerST extends AbstractST {
 
   private static final String CONTROLLER_BASE = "/rest/poolorders";
-  private static final Class<PoolOrder> controllerClass = PoolOrder.class;
+  private static final Class<PoolOrder> entityClass = PoolOrder.class;
 
   @Test
   public void testCreate() throws Exception {
@@ -62,16 +40,19 @@ public class PoolOrderRestControllerST extends AbstractST {
     one.setAlias("one");
     one.setPurposeId(1L);
     one.setDraft(true);
-    PoolOrder newPoolOrder = baseTestCreate(CONTROLLER_BASE, one, controllerClass, 200);
+    PoolOrder newPoolOrder = baseTestCreate(CONTROLLER_BASE, one, entityClass, 200);
     assertEquals(newPoolOrder.getAlias(), one.getAlias());
+    assertEquals(newPoolOrder.getPurpose().getId(), one.getPurposeId().longValue());
+    assertEquals(newPoolOrder.isDraft(), one.isDraft());
+
   }
 
   @Test
   public void testUpdate() throws Exception {
-    PoolOrder poolOrder = currentSession().get(controllerClass, 1);
+    PoolOrderDto poolOrder = Dtos.asDto(currentSession().get(entityClass, 1));
     poolOrder.setAlias("updated");
 
-    PoolOrder updated = baseTestUpdate(CONTROLLER_BASE, Dtos.asDto(poolOrder), 1, controllerClass);
+    PoolOrder updated = baseTestUpdate(CONTROLLER_BASE, poolOrder, 1, entityClass);
     assertEquals(updated.getAlias(), poolOrder.getAlias());
   }
 
@@ -105,13 +86,13 @@ public class PoolOrderRestControllerST extends AbstractST {
   @Test
   public void testDeletePoolOrder() throws Exception {
     // only creator or admin can delete pool order. this user has created pool order 3
-    testBulkDelete(controllerClass, 3, CONTROLLER_BASE);
+    testBulkDelete(entityClass, 3, CONTROLLER_BASE);
   }
 
   @Test
   public void testDeleteFail() throws Exception {
     // only create or admin can delete pool order
     // admin created pool order 1
-    testDeleteUnauthorized(controllerClass, 1, CONTROLLER_BASE);
+    testDeleteUnauthorized(entityClass, 1, CONTROLLER_BASE);
   }
 }
