428d6a3169974b9fbae6cb29ed7753be9a458854
PrabhjotChopra <pchopra@oicr.on.ca>
2025-08-05 13:46:15 -0400

    PR comments implemented

--- miso-web/src/spring-test/java/uk/ac/bbsrc/tgac/miso/webapp/springtest/KitDescriptorRestControllerST.java
+++ miso-web/src/spring-test/java/uk/ac/bbsrc/tgac/miso/webapp/springtest/KitDescriptorRestControllerST.java
@@ -5,62 +5,37 @@ import org.junit.Test;
 import org.springframework.web.servlet.*;
 
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
-
 import javax.ws.rs.core.MediaType;
-
-import org.checkerframework.checker.units.qual.Temperature;
-import org.junit.Before;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import org.springframework.test.web.servlet.ResultActions;
 import com.jayway.jsonpath.JsonPath;
-
 import static org.hamcrest.Matchers.*;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.TargetedSequencing;
-
-
-import org.springframework.test.web.servlet.MvcResult;
 import uk.ac.bbsrc.tgac.miso.dto.Dtos;
 
 import uk.ac.bbsrc.tgac.miso.core.data.impl.kit.KitDescriptor;
+import uk.ac.bbsrc.tgac.miso.core.data.type.KitType;
+import uk.ac.bbsrc.tgac.miso.core.data.type.PlatformType;
 import uk.ac.bbsrc.tgac.miso.dto.KitDescriptorDto;
 import uk.ac.bbsrc.tgac.miso.webapp.controller.rest.KitDescriptorRestController.KitChangeTargetedSequencingRequest;
 import uk.ac.bbsrc.tgac.miso.webapp.controller.rest.KitDescriptorRestController;
-
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.servlet.View;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.security.test.context.support.WithMockUser;
-import uk.ac.bbsrc.tgac.miso.core.data.type.StatusType;
 import static org.junit.Assert.*;
-import java.util.Collections;
 import org.springframework.util.MultiValueMap;
 import org.springframework.util.LinkedMultiValueMap;
-
-
-import java.util.List;
 import java.util.Arrays;
-import java.util.ArrayList;
 import java.util.Set;
-
-import org.springframework.test.web.servlet.MockMvc;
-import java.util.Date;
-import java.time.LocalDate;
 
 
 public class KitDescriptorRestControllerST extends AbstractST {
 
   private static final String CONTROLLER_BASE = "/rest/kitdescriptors";
-  private static final Class<KitDescriptor> controllerClass = KitDescriptor.class;
+  private static final Class<KitDescriptor> entityClass = KitDescriptor.class;
 
   @Test
   public void testDatatable() throws Exception {
-    testDtRequest(CONTROLLER_BASE + "/dt", Arrays.asList(1,2,3,4));
+    testDtRequest(CONTROLLER_BASE + "/dt", Arrays.asList(1, 2, 3, 4));
   }
 
   @Test
@@ -74,7 +49,7 @@ public class KitDescriptorRestControllerST extends AbstractST {
 
   @Test
   public void testListAll() throws Exception {
-    testListAll(CONTROLLER_BASE, Arrays.asList(1,2,3,4));
+    testListAll(CONTROLLER_BASE, Arrays.asList(1, 2, 3, 4));
   }
 
   @Test
@@ -87,14 +62,17 @@ public class KitDescriptorRestControllerST extends AbstractST {
   @WithMockUser(username = "admin", password = "admin", roles = {"INTERNAL", "ADMIN"})
   public void testChangeTargetedSequencing() throws Exception {
     // must be admin to change targeted sequencing
-    KitDescriptorRestController.KitChangeTargetedSequencingRequest req = new KitDescriptorRestController.KitChangeTargetedSequencingRequest();
-    req.setRemove(Arrays.asList(1L,2L));
+    KitDescriptorRestController.KitChangeTargetedSequencingRequest req =
+        new KitDescriptorRestController.KitChangeTargetedSequencingRequest();
+    req.setRemove(Arrays.asList(1L, 2L));
     req.setAdd(Arrays.asList(3L));
 
-    getMockMvc().perform(put(CONTROLLER_BASE + "/1/targetedsequencing").content(makeJson(req)).contentType(MediaType.APPLICATION_JSON))
-    .andExpect(status().isOk());
+    getMockMvc()
+        .perform(put(CONTROLLER_BASE + "/1/targetedsequencing").content(makeJson(req))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk());
 
-    KitDescriptor changed = currentSession().get(controllerClass, 1);
+    KitDescriptor changed = currentSession().get(entityClass, 1);
     Set<TargetedSequencing> seq = changed.getTargetedSequencing();
     assertTrue(seq.size() == 1);
     assertTrue(seq.iterator().next().getId() == 3);
@@ -102,12 +80,15 @@ public class KitDescriptorRestControllerST extends AbstractST {
 
   @Test
   public void testChangeTargetedSequencingUnauthorized() throws Exception {
-    KitDescriptorRestController.KitChangeTargetedSequencingRequest req = new KitDescriptorRestController.KitChangeTargetedSequencingRequest();
-    req.setRemove(Arrays.asList(1L,2L));
+    KitDescriptorRestController.KitChangeTargetedSequencingRequest req =
+        new KitDescriptorRestController.KitChangeTargetedSequencingRequest();
+    req.setRemove(Arrays.asList(1L, 2L));
     req.setAdd(Arrays.asList(3L));
 
-    getMockMvc().perform(put(CONTROLLER_BASE + "/1/targetedsequencing").content(makeJson(req)).contentType(MediaType.APPLICATION_JSON))
-    .andExpect(status().isUnauthorized());
+    getMockMvc()
+        .perform(put(CONTROLLER_BASE + "/1/targetedsequencing").content(makeJson(req))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isUnauthorized());
   }
 
 
@@ -135,14 +116,21 @@ public class KitDescriptorRestControllerST extends AbstractST {
   @WithMockUser(username = "admin", password = "admin", roles = {"INTERNAL", "ADMIN"})
   public void testCreate() throws Exception {
     // must be admin to create
-    KitDescriptor newKitDesc = baseTestCreate(CONTROLLER_BASE, makeCreateDto(), controllerClass, 201);
+    KitDescriptor newKitDesc = baseTestCreate(CONTROLLER_BASE, makeCreateDto(), entityClass, 201);
     assertEquals("kit desc", newKitDesc.getName());
+    assertEquals(false, newKitDesc.isArchived());
+    assertEquals(KitType.LIBRARY, newKitDesc.getKitType());
+    assertEquals("me", newKitDesc.getManufacturer());
+    assertEquals("kit desc", newKitDesc.getName());
+    assertEquals("1453", newKitDesc.getPartNumber());
+    assertEquals(PlatformType.ILLUMINA, newKitDesc.getPlatformType());
+    assertEquals(2, newKitDesc.getStockLevel().intValue());
   }
 
   @Test
   public void testCreateFail() throws Exception {
     // must be admin to create
-    testCreateUnauthorized(CONTROLLER_BASE, makeCreateDto(), controllerClass);
+    testCreateUnauthorized(CONTROLLER_BASE, makeCreateDto(), entityClass);
   }
 
 
@@ -151,9 +139,9 @@ public class KitDescriptorRestControllerST extends AbstractST {
   public void testUpdate() throws Exception {
     // must be admin to update
 
-    KitDescriptor kitdesc = currentSession().get(controllerClass, 1);
+    KitDescriptorDto kitdesc = Dtos.asDto(currentSession().get(entityClass, 1));
     kitdesc.setName("updated");
-    KitDescriptor updated = baseTestUpdate(CONTROLLER_BASE, Dtos.asDto(kitdesc), 1, controllerClass);
+    KitDescriptor updated = baseTestUpdate(CONTROLLER_BASE, kitdesc, 1, entityClass);
     assertEquals("updated", updated.getName());
   }
 
@@ -161,9 +149,9 @@ public class KitDescriptorRestControllerST extends AbstractST {
   public void testUpdateFail() throws Exception {
     // must be admin to update
 
-    KitDescriptor kitdesc = currentSession().get(controllerClass, 1);
+    KitDescriptorDto kitdesc = Dtos.asDto(currentSession().get(entityClass, 1));
     kitdesc.setName("updated");
-    testUpdateUnauthorized(CONTROLLER_BASE, Dtos.asDto(kitdesc), 1, controllerClass);
+    testUpdateUnauthorized(CONTROLLER_BASE, kitdesc, 1, entityClass);
   }
 
 
@@ -171,12 +159,12 @@ public class KitDescriptorRestControllerST extends AbstractST {
   @WithMockUser(username = "admin", password = "admin", roles = {"INTERNAL", "ADMIN"})
   public void testDelete() throws Exception {
     // must be admin to delete instrument
-    testBulkDelete(controllerClass, 4, CONTROLLER_BASE);
+    testBulkDelete(entityClass, 4, CONTROLLER_BASE);
   }
 
   @Test
   @WithMockUser(username = "hhenderson", roles = {"INTERNAL"})
   public void testDeleteFail() throws Exception {
-    testDeleteUnauthorized(controllerClass, 4, CONTROLLER_BASE);
+    testDeleteUnauthorized(entityClass, 4, CONTROLLER_BASE);
   }
 }
