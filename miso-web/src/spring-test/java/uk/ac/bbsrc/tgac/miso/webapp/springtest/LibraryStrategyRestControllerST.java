2521351b81747fab42991e3cc710f6c30d9f9432
PrabhjotChopra <pchopra@oicr.on.ca>
2025-08-05 14:29:30 -0400

    test fixes

--- miso-web/src/spring-test/java/uk/ac/bbsrc/tgac/miso/webapp/springtest/LibraryStrategyRestControllerST.java
+++ miso-web/src/spring-test/java/uk/ac/bbsrc/tgac/miso/webapp/springtest/LibraryStrategyRestControllerST.java
@@ -2,51 +2,14 @@ package uk.ac.bbsrc.tgac.miso.webapp.springtest;
 
 import org.junit.Test;
 
-import org.springframework.web.servlet.*;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+import org.springframework.security.test.context.support.WithMockUser;
+import static org.junit.Assert.*;
+import java.util.List;
+import java.util.ArrayList;
 
-import javax.ws.rs.core.MediaType;
-
-import org.checkerframework.checker.units.qual.Temperature;
-import org.junit.Before;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import org.springframework.test.web.servlet.ResultActions;
-import com.jayway.jsonpath.JsonPath;
-
-import jakarta.transaction.Transactional;
-
-import static org.hamcrest.Matchers.*;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-
-import org.springframework.test.web.servlet.MvcResult;
 import uk.ac.bbsrc.tgac.miso.dto.Dtos;
 import uk.ac.bbsrc.tgac.miso.core.data.type.LibraryStrategyType;
 import uk.ac.bbsrc.tgac.miso.dto.LibraryStrategyTypeDto;
-
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.servlet.View;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import org.springframework.security.test.context.support.WithMockUser;
-import uk.ac.bbsrc.tgac.miso.core.data.type.StatusType;
-import java.util.Collections;
-
-
-import static org.junit.Assert.*;
-
-import java.util.List;
-import java.util.Arrays;
-import java.util.ArrayList;
-
-import org.springframework.test.web.servlet.MockMvc;
-import java.util.Date;
 
 
 public class LibraryStrategyRestControllerST extends AbstractST {
@@ -76,8 +39,12 @@ public class LibraryStrategyRestControllerST extends AbstractST {
   public void testBulkCreateAsync() throws Exception {
     List<LibraryStrategyType> libraryStrategyTypes =
         baseTestBulkCreateAsync(CONTROLLER_BASE, entityClass, makeCreateDtos());
-    assertEquals(libraryStrategyTypes.get(0).getName(), "one");
-    assertEquals(libraryStrategyTypes.get(1).getName(), "two");
+    assertEquals("one", libraryStrategyTypes.get(0).getName());
+    assertEquals("first", libraryStrategyTypes.get(0).getDescription());
+
+    assertEquals("two", libraryStrategyTypes.get(1).getName());
+    assertEquals("second", libraryStrategyTypes.get(1).getDescription());
+
   }
 
   @Test
