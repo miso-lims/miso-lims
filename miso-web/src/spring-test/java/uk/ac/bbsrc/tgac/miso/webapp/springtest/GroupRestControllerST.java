f49f5ac2084c895267a605e75ce431f1b48125cf
PrabhjotChopra <pchopra@oicr.on.ca>
2025-08-05 12:36:51 -0400

    removed unused imports

--- miso-web/src/spring-test/java/uk/ac/bbsrc/tgac/miso/webapp/springtest/GroupRestControllerST.java
+++ miso-web/src/spring-test/java/uk/ac/bbsrc/tgac/miso/webapp/springtest/GroupRestControllerST.java
@@ -3,44 +3,24 @@ package uk.ac.bbsrc.tgac.miso.webapp.springtest;
 import org.junit.Test;
 
 import org.springframework.web.servlet.*;
-
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
-
 import javax.ws.rs.core.MediaType;
 
-import org.checkerframework.checker.units.qual.Temperature;
-import org.junit.Before;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import org.springframework.test.web.servlet.ResultActions;
-import com.jayway.jsonpath.JsonPath;
-
 import static org.hamcrest.Matchers.*;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-
-import org.springframework.test.web.servlet.MvcResult;
 import com.eaglegenomics.simlims.core.Group;
 import uk.ac.bbsrc.tgac.miso.dto.GroupDto;
 import uk.ac.bbsrc.tgac.miso.dto.Dtos;
 
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.servlet.View;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.security.test.context.support.WithMockUser;
-import uk.ac.bbsrc.tgac.miso.core.data.type.StatusType;
 import static org.junit.Assert.*;
-import java.util.Collections;
-
 import java.util.List;
-import java.util.Arrays;
 import java.util.ArrayList;
 
-import org.springframework.test.web.servlet.MockMvc;
-import java.util.Date;
 
 
 public class GroupRestControllerST extends AbstractST {
@@ -88,7 +68,7 @@ public class GroupRestControllerST extends AbstractST {
     ids.add(3L);
     ids.add(1L);
     Group shrunk = currentSession().get(entityClass, 1);
-    assertTrue(shrunk.getUsers().size() > 0);
+    assertEquals(2, shrunk.getUsers().size());
     getMockMvc()
         .perform(
             post(CONTROLLER_BASE + "/1/users/remove").content(makeJson(ids)).contentType(MediaType.APPLICATION_JSON))
