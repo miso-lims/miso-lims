2d6a083cd94186aadd40e535c0d08bf56d55c250
PrabhjotChopra <pchopra@oicr.on.ca>
2025-08-05 14:43:45 -0400

    test fixes

--- miso-web/src/spring-test/java/uk/ac/bbsrc/tgac/miso/webapp/springtest/MetricSubcategoryRestControllerST.java
+++ miso-web/src/spring-test/java/uk/ac/bbsrc/tgac/miso/webapp/springtest/MetricSubcategoryRestControllerST.java
@@ -2,51 +2,16 @@ package uk.ac.bbsrc.tgac.miso.webapp.springtest;
 
 import org.junit.Test;
 
-import org.springframework.web.servlet.*;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+import org.springframework.security.test.context.support.WithMockUser;
+import static org.junit.Assert.*;
+import java.util.List;
+import java.util.ArrayList;
 
-import javax.ws.rs.core.MediaType;
-
-import org.checkerframework.checker.units.qual.Temperature;
-import org.junit.Before;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import org.springframework.test.web.servlet.ResultActions;
-import com.jayway.jsonpath.JsonPath;
-
-import jakarta.transaction.Transactional;
-
-import static org.hamcrest.Matchers.*;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-
-import org.springframework.test.web.servlet.MvcResult;
 import uk.ac.bbsrc.tgac.miso.dto.Dtos;
 import uk.ac.bbsrc.tgac.miso.dto.MetricDto;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.MetricSubcategory;
+import uk.ac.bbsrc.tgac.miso.core.data.type.MetricCategory;
 import uk.ac.bbsrc.tgac.miso.dto.MetricSubcategoryDto;
-
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.servlet.View;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import org.springframework.security.test.context.support.WithMockUser;
-
-import java.util.Collections;
-
-import static org.junit.Assert.*;
-
-import java.util.List;
-import java.util.Arrays;
-import java.util.ArrayList;
-
-import org.springframework.test.web.servlet.MockMvc;
-import java.util.Date;
 
 
 public class MetricSubcategoryRestControllerST extends AbstractST {
@@ -76,8 +41,12 @@ public class MetricSubcategoryRestControllerST extends AbstractST {
   @WithMockUser(username = "admin", password = "admin", roles = {"INTERNAL", "ADMIN"})
   public void testBulkCreateAsync() throws Exception {
     List<MetricSubcategory> codes = baseTestBulkCreateAsync(CONTROLLER_BASE, entityClass, makeCreateDtos());
-    assertEquals(codes.get(0).getAlias(), "one");
-    assertEquals(codes.get(1).getAlias(), "two");
+    assertEquals("one", codes.get(0).getAlias());
+    assertEquals(MetricCategory.FULL_DEPTH_SEQUENCING, codes.get(0).getCategory());
+
+    assertEquals("two", codes.get(1).getAlias());
+    assertEquals(MetricCategory.EXTRACTION, codes.get(1).getCategory());
+
   }
 
   @Test
