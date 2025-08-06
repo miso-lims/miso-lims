d2eaa33946daae0e50668275b88d501ab7949f2f
PrabhjotChopra <pchopra@oicr.on.ca>
2025-08-06 15:22:33 -0400

    make the blank tests

---
+++ miso-web/src/spring-test/java/uk/ac/bbsrc/tgac/miso/webapp/springtest/AttachmentControllerST.java
@@ -0,0 +1,21 @@
+package uk.ac.bbsrc.tgac.miso.webapp.springtest;
+
+public class AttachmentControllerST extends AbstractST {
+  private static final String CONTROLLER_BASE = "/attachments";
+
+
+  @Test
+  public void testAcceptUpload() throws Exception {
+
+  }
+
+  @Test
+  public void testAcceptSharedUpload() throws Exception {
+
+  }
+
+  @Test
+  public void testDownloadAttachemnt() throws Exception {
+
+  }
+}
