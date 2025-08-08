6ff9241b6c39d0d5db8ed47b695ce225d65882c5
PrabhjotChopra <pchopra@oicr.on.ca>
2025-08-08 16:42:14 -0400

    set the linked objects for the create tests to all have different IDs

--- miso-web/src/spring-test/java/uk/ac/bbsrc/tgac/miso/webapp/springtest/LibraryDesignRestControllerST.java
+++ miso-web/src/spring-test/java/uk/ac/bbsrc/tgac/miso/webapp/springtest/LibraryDesignRestControllerST.java
@@ -24,16 +24,16 @@ public class LibraryDesignRestControllerST extends AbstractST {
     LibraryDesignDto one = new LibraryDesignDto();
     one.setName("1");
     one.setSampleClassId(1L);
-    one.setStrategyId(1L);
-    one.setSelectionId(1L);
-    one.setDesignCodeId(1L);
+    one.setStrategyId(20L);
+    one.setSelectionId(15L);
+    one.setDesignCodeId(7L);
 
     LibraryDesignDto two = new LibraryDesignDto();
     two.setName("2");
     two.setSampleClassId(23L);
     two.setStrategyId(2L);
-    two.setSelectionId(2L);
-    two.setDesignCodeId(2L);
+    two.setSelectionId(16L);
+    two.setDesignCodeId(8L);
 
     dtos.add(one);
     dtos.add(two);
@@ -46,16 +46,16 @@ public class LibraryDesignRestControllerST extends AbstractST {
   public void testBulkCreateAsync() throws Exception {
     List<LibraryDesign> codes = baseTestBulkCreateAsync(CONTROLLER_BASE, entityClass, makeCreateDtos());
     assertEquals("1", codes.get(0).getName());
-    assertEquals(1L, codes.get(0).getSampleClass().getId());
+    assertEquals(20L, codes.get(0).getSampleClass().getId());
     assertEquals(1L, codes.get(0).getLibraryStrategyType().getId());
-    assertEquals(1L, codes.get(0).getLibrarySelectionType().getId());
-    assertEquals(1L, codes.get(0).getLibraryDesignCode().getId());
+    assertEquals(15L, codes.get(0).getLibrarySelectionType().getId());
+    assertEquals(7L, codes.get(0).getLibraryDesignCode().getId());
 
     assertEquals("2", codes.get(1).getName());
     assertEquals(23L, codes.get(1).getSampleClass().getId());
     assertEquals(2L, codes.get(1).getLibraryStrategyType().getId());
-    assertEquals(2L, codes.get(1).getLibrarySelectionType().getId());
-    assertEquals(2L, codes.get(1).getLibraryDesignCode().getId());
+    assertEquals(16L, codes.get(1).getLibrarySelectionType().getId());
+    assertEquals(8L, codes.get(1).getLibraryDesignCode().getId());
 
   }
 
