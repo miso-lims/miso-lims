870aee6bc70b2865e31afcff802df478c2ff1c8d
PrabhjotChopra <pchopra@oicr.on.ca>
2025-08-22 13:28:11 -0400

    box use tests done and passing

---
+++ miso-web/src/spring-test/java/uk/ac/bbsrc/tgac/miso/webapp/springtest/BoxUseControllerST.java
@@ -0,0 +1,45 @@
+package uk.ac.bbsrc.tgac.miso.webapp.springtest;
+
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+import com.jayway.jsonpath.JsonPath;
+import static org.junit.Assert.assertEquals;
+import uk.ac.bbsrc.tgac.miso.core.data.BoxUse;
+
+
+public class BoxUseControllerST extends AbstractST {
+
+  private static final String CONTROLLER_BASE = "/boxuse";
+  private static final Class<BoxUse> entityClass = BoxUse.class;
+
+  private void assertDbVsModelObjects(String resultJson, List<Long> ids) {
+    assertEquals(Integer.valueOf(ids.size()), JsonPath.read(resultJson, "$.length()"));
+
+    for (int i = 0; i < ids.size(); i++) {
+      BoxUse dbObject = currentSession().get(entityClass, ids.get(i));
+      assertEquals(dbObject.getId(), readLong(resultJson, "$[" + i + "].id"));
+      assertEquals(dbObject.getAlias(), JsonPath.read(resultJson, "$[" + i + "].alias"));
+    }
+  }
+
+  @Test
+  public void testList() throws Exception {
+    List<Long> ids = Arrays.asList(1L, 3L, 2L, 4L, 5L, 6L);
+
+    String resultJson = testStaticListPage(CONTROLLER_BASE + "/list", "data");
+    assertDbVsModelObjects(resultJson, ids);
+  }
+
+  @Test
+  public void testBulkCreate() throws Exception {
+    testBulkCreatePage(CONTROLLER_BASE + "/bulk/new", 3, "input");
+  }
+
+  @Test
+  public void testBulkEdit() throws Exception {
+    List<Long> ids = Arrays.asList(1L, 2L);
+    String resultJson = testBulkEditPage(CONTROLLER_BASE + "/bulk/edit", ids, "input");
+    assertDbVsModelObjects(resultJson, ids);
+  }
+}
