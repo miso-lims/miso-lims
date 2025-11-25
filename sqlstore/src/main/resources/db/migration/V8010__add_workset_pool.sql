8218adaed4ed5ef7b510beb67a42b99de0065346
Riya Patel <rpatel@WL7405-RBHUIYAN.ad.oicr.on.ca>
2025-11-25 14:06:16 -0500

    Add & remove from workset button skeleton code

--- sqlstore/src/main/resources/db/migration/V8010__add_workset_pool.sql
+++ sqlstore/src/main/resources/db/migration/V8010__add_workset_pool.sql
@@ -1,12 +1,11 @@
-CREATE TABLE `Workset_Pool`(
-    `worksetId` BIGINT NOT NULL,
-    `poolId` BIGINT NOT NULL,
-    PRIMARY KEY (`worksetId`, `poolId`),
-    CONSTRAINT fk_workset_pool_workset
-        FOREIGN KEY (`worksetId`) REFERENCES `Workset`(`worksetId`),
-    CONSTRAINT fk_workset_pool_pool
-        FOREIGN KEY (`poolId`) REFERENCES `Pool`(`poolId`)
-);
-
-CREATE INDEX idx_workset_pool_worksetId on `Workset_Pool` (`worksetId`);
-CREATE INDEX idx_workset_pool_poolId on `Workset_Pool` (`poolId`);
+DROP TABLE IF EXISTS Workset_Pool;
+CREATE TABLE Workset_Pool(
+    worksetId BIGINT NOT NULL,
+    poolId BIGINT NOT NULL,
+    addedTime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (worksetId, poolId),
+    CONSTRAINT fk_worksetPool_workset
+        FOREIGN KEY (worksetId) REFERENCES Workset(worksetId),
+    CONSTRAINT fk_worksetPool_pool
+        FOREIGN KEY (poolId) REFERENCES Pool(poolId)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
