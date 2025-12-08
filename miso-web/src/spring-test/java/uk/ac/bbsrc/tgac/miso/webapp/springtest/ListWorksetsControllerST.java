bfe94750467a0f2687b6667d36edb0bd841b2baa
Riya Patel <rpatel@oicr.on.ca>
2025-12-08 09:25:36 -0500

    GLT-4594-Test-ListWorksetsController (#3175)
    * Springtest for ListWorksetsController
    
    Co-authored-by: Riya Patel <rpatel@ML8279-RPATEL.local>

---
+++ miso-web/src/spring-test/java/uk/ac/bbsrc/tgac/miso/webapp/springtest/ListWorksetsControllerST.java
@@ -0,0 +1,30 @@
+package uk.ac.bbsrc.tgac.miso.webapp.springtest;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.hamcrest.Matchers.hasKey;
+
+import org.junit.Test;
+
+import uk.ac.bbsrc.tgac.miso.core.data.impl.workset.Workset;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.workset.WorksetCategory;
+
+public class ListWorksetsControllerST extends AbstractST {
+
+    @Test
+    public void testListWorksets() throws Exception {
+        WorksetCategory cat = currentSession().get(WorksetCategory.class, 1L);
+        getMockMvc().perform(get("/worksets"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("/WEB-INF/pages/listTabbed.jsp"))
+                .andExpect(model().attribute("title", "Worksets"))
+                .andExpect(model().attribute("targetType", "ListTarget.workset"))
+                .andExpect(model().attribute("tabs", hasKey(Workset.ReservedWord.MINE.getText())))
+                .andExpect(model().attribute("tabs", hasKey(Workset.ReservedWord.ALL.getText())))
+                .andExpect(model().attribute("tabs", hasKey(Workset.ReservedWord.UNCATEGORIZED.getText())))
+                .andExpect(model().attribute("tabs", hasKey(cat.getAlias())));
+
+    }
+}
