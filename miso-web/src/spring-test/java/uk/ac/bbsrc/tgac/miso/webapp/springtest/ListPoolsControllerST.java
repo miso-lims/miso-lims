5a61490ad1c1ce8b5ea32d2640af84a5d35a8fc4
Riya Patel <rpatel@wl7883-maguilar.ad.oicr.on.ca>
2025-12-16 16:32:18 -0500

    Test case update- platform confirmation

--- miso-web/src/spring-test/java/uk/ac/bbsrc/tgac/miso/webapp/springtest/ListPoolsControllerST.java
+++ miso-web/src/spring-test/java/uk/ac/bbsrc/tgac/miso/webapp/springtest/ListPoolsControllerST.java
@@ -7,10 +7,25 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasKey;
 
+import uk.ac.bbsrc.tgac.miso.core.data.Pool;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.PoolImpl;
+import uk.ac.bbsrc.tgac.miso.webapp.controller.view.ListPoolsController;
+import uk.ac.bbsrc.tgac.miso.core.service.PoolService;
+
 import org.junit.Test;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.servlet.ModelAndView;
 
 public class ListPoolsControllerST extends AbstractST {
+
+    @Autowired
+    private PoolService poolService;
+
+    @Autowired
+    private ListPoolsController controller;
+
 
     @Test
     @WithMockUser(username = "admin", roles = {"INTERNAL", "ADMIN"})
@@ -21,6 +36,24 @@ public class ListPoolsControllerST extends AbstractST {
                 .andExpect(view().name("/WEB-INF/pages/listTabbed.jsp"))
                 .andExpect(model().attributeExists("title"))
                 .andExpect(model().attribute("title", "Pools"));
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"INTERNAL", "ADMIN"})
+    public void testPoolsTabsContainsLoadedPools() throws Exception {
+        Pool pool = poolService.getByAlias("POOL_1");
+        assertNotNull(pool.getPlatformType());
+
+        ModelMap model = new ModelMap();
+        ModelAndView mav = controller.listPools(model);
+
+        String platformName = pool.getPlatformType().name();
+        String modelString = String.valueOf(mav.getModel());
+
+        assertThat("Expected pools tab model to include platform type of a loaded pool",
+                modelString,
+                containsString(platformName));
+
     }
 
 }
