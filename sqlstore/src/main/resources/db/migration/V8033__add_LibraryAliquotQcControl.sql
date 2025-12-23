c089dd62e2e6614661eaa489b276b736b89816eb
Riya Patel <rpatel@WL6894-RTAMARC.ad.oicr.on.ca>
2025-12-23 15:07:06 -0500

    Migration issue solved

--- sqlstore/src/main/resources/db/migration/V8033__add_LibraryAliquotQcControl.sql
+++ sqlstore/src/main/resources/db/migration/V8033__add_LibraryAliquotQcControl.sql
@@ -21,3 +21,14 @@ CREATE TABLE LibraryAliquotQc(
     CONSTRAINT fk_LibraryAliquotQc_instrument FOREIGN KEY (instrumentId) REFERENCES Instrument (instrumentId)
 )
 Engine=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE LibraryAliquotQcControl (
+  qcControlId bigint PRIMARY KEY AUTO_INCREMENT,
+  qcId bigint NOT NULL,
+  controlId bigint NOT NULL,
+  lot varchar(50) NOT NULL,
+  qcPassed BOOLEAN NOT NULL,
+  CONSTRAINT fk_libraryAliquotQcControl_qc FOREIGN KEY (qcId) REFERENCES LibraryAliquotQC (qcId),
+  CONSTRAINT fk_libraryAliquotQcControl_control FOREIGN KEY (controlId) REFERENCES QcControl (controlId)
+) Engine=InnoDB DEFAULT CHARSET=utf8mb4;
