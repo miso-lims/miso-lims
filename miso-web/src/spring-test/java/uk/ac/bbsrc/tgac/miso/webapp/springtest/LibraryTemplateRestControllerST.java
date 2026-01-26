852a9fe4fd3a81686b218d69380869644e537fd4
Riya Patel <rpatel@oicr.on.ca>
2026-01-26 16:17:46 -0500

    Update LibraryTemplateRestControllerST.java

--- miso-web/src/spring-test/java/uk/ac/bbsrc/tgac/miso/webapp/springtest/LibraryTemplateRestControllerST.java
+++ miso-web/src/spring-test/java/uk/ac/bbsrc/tgac/miso/webapp/springtest/LibraryTemplateRestControllerST.java
@@ -27,11 +27,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import uk.ac.bbsrc.tgac.miso.dto.Dtos;
 import uk.ac.bbsrc.tgac.miso.core.data.Project;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.LibraryTemplate;
 import uk.ac.bbsrc.tgac.miso.core.data.impl.ProjectImpl;
 import uk.ac.bbsrc.tgac.miso.dto.LibraryTemplateDto;
+import uk.ac.bbsrc.tgac.miso.dto.LibraryTemplateIndexDto;
+import uk.ac.bbsrc.tgac.miso.core.data.type.PlatformType;
+import uk.ac.bbsrc.tgac.miso.core.data.VolumeUnit;
 
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.View;
@@ -54,7 +58,30 @@ import java.util.Date;
 public class LibraryTemplateRestControllerST extends AbstractST {
 
   private static final String CONTROLLER_BASE = "/rest/librarytemplates";
-  private static final Class<LibraryTemplate> controllerClass = LibraryTemplate.class;
+  private static final Class<LibraryTemplate> entityClass = LibraryTemplate.class;
+
+  private List<LibraryTemplateDto> makeCreateDtos() {
+
+      LibraryTemplateDto dto1 = new LibraryTemplateDto();
+      dto1.setAlias("Template one");
+      dto1.setLibraryTypeId(1L);
+      dto1.setSelectionId(1L);
+      dto1.setPlatformType(PlatformType.ILLUMINA.name());
+      dto1.setDefaultVolume("10.5");
+      dto1.setVolumeUnits(VolumeUnit.MICROLITRES.name());
+      dto1.setIndexFamilyId(1L);
+
+      LibraryTemplateDto dto2 = new LibraryTemplateDto();
+      dto2.setAlias("Template two");
+      dto2.setLibraryTypeId(1L);
+      dto2.setSelectionId(1L);
+      dto2.setPlatformType(PlatformType.ILLUMINA.name());
+      dto2.setDefaultVolume("20.0");
+      dto2.setVolumeUnits(VolumeUnit.MICROLITRES.name());
+      dto2.setIndexFamilyId(1L);
+
+      return Arrays.asList(dto1, dto2);
+    }
 
   @Test
   public void testDatatable() throws Exception {
@@ -63,7 +90,52 @@ public class LibraryTemplateRestControllerST extends AbstractST {
 
   @Test
   public void testDatatableByProject() throws Exception {
-    testDtRequest(CONTROLLER_BASE + "/dt/project/2", Arrays.asList(1, 2));
+    testDtRequest(CONTROLLER_BASE + "/dt/project/1", Arrays.asList(1, 2));
+  }
+
+  @Test
+  public void testBulkCreateAsync() throws Exception {
+      List<LibraryTemplate> libraryTemplates =
+              baseTestBulkCreateAsync(CONTROLLER_BASE, entityClass, makeCreateDtos());
+      assertEquals("Template one",libraryTemplates.get(0).getAlias());
+      assertEquals(1L, libraryTemplates.get(0).getLibraryType().getId());
+      assertEquals(10.5, libraryTemplates.get(0).getDefaultVolume().doubleValue(), 0.0);
+      assertEquals(PlatformType.ILLUMINA, libraryTemplates.get(0).getPlatformType());
+
+      assertEquals("Template two", libraryTemplates.get(1).getAlias());
+      assertEquals(20, libraryTemplates.get(1).getDefaultVolume().doubleValue(), 0.0);
+  }
+
+  @Test
+  public void testBulkUpdateAsync() throws Exception {
+      LibraryTemplateDto one = Dtos.asDto(currentSession().get(LibraryTemplate.class, 1));
+      LibraryTemplateDto three = Dtos.asDto(currentSession().get(LibraryTemplate.class, 3));
+      one.setAlias("one");
+      three.setAlias("three");
+
+      List<LibraryTemplateDto> dtos = new ArrayList<LibraryTemplateDto>();
+      dtos.add(one);
+      dtos.add(three);
+
+      List<LibraryTemplate> libraryTemplates =baseTestBulkUpdateAsync(CONTROLLER_BASE, entityClass, dtos, LibraryTemplateDto::getId);
+      assertEquals("one", libraryTemplates.get(0).getAlias());
+      assertEquals("three", libraryTemplates.get(1).getAlias());
+  }
+
+
+  @Test
+  public void testCreate() throws Exception {
+      LibraryTemplate fam = baseTestCreate(CONTROLLER_BASE, makeCreateDtos().get(0), entityClass, 200);
+      assertEquals(fam.getAlias(), "Template one");
+  }
+
+  @Test
+  public void testUpdate() throws Exception {
+      LibraryTemplateDto testlibtemp = Dtos.asDto(currentSession().get(LibraryTemplate.class, 1));
+      testlibtemp.setAlias("tester");
+
+      LibraryTemplate updated = baseTestUpdate(CONTROLLER_BASE, testlibtemp, 1, entityClass);
+      assertEquals("tester", updated.getAlias());
   }
 
   @Test
@@ -76,93 +148,91 @@ public class LibraryTemplateRestControllerST extends AbstractST {
 
     Project proj = currentSession().get(ProjectImpl.class, 1);
 
-    LibraryTemplate temp1 = currentSession().get(controllerClass, 1);
-    LibraryTemplate temp2 = currentSession().get(controllerClass, 2);
+    LibraryTemplate temp1 = currentSession().get(entityClass, 1);
+    LibraryTemplate temp2 = currentSession().get(entityClass, 2);
     assertTrue(temp1.getProjects().contains(proj));
     assertTrue(temp2.getProjects().contains(proj));
   }
 
   @Test
   public void testBulkRemoveProject() throws Exception {
-    // TODO
+      getMockMvc()
+              .perform(post(CONTROLLER_BASE + "/project/remove").param("projectId", "1").content(makeJson(Arrays.asList(1)))
+                      .contentType(MediaType.APPLICATION_JSON))
+              .andExpect(status().isNoContent());
+
+      Project proj = currentSession().get(ProjectImpl.class, 1);
+      LibraryTemplate temp = currentSession().get(entityClass, 1);
+
+      assertTrue(temp.getProjects().stream().noneMatch(p -> p.getId() == 1));
+  }
+
+  @Test
+  public void testBulkDelete() throws Exception{
+      LibraryTemplateDto dto = makeCreateDtos().get(0);
+      LibraryTemplate created = baseTestCreate(CONTROLLER_BASE, dto, entityClass, 200);
+      long id = created.getId();
+
+      assertNotNull(currentSession().get(entityClass, id));
+
+      getMockMvc()
+              .perform(post(CONTROLLER_BASE + "/bulk-delete").content(makeJson(Arrays.asList(id)))
+                      .contentType(MediaType.APPLICATION_JSON))
+              .andExpect(status().isNoContent());
+      assertNull(currentSession().get(entityClass, id));
   }
 
   @Test
   public void testAddIndices() throws Exception {
-    // TODO
+    long templateId = 1L;
 
+    LibraryTemplateIndexDto indexDto = new LibraryTemplateIndexDto();
+    indexDto.setBoxPosition("A03");
+    indexDto.setIndex1Id(3L);
+
+    pollingHelper("post", Arrays.asList(indexDto), CONTROLLER_BASE + "/" + templateId + "/indices");
+
+    LibraryTemplate template = currentSession().get(entityClass, templateId);
+    assertNotNull(template.getIndexOnes().get("A03"));
+    assertEquals(3L, template.getIndexOnes().get("A03").getId());
   }
 
   @Test
   public void testUpdateIndices() throws Exception {
-    // TODO
+      long templateId = 1L;
 
+      LibraryTemplateIndexDto indexDto = new LibraryTemplateIndexDto();
+      indexDto.setBoxPosition("A01");
+      indexDto.setIndex1Id(4L);
+
+      pollingHelper("put", Arrays.asList(indexDto), CONTROLLER_BASE + "/" + templateId + "/indices");
+
+      LibraryTemplate template = currentSession().get(entityClass, templateId);
+      assertNotNull(template.getIndexOnes().get("A01"));
+      assertEquals(4L, template.getIndexOnes().get("A01").getId());
   }
-
-  @Test
-  public void testCreate() throws Exception {
-    LibraryTemplate fam = baseTestCreate(CONTROLLER_BASE, makeCreateDtos().get(0), controllerClass, 200);
-    assertEquals(fam.getAlias(), "one");
-  }
-
-  @Test
-  public void testUpdate() throws Exception {
-    LibraryTemplateDto testlibtemp = Dtos.asDto(currentSession().get(LibraryTemplate.class, 1));
-    testlibtemp.setAlias("tester");
-
-    LibraryTemplate updated = baseTestUpdate(CONTROLLER_BASE, testlibtemp, 1, controllerClass);
-    assertEquals("tester", updated.getAlias());
-  }
-
-
-
-  private List<LibraryTemplateDto> makeCreateDtos() {
-
-    List<LibraryTemplateDto> dtos = new ArrayList<LibraryTemplateDto>();
-    LibraryTemplateDto one = new LibraryTemplateDto();
-    one.setAlias("one");
-
-    LibraryTemplateDto two = new LibraryTemplateDto();
-    two.setAlias("two");
-
-    dtos.add(one);
-    dtos.add(two);
-
-    return dtos;
-  }
-
-  @Test
-  public void testBulkCreateAsync() throws Exception {
-    List<LibraryTemplate> libraryTemplates =
-        baseTestBulkCreateAsync(CONTROLLER_BASE, controllerClass, makeCreateDtos());
-    assertEquals("one",libraryTemplates.get(0).getAlias());
-    assertEquals("two", libraryTemplates.get(1).getAlias());
-  }
-
-  @Test
-  public void testBulkUpdateAsync() throws Exception {
-    // only admin can update these
-    LibraryTemplateDto one = Dtos.asDto(currentSession().get(LibraryTemplate.class, 1));
-    LibraryTemplateDto three = Dtos.asDto(currentSession().get(LibraryTemplate.class, 3));
-    one.setAlias("one");
-    three.setAlias("three");
-
-    List<LibraryTemplateDto> dtos = new ArrayList<LibraryTemplateDto>();
-    dtos.add(one);
-    dtos.add(three);
-
-
-    List<LibraryTemplate> libraryTemplates =baseTestBulkUpdateAsync(CONTROLLER_BASE, controllerClass, dtos, LibraryTemplateDto::getId);
-    assertEquals("one", libraryTemplates.get(0).getAlias());
-    assertEquals("three", libraryTemplates.get(1).getAlias());
-  }
-
-
 
   @Test
   public void testDeleteLibraryTemplate() throws Exception {
-    testBulkDelete(controllerClass, 3, CONTROLLER_BASE);
+    testBulkDelete(entityClass, 3, CONTROLLER_BASE);
   }
 
+  private String pollingHelper(String requestType, List<?> dtos, String url) throws Exception {
+      ResultActions ac;
+      if("post".equalsIgnoreCase(requestType)) {
+          ac = getMockMvc().perform(post(url).contentType(MediaType.APPLICATION_JSON)
+                  .content(makeJson(dtos)));
+      } else if ("put".equalsIgnoreCase(requestType)) {
+          ac = getMockMvc().perform(put(url).contentType(MediaType.APPLICATION_JSON)
+                  .content(makeJson(dtos)));
+      } else {
+          throw new IllegalArgumentException("Unknown request type: " + requestType);
+      }
+
+      String responseContent = ac.andExpect(status().isAccepted()).andReturn().getResponse().getContentAsString();
+      String id = JsonPath.read(responseContent, "$.operationId");
+
+      return pollingResponse(CONTROLLER_BASE + "/bulk/" + id);
+  }
 
 }
