3ff3d4d906043e9904a050376e6db7195422b1ab
Riya Patel <rpatel@oicr.on.ca>
2026-04-14 15:37:08 -0400

    Resolved: migration error

--- sqlstore/src/main/resources/db/migration/V8010__arrayrun_sample.sql
+++ sqlstore/src/main/resources/db/migration/V8010__arrayrun_sample.sql
@@ -11,9 +11,11 @@ CREATE TABLE ArrayRun_Sample (
   qcUser bigint,
   qcDate date,
   PRIMARY KEY (arrayRunId, arrayId, position, sampleId),
-  CONSTRAINT fk_arrayRunSample_arrayRun FOREIGN KEY (arrayRunId, arrayId) REFERENCES ArrayRun (arrayRunId, arrayId),
-  CONSTRAINT fk_arrayRunSample_arrayPosition FOREIGN KEY (arrayId, position, sampleId)
-    REFERENCES ArrayPosition (arrayId, position, sampleId),
+  CONSTRAINT fk_arrayRunSample_arrayRun FOREIGN KEY (arrayRunId) REFERENCES ArrayRun (arrayRunId),
+  CONSTRAINT fk_arrayRunSample_array FOREIGN KEY (arrayId) REFERENCES Array (arrayId),
+  CONSTRAINT fk_arrayRunSample_arrayPosition FOREIGN KEY (arrayId, position)
+    REFERENCES ArrayPosition (arrayId, position),
+  CONSTRAINT fk_arrayRunSample_sample FOREIGN KEY (sampleId) REFERENCES Sample (sampleId),
   CONSTRAINT fk_arrayRunSample_status FOREIGN KEY (statusId) REFERENCES RunItemQcStatus (statusId),
   CONSTRAINT fk_arrayRunSample_qcUser FOREIGN KEY (qcUser) REFERENCES User (userId)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
