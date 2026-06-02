29a72c08672144447ea084dad95679efd821b99e
Riya Patel <rpatel@oicr.on.ca>
2026-06-02 15:56:53 -0400

    GLT-4488-Test-RunRestController (#3193)
    * modifying fields in dtos for update tests, id checking for update, and debug mode added
    
    * Updated: RunRestControllerST#testGetSpreadsheet
    
    * Removed unnecessary imports
    
    * Updated: RunRestControllerST
    
    * Updated testAddContainerByBarcode
    
    * Resolved InstrumentStatusRestControllerST
    
    ---------
    
    Co-authored-by: PrabhjotChopra <pchopra@oicr.on.ca>

--- miso-web/src/it/resources/db/migration/integration_test_data.sql
+++ miso-web/src/it/resources/db/migration/integration_test_data.sql
@@ -395,7 +395,7 @@ INSERT INTO InstrumentModel (instrumentModelId, platform, alias, numContainers, 
   (3, 'PACBIO', 'PacBio RS II', 1, 'SEQUENCER'),
   (4, 'ILLUMINA', 'Illumina iScan', 1, 'ARRAY_SCANNER'),
   (5, 'ILLUMINA', 'Deletable', 1, 'OTHER');
-  
+
 INSERT INTO SequencingParameters (parametersId, name, instrumentModelId, readLength, readLength2, createdBy, updatedBy, creationDate, lastUpdated, chemistry) VALUES
   (1, 'Custom (see notes)', 3, 0, 0, 1, 1, '2017-09-01 09:00:00', '2017-09-01 09:00:00', NULL),
   (2, 'Rapid Run 2x151', 1, 151, 151, 1, 1, '2017-09-01 09:00:00', '2017-09-01 09:00:00', 'RAPID_RUN'),
